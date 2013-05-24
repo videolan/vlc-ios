9d2f31f27f7758fa529feae16e1c278543221295
Felix Paul Kühne <fkuehne@videolan.org>
2013-05-24 17:41:51 +0200

    fix done button style in about panel

--- AspenProject/VLCAboutViewController.m
+++ AspenProject/VLCAboutViewController.m
@@ -26,6 +26,7 @@
     [super viewDidLoad];
 
     self.dismissButton.title = NSLocalizedString(@"BUTTON_DONE", @"");
+    self.dismissButton.style = UIBarButtonItemStyleDone;
     self.textContents.text = [NSString stringWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"About Contents" ofType:@"txt"] encoding:NSUTF8StringEncoding error:nil];
     self.aspenVersion.text = [[NSString stringWithFormat:NSLocalizedString(@"VERSION_FORMAT",@""), [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleVersion"]] stringByAppendingFormat:@" %@", kVLCVersionCodename];
     self.vlckitVersion.text = [NSString stringWithFormat:NSLocalizedString(@"BASED_ON_FORMAT",@""),[[VLCLibrary sharedLibrary] version]];
