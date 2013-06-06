3c1c73c5319b4d4aad387c5732d594c030e44ada
Felix Paul Kühne <fkuehne@videolan.org>
2013-06-06 21:33:59 +0200

    Add custom slider class

---
+++ AspenProject/VLCSlider.h
@@ -0,0 +1,17 @@
+//
+//  VLCSlider.h
+//  VLC for iOS
+//
+//  Created by Felix Paul Kühne on 06.06.13.
+//  Copyright (c) 2013 VideoLAN. All rights reserved.
+//
+
+#import "OBSlider.h"
+
+@interface VLCOBSlider : OBSlider
+
+@end
+
+@interface VLCSlider : UISlider
+
+@end
---
+++ AspenProject/VLCSlider.m
@@ -0,0 +1,55 @@
+//
+//  VLCSlider.m
+//  VLC for iOS
+//
+//  Created by Felix Paul Kühne on 06.06.13.
+//  Copyright (c) 2013 VideoLAN. All rights reserved.
+//
+
+#import "VLCSlider.h"
+
+@implementation VLCOBSlider
+
+- (void)awakeFromNib
+{
+    self.minimumValueImage = [UIImage imageNamed:@"sliderminiValue"];
+    self.maximumValueImage = [UIImage imageNamed:@"slidermaxValue"];
+    [self setMinimumTrackImage:[UIImage imageNamed:@"sliderminimumTrack"] forState:UIControlStateNormal];
+    [self setMaximumTrackImage:[UIImage imageNamed:@"slidermaximumTrack"] forState:UIControlStateNormal];
+    [self setThumbImage:[UIImage imageNamed:@"ballSlider"] forState:UIControlStateNormal];
+    [self setThumbImage:[UIImage imageNamed:@"knobSlider"] forState:UIControlStateHighlighted];
+}
+
+- (CGRect)trackRectForBounds:(CGRect)bounds
+{
+    CGRect returnValue = [super trackRectForBounds:bounds];
+    returnValue.origin.x = 5.;
+    returnValue.origin.y = 7.;
+    returnValue.size.width = bounds.size.width - 10.;
+    return returnValue;
+}
+
+@end
+
+
+@implementation VLCSlider
+
+- (void)awakeFromNib
+{
+    self.minimumValueImage = [UIImage imageNamed:@"sliderminiValue"];
+    self.maximumValueImage = [UIImage imageNamed:@"slidermaxValue"];
+    [self setMinimumTrackImage:[UIImage imageNamed:@"sliderminimumTrack"] forState:UIControlStateNormal];
+    [self setMaximumTrackImage:[UIImage imageNamed:@"slidermaximumTrack"] forState:UIControlStateNormal];
+    [self setThumbImage:[UIImage imageNamed:@"ballSlider"] forState:UIControlStateNormal];
+}
+
+- (CGRect)trackRectForBounds:(CGRect)bounds
+{
+    CGRect returnValue = [super trackRectForBounds:bounds];
+    returnValue.origin.x = 5.;
+    returnValue.origin.y = 7.;
+    returnValue.size.width = bounds.size.width - 10.;
+    return returnValue;
+}
+
+@end
