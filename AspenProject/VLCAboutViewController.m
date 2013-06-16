b242a5ec87045660549da0eeb884d305f2dd50d2
Romain Goyet <romain@goyet.com>
2013-06-16 21:26:59 +0200

    Remove unneeded code
    Signed-off-by: Felix Paul Kühne <fkuehne@videolan.org>

--- AspenProject/VLCAboutViewController.m
+++ AspenProject/VLCAboutViewController.m
@@ -13,13 +13,6 @@
 
 @implementation VLCAboutViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    return self;
-}
-
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
