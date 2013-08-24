6888e1d461a85b1332276365f61a3d7c46e6d1df
Felix Paul Kühne <fkuehne@videolan.org>
2013-08-24 18:34:51 +0200

    Compilation and useability fixes for the current SDKs

---
+++ AspenProject/VLCFixups.h
@@ -0,0 +1,19 @@
+//
+//  VLCFixups.h
+//  VLC for iOS
+//
+//  Created by Felix Paul Kühne on 24.08.13.
+//  Copyright (c) 2013 VideoLAN. All rights reserved.
+//
+//  Refer to the COPYING file of the official project for license.
+//
+
+#ifndef __IPHONE_7_0
+
+@interface UINavigationBar (UnimplementedCompileTimeFixesForThePast)
+
+@property(nonatomic,retain) UIColor *barTintColor;
+
+@end
+
+#endif
