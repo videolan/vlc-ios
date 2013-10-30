85dcfcf0cb37fd1b040d943485c32855025cc5c5
Felix Paul Kühne <fkuehne@videolan.org>
2013-10-30 16:52:57 +0100

    iPad Library view: fill the cell with the thumbnail on iOS and use different cell sizes per orientation
    See http://cl.ly/image/0p1G3h3B3H1D

--- AspenProject/VLCPlaylistViewController.m
+++ AspenProject/VLCPlaylistViewController.m
@@ -371,24 +371,34 @@
 
 - (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath
 {
-    if (SYSTEM_RUNS_IOS7_OR_LATER)
-        return CGSizeMake(308.0, 174.0);
+    if (SYSTEM_RUNS_IOS7_OR_LATER) {
+        if (UIDeviceOrientationIsLandscape([UIDevice currentDevice].orientation))
+            return CGSizeMake(341., 190.);
+        else
+            return CGSizeMake(384., 216.);
+    }
 
     return CGSizeMake(298.0, 220.0);
 }
 
 - (UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout insetForSectionAtIndex:(NSInteger)section
 {
+    if (SYSTEM_RUNS_IOS7_OR_LATER)
+        return UIEdgeInsetsMake(0., 0., 0., 0.);
     return UIEdgeInsetsMake(0.0, 34.0, 0.0, 34.0);
 }
 
 - (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout minimumLineSpacingForSectionAtIndex:(NSInteger)section
 {
+    if (SYSTEM_RUNS_IOS7_OR_LATER)
+        return 0.;
     return 10.0;
 }
 
 - (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout minimumInteritemSpacingForSectionAtIndex:(NSInteger)section
 {
+    if (SYSTEM_RUNS_IOS7_OR_LATER)
+        return 0.;
     return 10.0;
 }
 
@@ -485,6 +495,11 @@
 /* introduced in iOS 6 */
 - (BOOL)shouldAutorotate {
     return (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) || (_foundMedia.count > 0);
+}
+
+- (void)willRotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration {
+    [super willRotateToInterfaceOrientation:toInterfaceOrientation duration:duration];
+    [self.collectionView.collectionViewLayout invalidateLayout];
 }
 
 @end
