64cd4782d5952514b240f9289e07f3d625fb09a9
Felix Paul Kühne <fkuehne@videolan.org>
2013-12-25 15:03:35 +0100

    nav con: fix iOS 6 appearance

--- Sources/UINavigationController+Theme.m
+++ Sources/UINavigationController+Theme.m
@@ -18,9 +18,10 @@
 {
     UINavigationBar *navBar = self.navigationBar;
     if (!SYSTEM_RUNS_IOS7_OR_LATER) {
-        navBar.tintColor = [UIColor colorWithRed:(242.0f/255.0f) green:(112.0f/255.0f) blue:0.0f alpha:1.f];
+        [navBar setBackgroundImage:[UIImage imageNamed:@"navBarBackground"] forBarMetrics:UIBarMetricsDefault];
         navBar.barStyle = UIBarStyleBlack;
         navBar.translucent = NO;
+        navBar.opaque = YES;
     } else {
         navBar.barTintColor = [UIColor colorWithRed:1.0f green:(132.0f/255.0f) blue:0.0f alpha:1.f];
         navBar.tintColor = [UIColor whiteColor];
