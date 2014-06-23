c656a32affc0101ec9ef9417f5a7dd60597e219b
Felix Paul Kühne <fkuehne@videolan.org>
2014-06-23 20:53:19 +0200

    fix copyright header

--- Sources/UIColor+Presets.h
+++ Sources/UIColor+Presets.h
@@ -1,10 +1,14 @@
-//
-//  UIColor+Presets.h
-//  VLC for iOS
-//
-//  Created by Felix Paul Kühne on 23.06.14.
-//  Copyright (c) 2014 VideoLAN. All rights reserved.
-//
+/*****************************************************************************
+ * UIColor+Presets.m
+ * VLC for iOS
+ *****************************************************************************
+ * Copyright (c) 2014 VideoLAN. All rights reserved.
+ * $Id$
+ *
+ * Authors: Felix Paul Kühne <fkuehne # videolan org>
+ *
+ * Refer to the COPYING file of the official project for license.
+ *****************************************************************************/
 
 #import <UIKit/UIKit.h>
 
