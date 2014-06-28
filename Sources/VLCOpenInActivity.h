0d205887c1720befa4bf86ea006697fc86e093e8
Felix Paul Kühne <fkuehne@videolan.org>
2014-06-27 18:55:32 -0700

    VLCOpenInActivity: fix copyright header

--- Sources/VLCOpenInActivity.h
+++ Sources/VLCOpenInActivity.h
@@ -1,10 +1,14 @@
-//
-//  VLCOpenInActivity.h
-//  VLC for iOS
-//
-//  Created by Marc Etcheverry on 6/26/14.
-//  Copyright (c) 2014 VideoLAN. All rights reserved.
-//
+/*****************************************************************************
+ * VLCOpenInActivity.h
+ * VLC for iOS
+ *****************************************************************************
+ * Copyright (c) 2014 VideoLAN. All rights reserved.
+ * $Id$
+ *
+ * Authors: Marc Etcheverry <marc # taplightsoftware com>
+ *
+ * Refer to the COPYING file of the official project for license.
+ *****************************************************************************/
 
 /// A UIActivity that handles multiple files and presents them in a UIDocumentInteractionController
 /// This class is inspired by https://github.com/honkmaster/TTOpenInAppActivity
--- Sources/VLCOpenInActivity.m
+++ Sources/VLCOpenInActivity.m
@@ -1,10 +1,14 @@
-//
-//  VLCOpenInActivity.m
-//  VLC for iOS
-//
-//  Created by Marc Etcheverry on 6/26/14.
-//  Copyright (c) 2014 VideoLAN. All rights reserved.
-//
+/*****************************************************************************
+ * VLCOpenInActivity.m
+ * VLC for iOS
+ *****************************************************************************
+ * Copyright (c) 2014 VideoLAN. All rights reserved.
+ * $Id$
+ *
+ * Authors: Marc Etcheverry <marc # taplightsoftware com>
+ *
+ * Refer to the COPYING file of the official project for license.
+ *****************************************************************************/
 
 #import "VLCOpenInActivity.h"
 
