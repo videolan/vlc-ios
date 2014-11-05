4bdf8f72c33f1de6714e0d61868b8dc7f99fc969
Felix Paul Kühne <fkuehne@videolan.org>
2014-11-05 21:26:26 +0100

    Update the Dropbox SDK version

--- compileVLCforiOS.sh
+++ compileVLCforiOS.sh
@@ -233,7 +233,7 @@ else
 cd CocoaHTTPServer && git pull --rebase && cd ..
 fi
 if ! [ -e Dropbox ]; then
-DROPBOXSDKVERSION=1.3.9
+DROPBOXSDKVERSION=1.3.13
 curl -O https://www.dropbox.com/static/developers/dropbox-ios-sdk-${DROPBOXSDKVERSION}.zip
 unzip -q dropbox-ios-sdk-${DROPBOXSDKVERSION}.zip
 mv dropbox-ios-sdk-${DROPBOXSDKVERSION} Dropbox
