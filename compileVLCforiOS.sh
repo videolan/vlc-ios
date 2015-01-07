53bb4c60fbce6b8a549e60629777fb08ec9a86dd
Felix Paul Kühne <fkuehne@videolan.org>
2015-01-08 00:47:50 +0100

    Compile ARMv7s slice of the LiveSDK

--- compileVLCforiOS.sh
+++ compileVLCforiOS.sh
@@ -204,6 +204,7 @@ rm -rf __MACOSX
 fi
 if ! [ -e OneDrive ]; then
 git clone git://github.com/liveservices/LiveSDK-for-iOS.git OneDrive
+git am ../../patches/onedrive/0001-Compile-ARMv7s-slice.patch
 else
 cd OneDrive && git pull --rebase && cd ..
 fi
