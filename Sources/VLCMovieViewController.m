be55982ccd3206265033e5305ea8b85997483705
Felix Paul Kühne <fkuehne@videolan.org>
2015-03-12 21:23:03 +0100

    Revert 'make swipe jump duration variable to video length'
    This reverts commit 0329809becb13591ce1b5d7ec07c678c89052d9e.
    
    The feature will be back in 2.6 along with an option to keep the current behavior according to popular beta tester feedback.

--- Sources/VLCMovieViewController.m
+++ Sources/VLCMovieViewController.m
@@ -31,6 +31,8 @@
 #import "VLCStatusLabel.h"
 
 #define INPUT_RATE_DEFAULT  1000.
+#define FORWARD_SWIPE_DURATION 30
+#define BACKWARD_SWIPE_DURATION 10
 
 #define TRACK_SELECTOR_TABLEVIEW_CELL @"track selector table view cell"
 #define TRACK_SELECTOR_TABLEVIEW_SECTIONHEADER @"track selector table view section header"
@@ -1728,25 +1730,22 @@ typedef NS_ENUM(NSInteger, VLCPanType) {
         return;
 
     NSString * hudString = @" ";
-    int swipeDuration = (int)(_mediaDuration*0.001*0.05);
 
     if (swipeRecognizer.direction == UISwipeGestureRecognizerDirectionRight) {
         double timeRemainingDouble = (-_mediaPlayer.remainingTime.intValue*0.001);
         int timeRemaining = timeRemainingDouble;
 
-        if (swipeDuration < timeRemaining) {
-            if (swipeDuration < 1)
-                swipeDuration = 1;
-            [_mediaPlayer jumpForward:swipeDuration];
-            hudString = [NSString stringWithFormat:@"⇒ %is", swipeDuration];
+        if (FORWARD_SWIPE_DURATION < timeRemaining) {
+            [_mediaPlayer jumpForward:FORWARD_SWIPE_DURATION];
+            hudString = [NSString stringWithFormat:@"⇒ %is", FORWARD_SWIPE_DURATION];
         } else {
             [_mediaPlayer jumpForward:(timeRemaining - 5)];
             hudString = [NSString stringWithFormat:@"⇒ %is",(timeRemaining - 5)];
         }
     }
     else if (swipeRecognizer.direction == UISwipeGestureRecognizerDirectionLeft) {
-        [_mediaPlayer jumpBackward:swipeDuration];
-        hudString = [NSString stringWithFormat:@"⇐ %is",swipeDuration];
+        [_mediaPlayer jumpBackward:BACKWARD_SWIPE_DURATION];
+        hudString = [NSString stringWithFormat:@"⇐ %is",BACKWARD_SWIPE_DURATION];
     }
 
     if (swipeRecognizer.state == UIGestureRecognizerStateEnded) {
