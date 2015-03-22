0f86571f39e1a695b8d58059cbf01e293a78aeff
Carola Nitz <nitz.carola@googlemail.com>
2015-03-22 15:59:51 +0000

    initial watchkit extension

---
+++ VLC for iOS WatchKit Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  VLC for iOS WatchKit Extension
+//
+//  Created by Carola Nitz on 22/03/15.
+//  Copyright (c) 2015 VideoLAN. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
