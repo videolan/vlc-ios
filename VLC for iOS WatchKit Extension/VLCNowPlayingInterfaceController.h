a6306ca56755a5b558edecd5fdd6a40368a9f40a
Tobias Conradi <videolan@tobias-conradi.de>
2015-05-11 18:00:34 +0200

    Increase button size of backward, play/pause, forward buttons in now playing view controller still needs bigger assets

--- VLC for iOS WatchKit Extension/VLCNowPlayingInterfaceController.h
+++ VLC for iOS WatchKit Extension/VLCNowPlayingInterfaceController.h
@@ -19,6 +19,7 @@
 @property (weak, nonatomic) IBOutlet WKInterfaceLabel *durationLabel;
 @property (weak, nonatomic) IBOutlet WKInterfaceButton *skipBackwardButton;
 @property (weak, nonatomic) IBOutlet WKInterfaceButton *playPauseButton;
+@property (weak, nonatomic) IBOutlet WKInterfaceGroup *playPauseButtonGroup;
 @property (weak, nonatomic) IBOutlet WKInterfaceButton *skipForwardButton;
 @property (weak, nonatomic) IBOutlet WKInterfaceObject *progressObject;
 @property (weak, nonatomic) IBOutlet WKInterfaceSlider *volumeSlider;
