d9887af558f3b783fac001f87bb745fd51af07c2
Pierre SAGASPE <pierre.sagaspe@me.com>
2015-05-15 11:13:05 +0200

    Settings: add user plex account
    Signed-off-by: Felix Paul Kühne <fkuehne@videolan.org>

--- Sources/VLCConstants.h
+++ Sources/VLCConstants.h
@@ -71,6 +71,8 @@
 #define kVLCPLEXPort @"plex-port"
 #define kVLCLastPLEXServer @"last-plex-server"
 #define kVLCLastPLEXPort @"last-plex-port"
+#define kVLCPLEXLogin @"plex-login"
+#define kVLCPLEXPassword @"plex-password"
 
 #define kSupportedFileExtensions @"\\.(3gp|3gp|3gp2|3gpp|amv|asf|avi|axv|divx|dv|flv|f4v|gvi|gxf|m1v|m2p|m2t|m2ts|m2v|m4v|mks|mkv|moov|mov|mp2v|mp4|mpeg|mpeg1|mpeg2|mpeg4|mpg|mpv|mt2s|mts|mxf|mxg|nsv|nuv|oga|ogg|ogm|ogv|ogx|spx|ps|qt|rec|rm|rmvb|tod|ts|tts|vob|vro|webm|wm|wmv|wtv|xesc)$"
 #define kSupportedSubtitleFileExtensions @"\\.(srt|sub|cdg|idx|utf|ass|ssa|aqt|jss|psb|rt|smi|txt|smil)$"
