97428cd19f865955306420d02d80032455234a93
Felix Paul Kühne <fkuehne@videolan.org>
2015-05-25 20:44:17 +0200

    playback: fix 'crop to fill screen' on iPad (closes #14575)
    (cherry picked from commit f115f2f87bf4f3e2ec14e3fb68fc3246a86f44fa)

--- Sources/VLCPlaybackController.m
+++ Sources/VLCPlaybackController.m
@@ -560,7 +560,7 @@
                 _mediaPlayer.videoCropGeometry = "16:9";
             else if (f_ar == (float)(2./3.)) // all other iPhones
                 _mediaPlayer.videoCropGeometry = "16:10"; // libvlc doesn't support 2:3 crop
-            else if (f_ar == .75) // all iPads
+            else if (f_ar == (float)(1. + (1./3.))) // all iPads
                 _mediaPlayer.videoCropGeometry = "4:3";
             else if (f_ar == .5625) // AirPlay
                 _mediaPlayer.videoCropGeometry = "16:9";
