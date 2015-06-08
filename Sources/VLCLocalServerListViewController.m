8727b9a07e2a34d631d86daac6252be3457016dd
Felix Paul Kühne <fkuehne@videolan.org>
2015-06-08 16:13:01 +0200

    fix upnpx crash until resolved upstream

--- Sources/VLCLocalServerListViewController.m
+++ Sources/VLCLocalServerListViewController.m
@@ -63,6 +63,7 @@
 
     BOOL _udnpDiscoveryRunning;
     NSTimer *_searchTimer;
+    BOOL _setup;
 }
 
 @property (nonatomic) VLCHTTPUploaderController *uploadController;
@@ -235,7 +236,10 @@
     [[managerInstance DB] addObserver:self];
 
     //Optional; set User Agent
-    [[managerInstance SSDP] setUserAgentProduct:[NSString stringWithFormat:@"VLCforiOS/%@", [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"]] andOS:[NSString stringWithFormat:@"iOS/%@", [[UIDevice currentDevice] systemVersion]]];
+    if (!_setup) {
+        [[managerInstance SSDP] setUserAgentProduct:[NSString stringWithFormat:@"VLCforiOS/%@", [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"]] andOS:[NSString stringWithFormat:@"iOS/%@", [[UIDevice currentDevice] systemVersion]]];
+        _setup = YES;
+    }
 
     //Search for UPnP Devices
     [[managerInstance SSDP] startSSDP];
