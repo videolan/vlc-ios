5610359c4db0fd90ded81c0ebd8fdea338fe2925
Felix Paul Kühne <fkuehne@videolan.org>
2015-06-08 15:58:49 +0200

    plex: clean-up the implementation to remove mutable states from APIs, mutable states from iterations, to save ObjC selector dispatches and to never perform network operations on the main thread
    (cherry picked from commit 4de4e74e9d18edeea10a5e7b834c6ea646ae9cf9)

--- Sources/VLCPlexMediaInformationViewController.h
+++ Sources/VLCPlexMediaInformationViewController.h
@@ -25,6 +25,6 @@
 - (IBAction)download:(id)sender;
 - (IBAction)markMedia:(id)sender;
 
-- (id)initPlexMediaInformation:(NSMutableArray *)mediaInformation serverAddress:(NSString *)serverAddress portNumber:(NSString *)portNumber atPath:(NSString *)path authentification:(NSString *)auth;
+- (id)initPlexMediaInformation:(NSDictionary *)mediaObject serverAddress:(NSString *)serverAddress portNumber:(NSString *)portNumber atPath:(NSString *)path authentification:(NSString *)auth;
 
 @end
