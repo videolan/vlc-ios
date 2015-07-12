2e90ee50c6045e498101cb0faaf752db7be113fd
Felix Paul Kühne <fkuehne@videolan.org>
2015-07-12 17:05:20 +0200

    fix runtime exceptions

--- Sources/VLCDownloadViewController.m
+++ Sources/VLCDownloadViewController.m
@@ -274,7 +274,7 @@
                                                      delegate:self
                                             cancelButtonTitle:NSLocalizedString(@"BUTTON_CANCEL", nil)
                                             otherButtonTitles:nil];
-    [alert show];
+    [alert performSelectorOnMainThread:@selector(show) withObject:nil waitUntilDone:NO];
 }
 
 - (void)progressUpdatedTo:(CGFloat)percentage receivedDataSize:(CGFloat)receivedDataSize  expectedDownloadSize:(CGFloat)expectedDownloadSize
@@ -348,12 +348,12 @@
     _FTPDownloadRequest = nil;
     [self downloadEnded];
 
-    VLCAlertView * alert = [[VLCAlertView alloc] initWithTitle:[NSString stringWithFormat:NSLocalizedString(@"ERROR_NUMBER", nil), request.error.errorCode]
+    VLCAlertView *alert = [[VLCAlertView alloc] initWithTitle:[NSString stringWithFormat:NSLocalizedString(@"ERROR_NUMBER", nil), request.error.errorCode]
                                                        message:request.error.message
                                                       delegate:self
                                              cancelButtonTitle:NSLocalizedString(@"BUTTON_CANCEL", nil)
                                              otherButtonTitles:nil];
-    [alert show];
+    [alert performSelectorOnMainThread:@selector(show) withObject:nil waitUntilDone:NO];
 }
 
 #pragma mark - table view data source
