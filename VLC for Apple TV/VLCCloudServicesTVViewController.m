dd6063371b80543dd0f9d6735f1eb8f5cd4eae74
Felix Paul Kühne <fkuehne@videolan.org>
2015-10-22 20:10:41 +0200

    ATV cloud services: add basic dropbox support

--- VLC for Apple TV/VLCCloudServicesTVViewController.m
+++ VLC for Apple TV/VLCCloudServicesTVViewController.m
@@ -12,16 +12,21 @@
 #import "VLCCloudServicesTVViewController.h"
 #import <DropboxTVSDK/DropboxSDK.h>
 #import "VLCDropboxController.h"
+#import "VLCDropboxTableViewController.h"
+#import "SSKeychain.h"
+#import "VLCPlayerDisplayController.h"
 
 @interface VLCCloudServicesTVViewController ()
 
+@property (nonatomic) VLCDropboxTableViewController *dropboxTableViewController;
 @end
 
 @implementation VLCCloudServicesTVViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+    self.dropboxTableViewController = [[VLCDropboxTableViewController alloc] initWithNibName:@"VLCCloudStorageTableViewController" bundle:nil];
 }
 
 - (NSString *)title
@@ -31,25 +36,28 @@
 
 - (void)dropbox:(id)sender
 {
-    if (![[VLCDropboxController sharedInstance] isAuthorized]) {
-        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Login failure"
-                                                                       message:@"To use Dropbox, you need to login to iCloud with the same ID to both this Apple TV and an iOS device.\nAfterwards, login to Dropbox using the VLC app on your iOS device and try again."
-                                                                preferredStyle:UIAlertControllerStyleAlert];
-
-        UIAlertAction *defaultAction = [UIAlertAction actionWithTitle:NSLocalizedString(@"BUTTON_OK", nil)
-                                                                style:UIAlertActionStyleDestructive
-                                                              handler:^(UIAlertAction * action) {
-                                                              }];
-
-        UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:NSLocalizedString(@"BUTTON_RETRY", nil)
-                                                               style:UIAlertActionStyleDefault
-                                                             handler:^(UIAlertAction * action) {
-                                                                 [self dropbox:nil];
-                                                             }];
-        [alert addAction:defaultAction];
-        [alert addAction:cancelAction];
-        [self presentViewController:alert animated:YES completion:nil];
+    if ([[VLCDropboxController sharedInstance] restoreFromSharedCredentials]) {
+        [self showDetailViewController:self.dropboxTableViewController sender:self];
+        return;
     }
+
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Login failure"
+                                                                   message:@"To use Dropbox, you need to login to iCloud with the same ID to both this Apple TV and an iOS device.\nAfterwards, login to Dropbox using the VLC app on your iOS device and try again."
+                                                            preferredStyle:UIAlertControllerStyleAlert];
+
+    UIAlertAction *defaultAction = [UIAlertAction actionWithTitle:NSLocalizedString(@"BUTTON_OK", nil)
+                                                            style:UIAlertActionStyleDestructive
+                                                          handler:^(UIAlertAction * action) {
+                                                          }];
+
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:NSLocalizedString(@"BUTTON_RETRY", nil)
+                                                           style:UIAlertActionStyleDefault
+                                                         handler:^(UIAlertAction * action) {
+                                                             [self dropbox:nil];
+                                                         }];
+    [alert addAction:defaultAction];
+    [alert addAction:cancelAction];
+    [self presentViewController:alert animated:YES completion:nil];
 }
 
 @end
--- VLC for Apple TV/VLCTVConstants.h
+++ VLC for Apple TV/VLCTVConstants.h
@@ -16,6 +16,7 @@
 #define kVLCApplicationGroupIdentifier @"group.org.videolan.vlc-ios"
 
 #define kVLCRecentURLs @"recent-urls"
+#define kVLCStoreDropboxCredentials @"kVLCStoreDropboxCredentials"
 
 #define kSupportedFileExtensions @"\\.(3gp|3gp|3gp2|3gpp|amv|asf|avi|axv|divx|dv|flv|f4v|gvi|gxf|m1v|m2p|m2t|m2ts|m2v|m4v|mks|mkv|moov|mov|mp2v|mp4|mpeg|mpeg1|mpeg2|mpeg4|mpg|mpv|mt2s|mts|mxf|mxg|nsv|nuv|oga|ogg|ogm|ogv|ogx|spx|ps|qt|rec|rm|rmvb|tod|ts|tts|vlc|vob|vro|webm|wm|wmv|wtv|xesc)$"
 #define kSupportedSubtitleFileExtensions @"\\.(srt|sub|cdg|idx|utf|ass|ssa|aqt|jss|psb|rt|smi|txt|smil)$"
