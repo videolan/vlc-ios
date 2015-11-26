314abea8594dff679a962dcc21b680b3ab7b8156
Felix Paul Kühne <fkuehne@videolan.org>
2015-11-26 18:15:15 +0100

    NSString: add extension to convert language codes between 2 and 3 characters as well as to a localized name

---
+++ SharedSources/NSString+Locale.h
@@ -0,0 +1,20 @@
+/*****************************************************************************
+ * VLC for iOS
+ *****************************************************************************
+ * Copyright (c) 2015 VideoLAN. All rights reserved.
+ * $Id$
+ *
+ * Authors: Felix Paul Kühne <fkuehne # videolan.org>
+ *
+ * Refer to the COPYING file of the official project for license.
+ *****************************************************************************/
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (LocaleCodes)
+
+- (NSString *)twoLetterLanguageKeyForThreeLetterCode;
+- (NSString *)threeLetterLanguageKeyForTwoLetterCode;
+- (NSString *)localizedLanguageNameForTwoLetterCode;
+
+@end
