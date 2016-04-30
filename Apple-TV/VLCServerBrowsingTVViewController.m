a1617b836cdaf052a5c88cbff70b6add864726b2
Vincent L. Cone <vincent.l.cone@tuta.io>
2016-04-30 19:48:37 +0200

    tvOS: Remove collection view focus recovery workaround since we no longer perform reloads, instead use inserts and deletes if the content changes
    Signed-off-by: Felix Paul Kühne <fkuehne@videolan.org>

--- Apple-TV/VLCServerBrowsingTVViewController.m
+++ Apple-TV/VLCServerBrowsingTVViewController.m
@@ -20,8 +20,6 @@
 @interface VLCServerBrowsingTVViewController ()
 {
     UILabel *_nothingFoundLabel;
-    NSIndexPath *_currentFocus;
-    BOOL _focusChangeAllowed;
 }
 @property (nonatomic) VLCServerBrowsingController *browsingController;
 @property (nonatomic) NSArray<id <VLCNetworkServerBrowserItem>> *items;
@@ -42,9 +40,6 @@
         self.title = serverBrowser.title;
 
         self.downloadArtwork = [[NSUserDefaults standardUserDefaults] boolForKey:kVLCSettingDownloadArtwork];
-
-        _currentFocus = [NSIndexPath indexPathForRow:0 inSection:0];
-        _focusChangeAllowed = YES;
     }
     return self;
 }
@@ -187,29 +182,6 @@
     // currently the case on the TV
     const BOOL singlePlayback = ![[NSUserDefaults standardUserDefaults] boolForKey:kVLCAutomaticallyPlayNextItem];
     [self didSelectItem:item index:row singlePlayback:singlePlayback];
-}
-
-#pragma mark - focus recovery workaround
-
-- (void)collectionView:(UICollectionView *)collectionView didUpdateFocusInContext:(UICollectionViewFocusUpdateContext *)context withAnimationCoordinator:(UIFocusAnimationCoordinator *)coordinator
-{
-    if (context.previouslyFocusedIndexPath == nil && context.nextFocusedIndexPath != nil)
-        [self setNeedsFocusUpdate];
-
-    if (context.previouslyFocusedIndexPath != nil && context.nextFocusedIndexPath != nil)
-        _currentFocus = context.nextFocusedIndexPath;
-
-    _focusChangeAllowed = context.nextFocusedIndexPath != nil;
-}
-
-- (BOOL)collectionView:(UICollectionView *)collectionView canFocusItemAtIndexPath:(NSIndexPath *)indexPath
-{
-    return _focusChangeAllowed || [_currentFocus compare:indexPath] == NSOrderedSame;
-}
-
-- (NSIndexPath *)indexPathForPreferredFocusedViewInCollectionView:(UICollectionView *)collectionView
-{
-    return _currentFocus;
 }
 
 @end
