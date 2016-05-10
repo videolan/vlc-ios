d16f001352cdcd8a5913e4cbf5e65b98825a83cc
Pierre SAGASPE <pierre.sagaspe@me.com>
2016-05-10 19:35:10 +0200

    onedrive : displaying the number of items in bottom bar (cherry picked from commit d4634a7e6ed5c0164328667c8418007d268ffa4e)

--- Sources/VLCOneDriveTableViewController.m
+++ Sources/VLCOneDriveTableViewController.m
@@ -94,6 +94,14 @@
 {
     [self.tableView reloadData];
     [self.activityIndicator stopAnimating];
+
+    NSUInteger count = self.controller.currentListFiles.count;
+    if (count == 0)
+        self.numberOfFilesBarButtonItem.title = NSLocalizedString(@"NO_FILES", nil);
+    else if (count != 1)
+        self.numberOfFilesBarButtonItem.title = [NSString stringWithFormat:NSLocalizedString(@"NUM_OF_FILES", nil), count];
+    else
+        self.numberOfFilesBarButtonItem.title = NSLocalizedString(@"ONE_FILE", nil);
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
