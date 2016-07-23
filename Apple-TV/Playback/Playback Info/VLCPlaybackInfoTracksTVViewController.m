57f9ff5ae3dfa665cb421f06eaf54f3397280c25
Felix Paul Kühne <fkuehne@videolan.org>
2016-07-23 15:31:19 +0200

    tvOS: fix compilation warning

--- Apple-TV/Playback/Playback Info/VLCPlaybackInfoTracksTVViewController.m
+++ Apple-TV/Playback/Playback Info/VLCPlaybackInfoTracksTVViewController.m
@@ -135,7 +135,12 @@
     if (row >= audioTrackIndexes.count) {
         NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
         BOOL bValue = ![defaults boolForKey:kVLCSettingUseSPDIF];
+/* we are using private API of VLCKit here, nothing to warning about */
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wundeclared-selector"
         [[VLCPlaybackController sharedInstance].mediaPlayer performSelector:@selector(setPassthroughAudio:) withObject:@(bValue)];
+#pragma clang diagnostic pop
+
         [defaults setBool:bValue forKey:kVLCSettingUseSPDIF];
         [defaults synchronize];
         /* restart the audio output */
