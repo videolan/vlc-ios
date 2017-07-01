12d0a02ff225a0483c359154879e644c9619a4fa
Carola Nitz <nitz.carola@googlemail.com>
2017-07-01 17:06:08 +0200

    move updateDisplayMetadata call out of mainqueue to avoid unresponsive UI

--- Sources/VLCPlaybackController.m
+++ Sources/VLCPlaybackController.m
@@ -933,7 +933,7 @@ VLCMediaDelegate>
 {
     if (_needsMetadataUpdate == NO) {
         _needsMetadataUpdate = YES;
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), DISPATCH_QUEUE_PRIORITY_DEFAULT, ^{
             [self _updateDisplayedMetadata];
         });
     }
