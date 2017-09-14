c465e12bac9da8bdc7b7b630529502f414bc3bed
Carola Nitz <nitz.carola@googlemail.com>
2017-09-14 11:43:06 +0200

    We can't navigate in the playlists yet hence we revert this for now
    Revert "VLCMediaFileDiscoverer: Add playlists to playable files (closes #64)"
    
    This reverts commit 92f999e93d8fc095b5f83aa9bfbf5dccd3b6d5a1.

--- Sources/VLCMediaFileDiscoverer.m
+++ Sources/VLCMediaFileDiscoverer.m
@@ -171,7 +171,7 @@ const float MediaTimerInterval = 2.f;
 
             if (exists && !isDirectory) {
                 if (self.filterResultsForPlayability) {
-                    if ([fileName isSupportedMediaFormat] || [fileName isSupportedAudioMediaFormat] || [fileName isSupportedPlaylistFormat]) {
+                    if ([fileName isSupportedMediaFormat] || [fileName isSupportedAudioMediaFormat]) {
                         [_addedFilesMapping setObject:@(0) forKey:fileName];
                         [self notifyFileAdded:fileName loading:YES];
                     }
