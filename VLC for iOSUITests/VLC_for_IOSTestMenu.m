9bbfd92f1a5ab638ac7370c157bf2c9661ccabe9
David Cordero <david@corderoramirez.com>
2018-03-09 23:32:31 +0100

    Remove redundant tearDown method with default implementation

--- VLC for iOSUITests/VLC_for_IOSTestMenu.m
+++ VLC for iOSUITests/VLC_for_IOSTestMenu.m
@@ -22,11 +22,6 @@
     [[XCUIDevice sharedDevice] setOrientation:UIDeviceOrientationFaceUp];
 }
 
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
 - (void)testMenuTabAllFiles {
 
     XCUIApplication *app = [[XCUIApplication alloc] init];
