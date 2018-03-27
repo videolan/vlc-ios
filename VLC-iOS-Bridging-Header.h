9c3532c77fa7be934681e581d9c7dceb89679b1a
Carola Nitz <nitz.carola@googlemail.com>
2018-03-27 15:58:39 +0200

    Bridging-Header: have an alphabetical order

--- VLC-iOS-Bridging-Header.h
+++ VLC-iOS-Bridging-Header.h
@@ -1,23 +1,20 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
-#import <MediaLibraryKit/MediaLibraryKit.h>
-#import "VLCPlaylistTableViewCell.h"
-#import "VLCPlaylistCollectionViewCell.h"
-#import "VLCMediaDataSource.h"
-#import <PAPasscode/PAPasscodeViewController.h>
-#import <MobileVLCKit/MobileVLCKit.h>
-#import <XKKeychain/XKKeychain.h>
+#import "UIColor+Presets.h"
+#import "VLCAboutViewController.h"
+#import "VLCCloudServicesTableViewController.h"
 #import "VLCConstants.h"
-#import "VLCPlayerDisplayController.h"
+#import "VLCDownloadViewController.h"
+#import "VLCLibrarySearchDisplayDataSource.h"
+#import "VLCMediaDataSource.h"
+#import "VLCOpenNetworkStreamViewController.h"
 #import "VLCPlaybackController+MediaLibrary.h"
 #import "VLCPlaybackNavigationController.h"
-#import "VLCLibrarySearchDisplayDataSource.h"
+#import "VLCPlayerDisplayController.h"
+#import "VLCPlaylistTableViewCell.h"
+#import "VLCPlaylistCollectionViewCell.h"
 #import "VLCServerListViewController.h"
 #import "VLCSettingsController.h"
-#import "VLCCloudServicesTableViewController.h"
-#import "VLCDownloadViewController.h"
-#import "VLCOpenNetworkStreamViewController.h"
-#import "VLCAboutViewController.h"
-#import "UIColor+Presets.h"
+
+#import <MediaLibraryKit/MediaLibraryKit.h>
+#import <MobileVLCKit/MobileVLCKit.h>
+#import <PAPasscode/PAPasscodeViewController.h>
+#import <XKKeychain/XKKeychain.h>
