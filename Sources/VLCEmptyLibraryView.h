24ff2aec3ecc0a9d8ebb3efe5117e4601b6d9cc4
Mike JS. Choi <mkchoi212@icloud.com>
2018-04-09 01:09:38 -0500

    [UI] Include empty view for media controllers
    Note this uses pre-existing VLCEmptyLibraryView Objective-C code

---
+++ Sources/VLCEmptyLibraryView.h
@@ -0,0 +1,24 @@
+/*****************************************************************************
+ * VLCEmptyLibraryView.h
+ * VLC for iOS
+ *****************************************************************************
+ * Copyright (c) 2018 VideoLAN. All rights reserved.
+ * $Id$
+ *
+ * Authors: Mike JS. Choi <mkchoi212 # icloud.com>
+ *
+ * Refer to the COPYING file of the official project for license.
+ *****************************************************************************/
+
+#import <UIKit/UIKit.h>
+
+@interface VLCEmptyLibraryView: UIView
+
+@property (nonatomic, strong) IBOutlet UILabel *emptyLibraryLabel;
+@property (nonatomic, strong) IBOutlet UILabel *emptyLibraryLongDescriptionLabel;
+@property (nonatomic, strong) IBOutlet UIActivityIndicatorView *activityIndicator;
+@property (nonatomic, strong) IBOutlet UIButton *learnMoreButton;
+
+- (IBAction)learnMore:(id)sender;
+
+@end
