3fff25758d32a0059fd0d6aaf32a22d7bfd225c7
Soomin Lee <bubu@mikan.io>
2018-04-22 11:20:25 +0200

    iOSUITests: Screenshot: Remove trailing whitespaces

--- VLC for iOSUITests/Screenshot.swift
+++ VLC for iOSUITests/Screenshot.swift
@@ -16,7 +16,7 @@ import XCTest
 class Screenshot: XCTestCase {
     let app = XCUIApplication()
     var helper: TestHelper!
-    
+
     override func setUp() {
         super.setUp()
 
@@ -24,50 +24,50 @@ class Screenshot: XCTestCase {
         SDStatusBarManager.sharedInstance().enableOverrides()
         setupSnapshot(app)
         helper = TestHelper(app)
-        
+
         app.launch()
     }
-    
+
     override func tearDown() {
         SDStatusBarManager.sharedInstance().disableOverrides()
     }
-    
+
     func testCaptureVideoPlayback() {
         download(name: "http://jell.yfish.us/media/jellyfish-10-mbps-hd-h264.mkv")
         helper.tapTabBarItem(.video)
         app.collectionViews.cells.element(boundBy: 0).tap()
         XCUIDevice.shared.orientation = .landscapeLeft
-        
+
         snapshot("playback")
     }
-    
+
     func testCaptureAudioTab() {
         helper.tapTabBarItem(.audio)
         snapshot("audio_tab")
     }
-    
+
     func testCaptureNetworkTab() {
         helper.tapTabBarItem(.localNetwork)
         snapshot("network_tab")
     }
-    
+
     func testCaptureVideoTab() {
         helper.tapTabBarItem(.video)
         snapshot("video_tab")
     }
-    
+
     func download(name fileName: String) {
         helper.tapTabBarItem(.localNetwork)
         app.cells["Downloads"].tap()
-        
+
         let downloadTextfield = app.textFields["http://myserver.com/file.mkv"]
         downloadTextfield.clearAndEnter(text: fileName)
         app.buttons["Download"].tap()
-        
+
         let cancelDownloadButton = app.buttons["flatDeleteButton"]
         let predicate = NSPredicate(format: "exists == 0")
         expectation(for: predicate, evaluatedWith: cancelDownloadButton, handler: nil)
-        
+
         waitForExpectations(timeout: 20.0) { err in
             XCTAssertNil(err)
             downloadTextfield.typeText("\n")
