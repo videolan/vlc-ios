11029b1404a1cee6fce0ff5982199086d645d0ad
Soomin Lee <bubu@mikan.io>
2018-04-24 15:47:16 +0200

    VLCActionSheet: Separate VLCActionSheetCell

--- Sources/VLCActionSheet/VLCActionSheet.swift
+++ Sources/VLCActionSheet/VLCActionSheet.swift
@@ -34,69 +34,6 @@ class VLCRendererCollectionViewLayout: UICollectionViewFlowLayout {
     }
 }
 
-// MARK: VLCActionSheetCell
-class VLCActionSheetCell: UICollectionViewCell {
-
-    static let identifier = "VLCActionSheetCell"
-
-    let icon: UIImageView = {
-        let icon = UIImageView()
-        icon.translatesAutoresizingMaskIntoConstraints = false
-        icon.contentMode = .scaleAspectFit
-        return icon
-    }()
-
-    let name: UILabel = {
-        let name = UILabel()
-        name.font = UIFont.systemFont(ofSize: 15)
-        name.translatesAutoresizingMaskIntoConstraints = false
-        return name
-    }()
-
-    let stackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.spacing = 15.0
-        stackView.axis = .horizontal
-        stackView.alignment = .center
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        return stackView
-    }()
-
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setupViews()
-    }
-
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        setupViews()
-    }
-
-    private func setupViews() {
-        stackView.addArrangedSubview(icon)
-        stackView.addArrangedSubview(name)
-        addSubview(stackView)
-
-        var guide: LayoutAnchorContainer = self
-
-        if #available(iOS 11.0, *) {
-            guide = safeAreaLayoutGuide
-        }
-        NSLayoutConstraint.activate([
-            icon.heightAnchor.constraint(equalToConstant: 25),
-            icon.widthAnchor.constraint(equalTo: icon.heightAnchor),
-
-            name.trailingAnchor.constraint(equalTo: stackView.trailingAnchor),
-            name.centerYAnchor.constraint(equalTo: stackView.centerYAnchor),
-
-            stackView.leadingAnchor.constraint(equalTo: guide.leadingAnchor, constant: 20),
-            stackView.trailingAnchor.constraint(equalTo: guide.trailingAnchor, constant: -20),
-            stackView.heightAnchor.constraint(equalTo: heightAnchor),
-            stackView.topAnchor.constraint(equalTo: topAnchor)
-            ])
-    }
-}
-
 // MARK: VLCActionSheetSectionHeader
 class VLCActionSheetSectionHeader: UIView {
 
