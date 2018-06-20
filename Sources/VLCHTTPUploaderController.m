a2c6cc00009152425af7e2ee3e8c5ebdcb98e85d
Carola Nitz <nitz.carola@googlemail.com>
2018-06-20 10:20:07 +0200

    VLCHTTPUploaderController: change standard port to 8080 to avoid Chromecast conflicts
    (cherry picked from commit a3b4797e930b50a3ef0abe6243bcb83261dc8778)

--- Sources/VLCHTTPUploaderController.m
+++ Sources/VLCHTTPUploaderController.m
@@ -107,17 +107,11 @@
 - (NSString *)httpStatus
 {
     if (_httpServer.isRunning) {
-        if (_httpServer.listeningPort != 80) {
-            return [NSString stringWithFormat:@"http://%@:%i\nhttp://%@:%i",
-                    [self currentIPAddress],
-                    _httpServer.listeningPort,
-                    [self hostname],
-                    _httpServer.listeningPort];
-        } else {
-            return [NSString stringWithFormat:@"http://%@\nhttp://%@",
-                    [self currentIPAddress],
-                    [self hostname]];
-        }
+        return [NSString stringWithFormat:@"http://%@:%i\nhttp://%@:%i",
+                [self hostname],
+                _httpServer.listeningPort,
+                [self currentIPAddress],
+                _httpServer.listeningPort];
     } else {
         return NSLocalizedString(@"HTTP_UPLOAD_SERVER_OFF", nil);
     }
@@ -221,7 +215,7 @@
     APLog(@"Setting document root: %@", docRoot);
 
     [_httpServer setDocumentRoot:docRoot];
-    [_httpServer setPort:80];
+    [_httpServer setPort:8080];
 
     [_httpServer setConnectionClass:[VLCHTTPConnection class]];
 
