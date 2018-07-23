9bc166bd41bb0066f13577edd2353bd420c8f302
Soomin Lee <bubu@mikan.io>
2018-07-23 11:44:40 +0200

    Add VideoViewController

---
+++ MediaViewControllers/VideoViewController.swift
@@ -0,0 +1,37 @@
+/*****************************************************************************
+ * VideoViewController.swift
+ * VLC for iOS
+ *****************************************************************************
+ * Copyright (c) 2018 VideoLAN. All rights reserved.
+ * $Id$
+ *
+ * Authors: Carola Nitz <caro # videolan.org>
+ *
+ * Refer to the COPYING file of the official project for license.
+ *****************************************************************************/
+
+class VLCVideoViewController: VLCMediaViewController {
+    override init(services: Services) {
+        super.init(services: services)
+        setupUI()
+    }
+
+    private func setupUI() {
+        title = NSLocalizedString("VIDEO", comment: "")
+        tabBarItem = UITabBarItem(
+            title: NSLocalizedString("VIDEO", comment: ""),
+            image: UIImage(named: "TVShowsIcon"),
+            selectedImage: UIImage(named: "TVShowsIcon"))
+        tabBarItem.accessibilityIdentifier = VLCAccessibilityIdentifier.video
+    }
+
+    override func viewControllers(for pagerTabStripController: PagerTabStripViewController) -> [UIViewController] {
+        let movies = VLCMediaCategoryViewController<MLFile>(services: services, subcategory: VLCMediaSubcategories.movies)
+        movies.delegate = super.self()
+        let episodes = VLCMediaCategoryViewController<MLShowEpisode>(services: services, subcategory: VLCMediaSubcategories.episodes)
+        episodes.delegate = super.self()
+        let playlists = VLCMediaCategoryViewController<MLLabel>(services: services, subcategory: VLCMediaSubcategories.videoPlaylists)
+        playlists.delegate = super.self()
+        return [movies, episodes, playlists]
+    }
+}
