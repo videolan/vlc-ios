62202c1bee3740dfd44dddf80e1bfb736800148f
Soomin Lee <bubu@mikan.io>
2018-07-31 13:44:06 +0200

    MediaLibraryBaseModel: Remove old notifications

--- SharedSources/MediaLibraryModel/MediaLibraryBaseModel.swift
+++ SharedSources/MediaLibraryModel/MediaLibraryBaseModel.swift
@@ -9,19 +9,6 @@
  * Refer to the COPYING file of the official project for license.
  *****************************************************************************/
 
-extension Notification.Name {
-    static let VLCMoviesDidChangeNotification = Notification.Name("MoviesDidChangeNotfication")
-    static let VLCEpisodesDidChangeNotification = Notification.Name("EpisodesDidChangeNotfication")
-    static let VLCArtistsDidChangeNotification = Notification.Name("ArtistsDidChangeNotfication")
-    static let VLCAlbumsDidChangeNotification = Notification.Name("AlbumsDidChangeNotfication")
-    static let VLCTracksDidChangeNotification = Notification.Name("TracksDidChangeNotfication")
-    static let VLCGenresDidChangeNotification = Notification.Name("GenresDidChangeNotfication")
-    static let VLCAudioPlaylistsDidChangeNotification = Notification.Name("AudioPlaylistsDidChangeNotfication")
-    static let VLCVideoPlaylistsDidChangeNotification = Notification.Name("VideoPlaylistsDidChangeNotfication")
-    static let VLCVideosDidChangeNotification = Notification.Name("VideosDidChangeNotfication")
-    static let VLCAudioDidChangeNotification = Notification.Name("AudioDidChangeNotfication")
-}
-
 protocol MediaLibraryModelView {
     func dataChanged()
 }
@@ -35,7 +22,6 @@ protocol MediaLibraryBaseModel: class {
     var view: MediaLibraryModelView? { get set }
 
     var indicatorName: String { get }
-    var notificaitonName: Notification.Name { get }
 
     func append(_ item: MLType)
     func isIncluded(_ item: MLType)
