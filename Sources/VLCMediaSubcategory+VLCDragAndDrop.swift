d066b46919fc72cbdae8d84b0cf47327a3c807bf
Soomin Lee <bubu@mikan.io>
2018-07-31 13:43:40 +0200

    Disable DnD

--- Sources/VLCMediaSubcategory+VLCDragAndDrop.swift
+++ Sources/VLCMediaSubcategory+VLCDragAndDrop.swift
@@ -12,33 +12,33 @@
 
 import Foundation
 
-@available(iOS 11.0, *)
-extension VLCMediaSubcategoryModel: VLCDragAndDropManagerDelegate {
-
-    func dragAndDropManagerRequestsFile(manager: NSObject, atIndexPath indexPath: IndexPath) -> Any? {
-        return files[indexPath.row]
-    }
-
-    func dragAndDropManagerInsertItem(manager: NSObject, item: NSManagedObject, atIndexPath indexPath: IndexPath) {
-        if item as? MLLabel != nil && indexPath.row < files.count {
-            files.remove(at: indexPath.row)
-        }
-        // TODO: handle insertion
-        //files.insert(item, at: indexPath.row)
-    }
-
-    func dragAndDropManagerDeleteItem(manager: NSObject, atIndexPath indexPath: IndexPath) {
-        files.remove(at: indexPath.row)
-    }
-
-    func dragAndDropManagerCurrentSelection(manager: NSObject) -> AnyObject? {
-
-        //TODO: Handle playlists and Collections
-        fatalError()
-    }
-
-    func dragAndDropManagerRemoveFileFromFolder(manager: NSObject, file: NSManagedObject) {
-        //TODO: handle removing from playlists and Collections
-        fatalError()
-    }
-}
+//@available(iOS 11.0, *)
+//extension MediaLibraryBaseModel: VLCDragAndDropManagerDelegate {
+//
+//    func dragAndDropManagerRequestsFile(manager: NSObject, atIndexPath indexPath: IndexPath) -> Any? {
+//        return files[indexPath.row]
+//    }
+//
+//    func dragAndDropManagerInsertItem(manager: NSObject, item: NSManagedObject, atIndexPath indexPath: IndexPath) {
+//        if item as? MLLabel != nil && indexPath.row < files.count {
+//            files.remove(at: indexPath.row)
+//        }
+//        // TODO: handle insertion
+//        //files.insert(item, at: indexPath.row)
+//    }
+//
+//    func dragAndDropManagerDeleteItem(manager: NSObject, atIndexPath indexPath: IndexPath) {
+//        files.remove(at: indexPath.row)
+//    }
+//
+//    func dragAndDropManagerCurrentSelection(manager: NSObject) -> AnyObject? {
+//
+//        //TODO: Handle playlists and Collections
+//        fatalError()
+//    }
+//
+//    func dragAndDropManagerRemoveFileFromFolder(manager: NSObject, file: NSManagedObject) {
+//        //TODO: handle removing from playlists and Collections
+//        fatalError()
+//    }
+//}
