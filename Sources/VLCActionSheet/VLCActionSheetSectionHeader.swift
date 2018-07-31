d29158b7488a4cee5514d6effecd2fb7a2e9bd1e
Mike JS. Choi <mkchoi212@icloud.com>
2018-07-31 16:42:00 +0900

    VLCActionSheet: Bold header title

--- Sources/VLCActionSheet/VLCActionSheetSectionHeader.swift
+++ Sources/VLCActionSheet/VLCActionSheetSectionHeader.swift
@@ -15,7 +15,7 @@ class VLCActionSheetSectionHeader: UIView {
 
     let title: UILabel = {
         let title = UILabel()
-        title.font = UIFont.systemFont(ofSize: 13)
+        title.font = UIFont.boldSystemFont(ofSize: 13)
         title.translatesAutoresizingMaskIntoConstraints = false
         return title
     }()
