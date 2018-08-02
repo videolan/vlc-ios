e948314420cd1316bcd5764d93cc2ae9b1c9860e
Soomin Lee <bubu@mikan.io>
2018-08-02 14:18:12 +0200

    VLCEditToolbar: Initial

---
+++ Sources/VLCEditToolbar.swift
@@ -0,0 +1,24 @@
+/*****************************************************************************
+ * VLCEditToolbar.swift
+ *
+ * Copyright © 2018 VLC authors and VideoLAN
+ * Copyright © 2018 Videolabs
+ *
+ * Authors: Soomin Lee <bubu@mikan.io>
+ *
+ * Refer to the COPYING file of the official project for license.
+ *****************************************************************************/
+
+class EditToolbar: UIToolbar {
+
+//    var mainStackView: UIStackView = {
+//        let mainStackView = UIStackView()
+//        return mainStackView
+//    }()
+//
+
+//    var deleteBarButtonItem: UIBarButtonItem {
+//        let deleteBarButtonItem = UIBarButtonItem(image: <#T##UIImage?#>, style: <#T##UIBarButtonItemStyle#>, target: <#T##Any?#>, action: <#T##Selector?#>)
+//        return deleteBarButtonItem
+//    }()
+}
