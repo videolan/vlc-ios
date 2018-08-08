c83abf7c312e46eeb9440273af0c993d96e582ce
Soomin Lee <bubu@mikan.io>
2018-08-08 15:07:56 +0200

    VideoModel: Check for duplicate before append

--- SharedSources/MediaLibraryModel/VideoModel.swift
+++ SharedSources/MediaLibraryModel/VideoModel.swift
@@ -27,6 +27,11 @@ class VideoModel: MLBaseModel {
     }
 
     func append(_ item: VLCMLMedia) {
+        for file in files {
+            if file.identifier() == item.identifier() {
+                return
+            }
+        }
         files.append(item)
     }
 
