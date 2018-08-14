05e13281f61b3520dd877fbd6240796858da21e5
Soomin Lee <bubu@mikan.io>
2018-08-14 16:35:00 +0200

    VideoModel: Listen to media deletion

--- SharedSources/MediaLibraryModel/VideoModel.swift
+++ SharedSources/MediaLibraryModel/VideoModel.swift
@@ -59,6 +59,16 @@ extension VideoModel: MediaLibraryObserver {
         videos.forEach({ append($0) })
         updateView?()
     }
+
+    func medialibrary(_ medialibrary: VLCMediaLibraryManager, didDeleteMediaWithIds ids: [NSNumber]) {
+        files = files.filter() {
+            for id in ids where $0.identifier() == id.int64Value {
+                return false
+            }
+            return true
+        }
+        updateView?()
+    }
 }
 
 extension VLCMLMedia {
