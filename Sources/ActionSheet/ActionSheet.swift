5598a6f246ec4c2706939d6588eeaff98cb8ddbd
Soomin Lee <bubu@mikan.io>
2019-05-19 12:59:06 +0200

    ActionSheet: Fix flickering on dismissal
    (closes #486)

--- Sources/ActionSheet/ActionSheet.swift
+++ Sources/ActionSheet/ActionSheet.swift
@@ -286,10 +286,11 @@ extension ActionSheet {
                         mainStackView.frame.origin.y += mainStackView.frame.size.height
                         backgroundView.alpha = 0
             }, completion: {
-                [unowned self, presentingViewController] finished in
+                [mainStackView, presentingViewController] finished in
                 // When everything is complete, reset the frame for the re-use
-                self.mainStackView.frame = realMainStackView
-                presentingViewController?.dismiss(animated: false, completion: nil)
+                mainStackView.isHidden = true
+                mainStackView.frame = realMainStackView
+                presentingViewController?.dismiss(animated: false)
         })
     }
 }
