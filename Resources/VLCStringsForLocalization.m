bd9c6ff273ff70b4c9bed7573e60ba0e9fbc3f11
Carola Nitz <nitz.carola@googlemail.com>
2019-05-22 12:17:28 +0200

    Localization: passcode strings are needed after all
    (fixes #179)

--- Resources/VLCStringsForLocalization.m
+++ Resources/VLCStringsForLocalization.m
@@ -13,4 +13,17 @@ void _notAMethod()
 {
     NSLocalizedString(@"STORE_DESCRIPTION", nil);
     NSLocalizedString(@"STORE_DESCRIPTION_TV", nil);
+    NSLocalizedString(@"%d Failed Passcode Attempts", nil);
+    NSLocalizedString(@"1 Failed Passcode Attempt", nil);
+    NSLocalizedString(@"Change Passcode", nil);
+    NSLocalizedString(@"Enter Passcode", nil);
+    NSLocalizedString(@"Enter a passcode", nil);
+    NSLocalizedString(@"Enter your new passcode", nil);
+    NSLocalizedString(@"Enter your old passcode", nil);
+    NSLocalizedString(@"Enter your passcode", nil);
+    NSLocalizedString(@"Next", nil);
+    NSLocalizedString(@"Passcodes did not match. Try again.", nil);
+    NSLocalizedString(@"Re-enter your new passcode", nil);
+    NSLocalizedString(@"Re-enter your passcode", nil);
+    NSLocalizedString(@"Set Passcode", nil);
 }
