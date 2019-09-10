videolan/vlc-ios

commit ea91de9f7ea028cefcad0b3f4e65badaf1d8967c

Adapt to iOS 13 API changes

Sources/VLCTabBarCoordinator.swift
@@ -37,11 +37,14 @@ class VLCTabBarCoordinator: NSObject {
             if let navController = $0 as? UINavigationController, navController.topViewController is VLCSettingsController {
                 navController.navigationBar.barTintColor = PresentationTheme.current.colors.navigationbarColor
                 navController.navigationBar.tintColor = PresentationTheme.current.colors.orangeUI
-                navController.navigationBar.isTranslucent = false
                 navController.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor:  PresentationTheme.current.colors.navigationbarTextColor]
 
                 if #available(iOS 11.0, *) {
                     navController.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor:  PresentationTheme.current.colors.navigationbarTextColor]
+                }
+                if #available(iOS 13.0, *) {
+                    navController.navigationBar.standardAppearance = AppearanceManager.navigationbarAppearance()
+                    navController.navigationBar.scrollEdgeAppearance = AppearanceManager.navigationbarAppearance()
                 }
             }
         }

Sources/AppearanceManager.swift
@@ -27,12 +27,12 @@ class AppearanceManager: NSObject {
         UINavigationBar.appearance().barTintColor = theme.colors.navigationbarColor
         UINavigationBar.appearance(whenContainedInInstancesOf: [VLCPlaybackNavigationController.self]).barTintColor = nil
         UINavigationBar.appearance().tintColor = theme.colors.orangeUI
-        UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor: theme.colors.navigationbarTextColor]
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: theme.colors.navigationbarTextColor]
 
         if #available(iOS 11.0, *) {
             UINavigationBar.appearance().prefersLargeTitles = true
             UINavigationBar.appearance(whenContainedInInstancesOf: [VLCPlaybackNavigationController.self]).prefersLargeTitles = false
-            UINavigationBar.appearance().largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: theme.colors.navigationbarTextColor]
+            UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: theme.colors.navigationbarTextColor]
         }
 
         let selectedBackgroundView = UIView()
@@ -53,6 +53,15 @@ class AppearanceManager: NSObject {
         UIPageControl.appearance().backgroundColor = theme.colors.background
         UIPageControl.appearance().pageIndicatorTintColor = .lightGray
         UIPageControl.appearance().currentPageIndicatorTintColor = theme.colors.orangeUI
+    }
+
+    @available(iOS 13.0, *)
+    @objc class func navigationbarAppearance() -> UINavigationBarAppearance {
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.backgroundColor = PresentationTheme.current.colors.navigationbarColor
+        navBarAppearance.titleTextAttributes = [.foregroundColor: PresentationTheme.current.colors.navigationbarTextColor]
+        navBarAppearance.largeTitleTextAttributes = [.foregroundColor: PresentationTheme.current.colors.navigationbarTextColor]
+        return navBarAppearance
     }
 }
 

Sources/LocalNetworkConnectivity/VLCNetworkLoginViewController.m
@@ -68,6 +68,10 @@
     self.dataSource = dataSource;
 
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:NSLocalizedString(@"BUTTON_CONNECT", nil) style:UIBarButtonItemStyleDone target:self action:@selector(connectLoginDataSource)];
+    if (@available(iOS 13.0, *)) {
+        self.navigationController.navigationBar.standardAppearance = [VLCApperanceManager navigationbarAppearance];
+        self.navigationController.navigationBar.scrollEdgeAppearance = [VLCApperanceManager navigationbarAppearance];
+    }
 }
 
 - (UIStatusBarStyle)preferredStatusBarStyle

Sources/LocalNetworkConnectivity/VLCServerListViewController.m
@@ -169,7 +169,7 @@
 
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(themeDidChange) name:kVLCThemeDidChangeNotification object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(contentSizeDidChange) name:UIContentSizeCategoryDidChangeNotification object:nil];
-
+    [self themeDidChange];
     NSArray *browserClasses = @[
                                 [VLCLocalNetworkServiceBrowserUPnP class],
                                 [VLCLocalNetworkServiceBrowserPlex class],
@@ -337,6 +337,10 @@
     _localNetworkTableView.separatorColor = PresentationTheme.current.colors.background;
     _refreshControl.backgroundColor = PresentationTheme.current.colors.background;
     self.navigationController.view.backgroundColor = PresentationTheme.current.colors.background;
+    if (@available(iOS 13.0, *)) {
+        self.navigationController.navigationBar.standardAppearance = [VLCApperanceManager navigationbarAppearance];
+        self.navigationController.navigationBar.scrollEdgeAppearance = [VLCApperanceManager navigationbarAppearance];
+    }
     [self setNeedsStatusBarAppearanceUpdate];
 }
 

Sources/MediaCategories/MediaCategoryViewController.swift
@@ -365,7 +365,7 @@ extension MediaCategoryViewController {
         searchDataSource.shouldReloadFor(searchString: searchText)
         reloadData()
         if searchText.isEmpty {
-            self.searchBar.resignFirstResponder
+            self.searchBar.resignFirstResponder()
         }
     }
 }

Sources/VLCFirstStepsViewController.m
@@ -69,6 +69,10 @@
 - (void)updateTheme
 {
     self.view.backgroundColor = PresentationTheme.current.colors.background;
+    if (@available(iOS 13.0, *)) {
+        self.navigationController.navigationBar.standardAppearance = [VLCApperanceManager navigationbarAppearance];
+        self.navigationController.navigationBar.scrollEdgeAppearance = [VLCApperanceManager navigationbarAppearance];
+    }
 }
 
 - (UIStatusBarStyle)preferredStatusBarStyle

commit 9f2df21052cb56082749668a53af8134ea37a7bd

VLCTabBarCoordinator: VLCSettingsController: Set navigationBar translucency to fasle
This avoids to have a color difference between the navigationBar
and the content.

(regression from commit ea91de9f7ea028cefcad0b3f4e65badaf1d8967c)

Sources/VLCTabBarCoordinator.swift
@@ -35,6 +35,7 @@ class VLCTabBarCoordinator: NSObject {
         tabBarController.tabBar.itemPositioning = .fill
         tabBarController.viewControllers?.forEach {
             if let navController = $0 as? UINavigationController, navController.topViewController is VLCSettingsController {
+                navController.navigationBar.isTranslucent = false
                 navController.navigationBar.barTintColor = PresentationTheme.current.colors.navigationbarColor
                 navController.navigationBar.tintColor = PresentationTheme.current.colors.orangeUI
                 navController.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor:  PresentationTheme.current.colors.navigationbarTextColor]