7fd4e2b9623b85fecb7572ba64cbfd23ed88e171
Soomin Lee <bubu@mikan.io>
2019-11-28 16:44:43 +0100

    AudioCollectionModel: Filter files after deletion
    Filter files directly after a deletion without waiting for the
    medialibrary callbacks.
    
    Indeed, by waiting, it could lead to crashes due to inconsistency.

--- SharedSources/MediaLibraryModel/AudioCollectionModel.swift
+++ SharedSources/MediaLibraryModel/AudioCollectionModel.swift
@@ -32,6 +32,7 @@ extension AudioCollectionModel {
                     }
                 }
             }
+            filterFilesFromDeletion(of: items)
             medialibrary.reload()
         }
         catch let error as NSError {
