videolan/vlc-ios

commit 72815897c09876174226788f8184141da8460058

PlaybackService: Fix shuffle mode at the end of a media playback
Until now the shuffle mode only worked by pressing next.

This fixes will make sure that the shuffle mode is respected on both
cases.

Closes #753

Sources/VLCPlaybackService.m
@@ -663,13 +663,15 @@ NSString *const VLCPlaybackServicePlaybackPositionUpdated = @"VLCPlaybackService
         } break;
         case VLCMediaPlayerStateEnded:
         case VLCMediaPlayerStateStopped: {
-            [_listPlayer.mediaList lock];
-            NSUInteger listCount = _listPlayer.mediaList.count;
-            [_listPlayer.mediaList unlock];
-            if ([_listPlayer.mediaList indexOfMedia:_mediaPlayer.media] == listCount - 1 && self.repeatMode == VLCDoNotRepeat) {
+            NSInteger nextIndex = [self nextMediaIndex];
+
+            if (nextIndex == -1) {
                 _sessionWillRestart = NO;
                 [self stopPlayback];
-                return;
+            } else {
+                [_listPlayer playItemAtNumber:@(nextIndex)];
+                [[NSNotificationCenter defaultCenter]
+                 postNotificationName:VLCPlaybackServicePlaybackMetadataDidChange object:self];
             }
         } break;
         default:
@@ -716,42 +718,55 @@ NSString *const VLCPlaybackServicePlaybackPositionUpdated = @"VLCPlaybackService
     }
 }
 
-- (void)next
+- (NSInteger)nextMediaIndex
 {
+    NSInteger nextIndex = -1;
     NSInteger mediaListCount = _mediaList.count;
+    NSUInteger currentIndex = [_mediaList indexOfMedia:_listPlayer.mediaPlayer.media];
 
-#if TARGET_OS_IOS
-    if (self.repeatMode != VLCRepeatCurrentItem && mediaListCount > 2 && _shuffleMode) {
+    if (self.repeatMode == VLCRepeatCurrentItem) {
+        return currentIndex;
+    }
 
-        NSNumber *nextIndex;
-        NSUInteger currentIndex = [_mediaList indexOfMedia:_listPlayer.mediaPlayer.media];
-
+    if (_shuffleMode && mediaListCount > 2) {
         //Reached end of playlist
         if (_shuffleStack.count + 1 == mediaListCount) {
             if ([self repeatMode] == VLCDoNotRepeat)
-                return;
+                return -1;
             [_shuffleStack removeAllObjects];
         }
 
-        [_shuffleStack addObject:[NSNumber numberWithUnsignedInteger:currentIndex]];
+        [_shuffleStack addObject:@(currentIndex)];
         do {
-            nextIndex = [NSNumber numberWithUnsignedInt:arc4random_uniform((uint32_t)mediaListCount)];
-        } while (currentIndex == nextIndex.unsignedIntegerValue || [_shuffleStack containsObject:nextIndex]);
-
-        [_listPlayer playItemAtNumber:[NSNumber numberWithUnsignedInteger:nextIndex.unsignedIntegerValue]];
-        [[NSNotificationCenter defaultCenter] postNotificationName:VLCPlaybackServicePlaybackMetadataDidChange object:self];
-
-        return;
-    }
-#endif
-
-    if (mediaListCount > 1) {
-        [_listPlayer next];
-        [[NSNotificationCenter defaultCenter] postNotificationName:VLCPlaybackServicePlaybackMetadataDidChange object:self];
+            nextIndex = arc4random_uniform((uint32_t)mediaListCount);
+        } while (currentIndex == nextIndex || [_shuffleStack containsObject:@(nextIndex)]);
     } else {
+        // Normal playback
+        if (currentIndex + 1 < mediaListCount) {
+            nextIndex =  currentIndex + 1;
+        } else if ([self repeatMode] == VLCDoNotRepeat) {
+            nextIndex = -1;
+        }
+    }
+    return nextIndex;
+}
+
+- (void)next
+{
+    if (_mediaList.count == 1) {
         NSNumber *skipLength = [[NSUserDefaults standardUserDefaults] valueForKey:kVLCSettingPlaybackForwardSkipLength];
         [_mediaPlayer jumpForward:skipLength.intValue];
+        return;
     }
+
+    NSInteger nextIndex = [self nextMediaIndex];
+
+    if (nextIndex < 0) {
+        return;
+    }
+
+    [_listPlayer playItemAtNumber:@(nextIndex)];
+    [[NSNotificationCenter defaultCenter] postNotificationName:VLCPlaybackServicePlaybackMetadataDidChange object:self];
 }
 
 - (void)previous

commit 7d4459b5219902e33ac78e68d508793987f8e770

VLCPlaybackService: Fix launch media playback during playback
If a media was launched while a media was already playing, the player
would stop the playback.

Closes #766

(regression from 72815897c09876174226788f8184141da8460058)

Sources/VLCPlaybackService.m
@@ -667,8 +667,7 @@ NSString *const VLCPlaybackServicePlaybackPositionUpdated = @"VLCPlaybackService
             _sessionWillRestart = NO;
             [self stopPlayback];
         } break;
-        case VLCMediaPlayerStateEnded:
-        case VLCMediaPlayerStateStopped: {
+        case VLCMediaPlayerStateEnded: {
             NSInteger nextIndex = [self nextMediaIndex];
 
             if (nextIndex == -1) {
@@ -678,6 +677,17 @@ NSString *const VLCPlaybackServicePlaybackPositionUpdated = @"VLCPlaybackService
                 [_listPlayer playItemAtNumber:@(nextIndex)];
                 [[NSNotificationCenter defaultCenter]
                  postNotificationName:VLCPlaybackServicePlaybackMetadataDidChange object:self];
+            }
+        } break;
+        case VLCMediaPlayerStateStopped: {
+            [_listPlayer.mediaList lock];
+            NSUInteger listCount = _listPlayer.mediaList.count;
+            [_listPlayer.mediaList unlock];
+
+            if ([_listPlayer.mediaList indexOfMedia:_mediaPlayer.media] == listCount - 1
+                && self.repeatMode == VLCDoNotRepeat) {
+                _sessionWillRestart = NO;
+                [self stopPlayback];
             }
         } break;
         default: