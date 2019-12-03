ad2798bde998f1dc8a1c22cf2bded4fbb07897cb
Edgar Fouillet <vlc@edgar.fouillet.eu>
2019-12-03 16:01:15 +0100

    CloudServicesTableVC: Cosmetic: Use VLCBoxController instead of BoxSDK

--- Sources/VLCCloudServicesTableViewController.m
+++ Sources/VLCCloudServicesTableViewController.m
@@ -97,7 +97,7 @@
 {
     int i = [[VLCDropboxController sharedInstance] isAuthorized] ? 1 : 0;
     i += [[VLCGoogleDriveController sharedInstance] isAuthorized] ? 1 : 0;
-    i += [[BoxSDK sharedSDK].OAuth2Session isAuthorized] ? 1 : 0;
+    i += [[VLCBoxController sharedInstance] isAuthorized] ? 1 : 0;
     i += [[VLCOneDriveController sharedInstance] isAuthorized] ? 1 : 0;
     return i;
 }
@@ -154,7 +154,7 @@
         }
         case 2: {
             //Box
-            BOOL isAuthorized = [[BoxSDK sharedSDK].OAuth2Session isAuthorized];
+            BOOL isAuthorized = [[VLCBoxController sharedInstance] isAuthorized];
             cell.icon.image = [UIImage imageNamed:@"BoxCell"];
             cell.cloudTitle.text = @"Box";
             cell.cloudInformation.text = isAuthorized ? NSLocalizedString(@"LOGGED_IN", "") : NSLocalizedString(@"LOGIN", "");
