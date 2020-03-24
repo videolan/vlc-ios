videolan/vlc-ios

commit f6e28cd3fc044b15db53884ee08f27a330dde03e

tvOS playback: fix foreground color for tab bar items when focussed
This commit closes #808

Apple-TV/Playback/Playback Info/VLCPlaybackInfoTVViewController.m
@@ -113,7 +113,7 @@
     UITabBarItem *tabBarItemApprearance = [UITabBarItem appearanceWhenContainedInInstancesOfClasses:@[[VLCPlaybackInfoTVTabBarController class]]];
     NSDictionary *attributesSelected = @{NSForegroundColorAttributeName : [UIColor colorWithWhite:0.75 alpha:1.0]};
     [tabBarItemApprearance setTitleTextAttributes:attributesSelected forState:UIControlStateSelected];
-    NSDictionary *attributesFocused = @{NSForegroundColorAttributeName : [UIColor colorWithWhite:1.0 alpha:1.0]};
+    NSDictionary *attributesFocused = @{NSForegroundColorAttributeName : [UIColor blackColor]};
     [tabBarItemApprearance setTitleTextAttributes:attributesFocused forState:UIControlStateFocused];
 }
 

commit 02fa2db93dc529e11b11533d99295ff2e06629df

tvOS playback: restore foreground color for focussed items on tvOS < 13.0
Earlier tvOS versions are using a different tab bar with an inverted color scheme. This solves a regression introduced in f6e28cd3fc.

Apple-TV/Playback/Playback Info/VLCPlaybackInfoTVViewController.m
@@ -113,7 +113,12 @@
     UITabBarItem *tabBarItemApprearance = [UITabBarItem appearanceWhenContainedInInstancesOfClasses:@[[VLCPlaybackInfoTVTabBarController class]]];
     NSDictionary *attributesSelected = @{NSForegroundColorAttributeName : [UIColor colorWithWhite:0.75 alpha:1.0]};
     [tabBarItemApprearance setTitleTextAttributes:attributesSelected forState:UIControlStateSelected];
-    NSDictionary *attributesFocused = @{NSForegroundColorAttributeName : [UIColor blackColor]};
+    NSDictionary *attributesFocused;
+    if (@available(tvOS 13.0, *)) {
+        attributesFocused = @{NSForegroundColorAttributeName : [UIColor blackColor]};
+    } else {
+        attributesFocused = @{NSForegroundColorAttributeName : [UIColor whiteColor]};
+    }
     [tabBarItemApprearance setTitleTextAttributes:attributesFocused forState:UIControlStateFocused];
 }
 