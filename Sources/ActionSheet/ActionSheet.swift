54370f3025ea023d6f82a4bb3ba13c0c39a72510
Soomin Lee <bubu@mikan.io>
2021-07-22 15:18:07 +0200

    Revert "ActionSheet: Resolve navigation problem with actionsheet close with VoiceOver"
    This reverts commit 8fc1d4807f1b8efbb9e95878b78b163c27ae45b5.

--- Sources/ActionSheet/ActionSheet.swift
+++ Sources/ActionSheet/ActionSheet.swift
@@ -44,7 +44,6 @@ class ActionSheet: UIViewController {
         backgroundView.alpha = 0
         backgroundView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
         backgroundView.backgroundColor = UIColor.black.withAlphaComponent(0.6)
-        backgroundView.isAccessibilityElement = true
         backgroundView.addGestureRecognizer(UITapGestureRecognizer(target: self,
                                                                    action: #selector(self.removeActionSheet)))
         return backgroundView
@@ -158,6 +157,8 @@ class ActionSheet: UIViewController {
         mainStackView.addArrangedSubview(headerView)
         mainStackView.addArrangedSubview(collectionWrapperView)
 
+        backgroundView.frame = UIScreen.main.bounds
+
         setupCollectionWrapperView()
         setupMainStackViewConstraints()
         setupCollectionViewConstraints()
@@ -179,6 +180,7 @@ class ActionSheet: UIViewController {
 
         // This is to avoid a horrible visual glitch!
         mainStackView.isHidden = false
+
         let realMainStackView = mainStackView.frame
 
         mainStackView.frame.origin.y += mainStackView.frame.origin.y
@@ -192,8 +194,6 @@ class ActionSheet: UIViewController {
                         mainStackView.frame = realMainStackView
                         backgroundView.alpha = 1
         })
-        backgroundView.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: mainStackView.frame.origin.y)
-        backgroundView.setNeedsUpdateConstraints()
     }
 
     override func viewWillTransition(to size: CGSize,
