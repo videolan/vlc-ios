videolan/vlc-ios

commit 7eed6fc8ac1bdab7730fdc4583d8482e8e7daad9

SliderInfoView: Fix vertical slider and constraints for smaller heights
Closes #1158

Sources/Playback/VideoPlayer/VideoPlayerViewController.swift
@@ -957,21 +957,35 @@ private extension VideoPlayerViewController {
         setupAspectRatioContraints()
     }
 
-    private func setupBrightnessControlConstraints() {
+    private func setupCommonSliderConstraints(for slider: UIView) {
+        let heightConstraint = slider.heightAnchor.constraint(lessThanOrEqualToConstant: 170)
+        let topConstraint = slider.topAnchor.constraint(equalTo: mediaNavigationBar.bottomAnchor)
+        let bottomConstraint = slider.bottomAnchor.constraint(equalTo: scrubProgressBar.topAnchor, constant: -10)
+        let yConstraint = slider.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        heightConstraint.priority = .required
+        topConstraint.priority = .defaultHigh
+        bottomConstraint.priority = .defaultHigh
+        yConstraint.priority = .defaultHigh
         NSLayoutConstraint.activate([
-            brightnessControlView.heightAnchor.constraint(equalToConstant: 50),
-            brightnessControlView.widthAnchor.constraint(equalToConstant: 170),
-            brightnessControlView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant:-10),
-            brightnessControlView.leadingAnchor.constraint(equalTo: mainLayoutGuide.leadingAnchor, constant: -70)
+            heightConstraint,
+            topConstraint,
+            bottomConstraint,
+            slider.widthAnchor.constraint(equalToConstant: 50),
+            yConstraint,
+        ])
+    }
+
+    private func setupBrightnessControlConstraints() {
+        setupCommonSliderConstraints(for: brightnessControlView)
+        NSLayoutConstraint.activate([
+            brightnessControlView.leadingAnchor.constraint(equalTo: mainLayoutGuide.leadingAnchor)
         ])
     }
 
     private func setupVolumeControlConstraints() {
+        setupCommonSliderConstraints(for: volumeControlView)
         NSLayoutConstraint.activate([
-            volumeControlView.heightAnchor.constraint(equalToConstant:50),
-            volumeControlView.widthAnchor.constraint(equalToConstant: 170),
-            volumeControlView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant:-10),
-            volumeControlView.trailingAnchor.constraint(equalTo: mainLayoutGuide.trailingAnchor, constant: 70)
+            volumeControlView.trailingAnchor.constraint(equalTo: mainLayoutGuide.trailingAnchor)
         ])
     }
 

Sources/Playback/VideoPlayer/SliderInfoView.swift
@@ -28,8 +28,8 @@ class SliderInfoView: UIView {
 
     var iconNames: [String] = []
 
-    let levelSlider: UISlider = {
-        let levelSlider = UISlider()
+    let levelSlider: VerticalSlider = {
+        let levelSlider = VerticalSlider()
         levelSlider.tintColor = .white
         levelSlider.minimumValue = 0
         levelSlider.maximumValue = 1
@@ -64,29 +64,24 @@ class SliderInfoView: UIView {
     }
 
     override func layoutSubviews() {
-        rotateSliderView()
         addSubview(levelImageView)
         addSubview(levelSlider)
         bringSubviewToFront(levelSlider)
-        self.isUserInteractionEnabled = true
+        isUserInteractionEnabled = true
+        translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
+            levelImageView.topAnchor.constraint(equalTo: topAnchor),
             levelImageView.heightAnchor.constraint(equalToConstant: 25),
             levelImageView.widthAnchor.constraint(equalToConstant: 25),
-            levelImageView.centerYAnchor.constraint(equalTo: centerYAnchor),
-            levelImageView.rightAnchor.constraint(equalTo: rightAnchor, constant: 40)
+            levelImageView.centerXAnchor.constraint(equalTo: centerXAnchor),
         ])
 
         NSLayoutConstraint.activate([
-            levelSlider.heightAnchor.constraint(equalToConstant: 30),
-            levelSlider.widthAnchor.constraint(equalToConstant: 170),
-            levelSlider.centerYAnchor.constraint(equalTo: centerYAnchor),
+            levelSlider.topAnchor.constraint(equalTo: levelImageView.bottomAnchor, constant: 10),
+            levelSlider.bottomAnchor.constraint(equalTo: bottomAnchor),
+            levelSlider.widthAnchor.constraint(equalToConstant: 30),
             levelSlider.centerXAnchor.constraint(equalTo: centerXAnchor),
         ])
-    }
-
-    private func rotateSliderView() {
-        self.transform = CGAffineTransform(rotationAngle: .pi * -0.5)
-        self.levelImageView.transform = CGAffineTransform(rotationAngle: .pi/2)
     }
 }
 
@@ -95,7 +90,7 @@ class BrightnessControlView: SliderInfoView {
     init() {
         super.init(frame: .zero)
         if  !UIAccessibility.isVoiceOverRunning {
-            levelSlider.setThumbImage(UIImage(), for: .normal)
+            levelSlider.setThumbImage(image: UIImage(), for: .normal)
         }
         self.iconNames = ["brightnessLow", "brightnessLow", "brightnessMedium", "brightnessHigh"]
         levelSlider.addTarget(self, action: #selector(self.onLuminosityChange), for: .valueChanged)
@@ -111,12 +106,8 @@ class BrightnessControlView: SliderInfoView {
     }
 
     @objc func onLuminosityChange() {
-        UIScreen.main.brightness = CGFloat(self.levelSlider.value)
-        updateIcon(level: Float(CGFloat(self.levelSlider.value)))
-    }
-
-    private func rotateSliderView() {
-        self.transform = CGAffineTransform(rotationAngle: .pi * -0.5)
+        UIScreen.main.brightness = CGFloat(levelSlider.value)
+        updateIcon(level: levelSlider.value)
     }
 }
 
@@ -126,7 +117,7 @@ class VolumeControlView: SliderInfoView {
         super.init(frame: .zero)
         self.levelSlider.value = AVAudioSession.sharedInstance().outputVolume
         if  !UIAccessibility.isVoiceOverRunning {
-            levelSlider.setThumbImage(UIImage(), for: .normal)
+            levelSlider.setThumbImage(image: UIImage(), for: .normal)
         }
 
         levelSlider.addTarget(self, action: #selector(self.onVolumeChange), for: .valueChanged)
@@ -138,8 +129,8 @@ class VolumeControlView: SliderInfoView {
     }
 
     @objc func onVolumeChange() {
-        MPVolumeView.setVolume(Float(self.levelSlider.value))
-        updateIcon(level: Float(self.levelSlider.value))
+        MPVolumeView.setVolume(levelSlider.value)
+        updateIcon(level: levelSlider.value)
     }
 
     required init?(coder: NSCoder) {

commit 6b8ada29222e5d52b256124b54a2a3ba08ad1ea1

VideoPlayerViewController: Fix sliders gesture
Fix gesture orientation bug introduced by 7eed6fc8.

Sources/Playback/VideoPlayer/VideoPlayerViewController.swift
@@ -645,16 +645,16 @@ extension VideoPlayerViewController {
 
     @objc func handleSlideVolumePanGesture(gesture: UIPanGestureRecognizer) {
         let currentPoint = gesture.location(in: volumeControlView)
-        let percentage = currentPoint.x/volumeControlView.bounds.size.width
-        let delta = Float(percentage) *  (volumeControlView.levelSlider.maximumValue - volumeControlView.levelSlider.minimumValue)
+        let percentage = 1.0 - currentPoint.y / volumeControlView.bounds.size.height
+        let delta = Float(percentage) * (volumeControlView.levelSlider.maximumValue - volumeControlView.levelSlider.minimumValue)
         let value = volumeControlView.levelSlider.minimumValue + delta
         MPVolumeView.setVolume(value)
     }
 
     @objc func handleSlideBrightnessPanGesture(gesture: UIPanGestureRecognizer) {
         let currentPoint = gesture.location(in: brightnessControlView)
-        let percentage = currentPoint.x/brightnessControlView.bounds.size.width
-        let delta = Float(percentage) *  (brightnessControlView.levelSlider.maximumValue - brightnessControlView.levelSlider.minimumValue)
+        let percentage = 1.0 - currentPoint.y / brightnessControlView.bounds.size.height
+        let delta = Float(percentage) * (brightnessControlView.levelSlider.maximumValue - brightnessControlView.levelSlider.minimumValue)
         let value = brightnessControlView.levelSlider.minimumValue + delta
         brightnessControlView.onLuminosityChange()
         brightnessControlView.updateIcon(level: value)