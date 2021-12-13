24f9806ead6da6a747f32c2dc98412b0695a50e5
Soomin Lee <bubu@mikan.io>
2021-12-13 14:42:30 +0100

    VLCSlider: remove outdated methods

--- Sources/VLCSlider.m
+++ Sources/VLCSlider.m
@@ -22,28 +22,6 @@
     [super awakeFromNib];
 }
 
-- (CGRect)thumbRectForBounds:(CGRect)bounds trackRect:(CGRect)rect value:(float)value
-{
-    // By default do not set padding for small devices such as ipod.
-    CGFloat padding = 0;
-
-    // Add paddingg only to recent OS, older versions has a visual glitch on the thumb
-    if (@available(iOS 13, *)) {
-        // Add padding to safe area devices.
-        if ([UIDevice VLCDeviceHasSafeArea]) {
-            padding = -5;
-        }
-
-        // Add even more padding to ipads.
-        if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
-            padding = -10;
-        }
-    }
-
-    return CGRectInset([super thumbRectForBounds:bounds trackRect:rect value:value],
-                       padding , padding);
-}
-
 @end
 
 
