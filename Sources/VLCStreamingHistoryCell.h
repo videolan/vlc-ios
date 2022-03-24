4e3462d5c9c00e13c5daae9274bf9c667ce68793
Felix Paul Kühne <felix@feepk.net>
2022-03-24 14:36:56 +0100

    network streams: fix renaming streams
    This fundamentally reverts 3b47b089 and introduces a properly designated class. Additionally, this correctly reloads the view now if the user is not logged in to iCloud.
    
    (cherry picked from commit dcf7c2fd63b00e3a58ecabb08f99e2ef56f67759)

--- Sources/VLCStreamingHistoryCell.h
+++ Sources/VLCStreamingHistoryCell.h
@@ -2,24 +2,31 @@
  * VLCStreamingHistoryCell.h
  * VLC for iOS
  *****************************************************************************
- * Copyright (c) 2016 VideoLAN. All rights reserved.
+ * Copyright (c) 2016-2022 VideoLAN. All rights reserved.
  * $Id$
  *
  * Authors: Adam Viaud <mcnight # mcnight.fr>
+ *          Felix Paul Kühne <fkuehne # videolan.org>
  *
  * Refer to the COPYING file of the official project for license.
  *****************************************************************************/
 
 #import <UIKit/UIKit.h>
-#import "VLCNetworkListCell.h"
 
 @protocol VLCStreamingHistoryCellMenuItemProtocol
 - (void)renameStreamFromCell:(UITableViewCell *)cell;
 @end
 
-@interface VLCStreamingHistoryCell : VLCNetworkListCell
+@interface VLCStreamingHistoryCell : UITableViewCell
+
+@property (nonatomic, strong) IBOutlet UILabel *titleLabel;
+@property (nonatomic, strong) IBOutlet UILabel *subtitleLabel;
+@property (nonatomic, strong) IBOutlet UIImageView *thumbnailView;
 
 @property (weak, nonatomic) id<VLCStreamingHistoryCellMenuItemProtocol> delegate;
+
++ (VLCStreamingHistoryCell *)cellWithReuseIdentifier:(NSString *)ident;
++ (CGFloat)heightOfCell;
 
 - (void)renameStream:(id)sender;
 - (void)customizeAppearance;
--- Sources/VLCStreamingHistoryCell.m
+++ Sources/VLCStreamingHistoryCell.m
@@ -6,13 +6,59 @@
  * $Id$
  *
  * Authors: Adam Viaud <mcnight # mcnight.fr>
+ *          Felix Paul Kühne <fkuehne # videolan.org>
  *
  * Refer to the COPYING file of the official project for license.
  *****************************************************************************/
 
 #import "VLCStreamingHistoryCell.h"
+#import "VLC-Swift.h"
 
 @implementation VLCStreamingHistoryCell
+
++ (VLCStreamingHistoryCell *)cellWithReuseIdentifier:(NSString *)ident
+{
+    NSArray *nibContentArray = [[NSBundle mainBundle] loadNibNamed:@"VLCStreamingHistoryCell" owner:nil options:nil];
+    NSAssert([nibContentArray count] == 1, @"meh");
+    NSAssert([[nibContentArray lastObject] isKindOfClass:[VLCStreamingHistoryCell class]], @"meh meh");
+    VLCStreamingHistoryCell *cell = (VLCStreamingHistoryCell *)[nibContentArray lastObject];
+
+    return cell;
+}
+
+- (void)awakeFromNib
+{
+    self.titleLabel.text = @"";
+    self.subtitleLabel.text = @"";
+    self.thumbnailView.contentMode = UIViewContentModeScaleAspectFit;
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(themeDidChange)
+                                                 name:kVLCThemeDidChangeNotification object:nil];
+    // If a tableViewCell is highlighted, one needs to manualy set the opaque property
+    if (@available(iOS 13.0, *)) {
+        self.opaque = NO;
+    }
+    [self themeDidChange];
+    [super awakeFromNib];
+}
+
+- (void)themeDidChange
+{
+    ColorPalette *colors = PresentationTheme.current.colors;
+    self.titleLabel.textColor = colors.cellTextColor;
+    self.subtitleLabel.textColor = colors.cellDetailTextColor;
+    self.titleLabel.highlightedTextColor = colors.cellTextColor;
+    self.subtitleLabel.highlightedTextColor = colors.cellDetailTextColor;
+
+    UIColor *backgroundColor = colors.background;
+
+    if (@available(iOS 13.0, *)) {
+        backgroundColor = UIColor.clearColor;
+    }
+
+    self.backgroundColor = backgroundColor;
+    self.titleLabel.backgroundColor = backgroundColor;
+    self.subtitleLabel.backgroundColor = backgroundColor;
+}
 
 - (BOOL)canBecomeFirstResponder {
     return YES;
@@ -29,6 +75,14 @@
 
 - (void)renameStream:(id)sender {
     [self.delegate renameStreamFromCell:self];
+}
+
++ (CGFloat)heightOfCell
+{
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad)
+        return 80.;
+
+    return 68.;
 }
 
 @end
