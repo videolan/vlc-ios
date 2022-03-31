videolan/vlc-ios

commit c02bd8d63cac513dc8ad913ef74c6fa813f1ee3f

Update VLCMediaLibraryKit to latest beta and to the new album track API

SharedSources/MediaLibraryModel/MediaModel.swift
@@ -109,15 +109,15 @@ extension VLCMLMedia {
                 attributeSet.audioSampleRate = NSNumber(value: track.sampleRate())
             }
         }
-        if let albumTrack = albumTrack {
-            if let genre = albumTrack.genre {
+        if subtype() == .albumTrack {
+            if let genre = genre {
                 attributeSet.genre = genre.name
             }
-            if let artist = albumTrack.artist {
+            if let artist = artist {
                 attributeSet.artist = artist.name
             }
-            attributeSet.audioTrackNumber = NSNumber(value:albumTrack.trackNumber())
-            if let album = albumTrack.album {
+            attributeSet.audioTrackNumber = NSNumber(value:trackNumber)
+            if let album = album {
                 attributeSet.artist = album.title
             }
         }
@@ -178,15 +178,25 @@ extension VLCMLMedia {
 // MARK: - Search
 extension VLCMLMedia: SearchableMLModel {
     func contains(_ searchString: String) -> Bool {
-        return title.lowercased().contains(searchString)
+        var matches = false
+
+        if subtype() == .albumTrack {
+            matches = matches || artist?.contains(searchString) ?? false
+            matches = matches || genre?.contains(searchString) ?? false
+            matches = matches || album?.contains(searchString) ?? false
+        }
+
+        matches = matches || title.lowercased().contains(searchString)
+
+        return matches
     }
 }
 
 extension VLCMLMedia {
     func albumTrackArtistName() -> String {
-        guard let albumTrack = albumTrack else {
+        guard let albumTrackName = artist?.name else {
             return NSLocalizedString("UNKNOWN_ARTIST", comment: "")
         }
-        return albumTrack.albumArtistName()
+        return albumTrackName
     }
 }

Podfile
@@ -20,7 +20,7 @@ target 'VLC-iOS' do
   pod 'InAppSettingsKit', :git => 'https://github.com/Mikanbu/InAppSettingsKit.git', :commit => 'a429840' #tvOS fix
   pod 'GoogleAPIClientForREST/Drive'
   pod 'MobileVLCKit', '3.3.18b12'
-  pod 'VLCMediaLibraryKit', '0.10.1'
+  pod 'VLCMediaLibraryKit', '0.11.0b1'
   pod 'GTMAppAuth', '0.7.1'
   pod 'ADAL', :git => 'https://code.videolan.org/fkuehne/azure-activedirectory-library-for-objc.git', :commit => '348e94df'
   pod 'OneDriveSDK', :git => 'https://code.videolan.org/fkuehne/onedrive-sdk-ios.git', :commit => '810f82da'

SharedSources/MediaLibraryModel/AlbumModel.swift
@@ -64,16 +64,6 @@ extension VLCMLAlbum: SearchableMLModel {
     }
 }
 
-extension VLCMLAlbumTrack: SearchableMLModel {
-    func contains(_ searchString: String) -> Bool {
-        var matches = false
-        matches = matches || artist?.contains(searchString) ?? false
-        matches = matches || genre?.contains(searchString) ?? false
-        matches = matches || album?.contains(searchString) ?? false
-        return matches
-    }
-}
-
 // MARK: - MediaLibraryObserver
 
 extension AlbumModel: MediaLibraryObserver {
@@ -155,14 +145,5 @@ extension VLCMLAlbum {
             return albumName() + " " + albumArtistName() + " " + numberOfTracksString()
         }
         return albumName() + " " + albumArtistName()
-    }
-}
-
-extension VLCMLAlbumTrack {
-    func albumArtistName() -> String {
-        guard let artist = artist else {
-            return NSLocalizedString("UNKNOWN_ARTIST", comment: "")
-        }
-        return artist.artistName()
     }
 }

SharedSources/VLCMetadata.m
@@ -44,9 +44,9 @@
 {
     if (media && !media.isExternalMedia) {
         self.title = media.title;
-        self.artist = media.albumTrack.artist.name;
-        self.trackNumber = @(media.albumTrack.trackNumber);
-        self.albumName = media.albumTrack.album.title;
+        self.artist = media.artist.name;
+        self.trackNumber = @(media.trackNumber);
+        self.albumName = media.album.title;
         self.artworkImage = [media thumbnailImage];
         self.isAudioOnly = [media subtype] == VLCMLMediaSubtypeAlbumTrack;
     } else { // We're streaming something

Sources/CollectionViewCellPreviewController.swift
@@ -141,7 +141,7 @@ extension CollectionViewCellPreviewController {
             if media.subtype() == .albumTrack {
                 var description: [String] = [media.albumTrackArtistName()]
 
-                if let album = media.albumTrack?.album, !album.title.isEmpty {
+                if let album = media.album, !album.title.isEmpty {
                     description.append(album.title)
                 }
                 infos.append(PreviewInformation(value: description.joined(separator: listSeparator),

Sources/MediaCategoryCells/MediaCollectionViewCell.swift
@@ -278,7 +278,7 @@ class MediaCollectionViewCell: BaseCollectionViewCell, UIScrollViewDelegate {
         titleLabel.text = audiotrack.title()
         accessibilityLabel = audiotrack.accessibilityText(editing: false)
         var descriptionText = audiotrack.albumTrackArtistName()
-        if let albumTitle = audiotrack.albumTrack?.album?.title, !albumTitle.isEmpty {
+        if let albumTitle = audiotrack.album?.title, !albumTitle.isEmpty {
             descriptionText += " · " + albumTitle
         }
         if isEditing {

Sources/MediaCategoryCells/MediaGridCollectionCell.swift
@@ -261,7 +261,7 @@ class MediaGridCollectionCell: BaseCollectionViewCell {
         titleLabel.text = audioTrack.title()
         accessibilityLabel = audioTrack.accessibilityText(editing: false)
         var descriptionText = audioTrack.albumTrackArtistName()
-        if let albumTitle = audioTrack.albumTrack?.album?.title, !albumTitle.isEmpty {
+        if let albumTitle = audioTrack.album?.title, !albumTitle.isEmpty {
             descriptionText += " - " + albumTitle
         }
         descriptionLabel.text = descriptionText

commit 089855c0f9c1bfb3a9cf52dc3c3353f34add1124

MediaModel: artist: albumTracks: fix unknown artists name
regression from: c02bd8d63cac513dc8ad913ef74c6fa813f1ee3f

SharedSources/MediaLibraryModel/MediaModel.swift
@@ -190,9 +190,9 @@ extension VLCMLMedia: SearchableMLModel {
 
 extension VLCMLMedia {
     func albumTrackArtistName() -> String {
-        guard let albumTrackName = artist?.name else {
+        guard let artist = artist, artist.identifier() != UnknownArtistID else {
             return NSLocalizedString("UNKNOWN_ARTIST", comment: "")
         }
-        return albumTrackName
+        return artist.name
     }
 }