c92c47edc7523ed4354f5182f0f24aabee0514c5
Felix Paul Kühne <felix@feepk.net>
2022-04-07 16:28:39 +0200

    Apple TV: split media management code
    This moves all the media management code to a designated class that is
    accessible is independent from the existence of a view controller.
    
    There are no functional changes besides that all locally stored files
    will now play in a row which was the original design intention anyway.
    
    (cherry picked from commit 609f21caf3145bbd545895f7a9a8c9ac934d3d92)

---
+++ Apple-TV/VLCMicroMediaLibraryService.h
@@ -0,0 +1,43 @@
+/*****************************************************************************
+ * VLC for iOS
+ *****************************************************************************
+ * Copyright (c) 2022 VideoLAN. All rights reserved.
+ * $Id$
+ *
+ * Authors: Felix Paul Kühne <fkuehne # videolan.org>
+ *
+ * Refer to the COPYING file of the official project for license.
+ *****************************************************************************/
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@class VLCMicroMediaLibraryService;
+
+@protocol VLCMicroMediaLibraryServiceDelegate <NSObject>
+
+@required
+- (void)mediaListUpdatedForService:(VLCMicroMediaLibraryService *)service;
+
+@end
+
+@interface VLCMicroMediaLibraryService : NSObject
+
++ (instancetype)sharedInstance;
+
+@property (readwrite, weak) id<VLCMicroMediaLibraryServiceDelegate> delegate;
+@property (readonly) NSInteger numberOfDiscoveredMedia;
+@property (readonly, copy) VLCMediaList *mediaList;
+
+- (void)updateMediaList;
+- (NSString *)filenameOfItemAtIndex:(NSInteger)index;
+- (void)deleteFileAtIndex:(NSInteger)index;
+
+- (NSURL *)thumbnailURLForItemAtIndex:(NSInteger)index;
+- (NSString *)titleForItemAtIndex:(NSInteger)index;
+- (UIImage *)placeholderImageForItemWithTitle:(NSString *)title;
+
+@end
+
+NS_ASSUME_NONNULL_END
