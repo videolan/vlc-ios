0dae0c58d48a94e337d7ce9f7abeb45ece902999
Felix Paul Kühne <felix@feepk.net>
2022-04-22 20:50:34 +0200

    EqualizerPresetSelector: rewrite profile selector using UITableView
    The previous implementation was using a mix of UIScrollView and
    UIStackView as well as 18 buttons show a list of options.
    
    This improves rendering speed, memory use and reduces LoC by 50%.

--- Sources/EqualizerPresetSelector.swift
+++ Sources/EqualizerPresetSelector.swift
@@ -10,57 +10,17 @@
 
 import UIKit
 
-private class EqualizerPresetButton: UIButton {
-    let id: Int
-    private let title: String
-
-    // MARK: - Init
-    init(id: Int, title: String) {
-        self.id = id
-        self.title = title
-        super.init(frame: .zero)
-        setupViews()
-    }
-
-    required init?(coder: NSCoder) {
-        self.id = -1
-        self.title = ""
-        super.init(coder: coder)
-        setupViews()
-    }
-
-    private func setupViews() {
-        translatesAutoresizingMaskIntoConstraints = false
-        setTitle(title, for: .normal)
-        titleLabel?.font = .boldSystemFont(ofSize: 16)
-        setTitleColor(PresentationTheme.current.colors.orangeUI, for: .normal)
-    }
-}
-
-private class EqualizerPresetButtonsFactory {
-    static func generate(with profiles: [NSString]) -> [EqualizerPresetButton] {
-        var buttons: [EqualizerPresetButton] = []
-
-        for (i, profile) in profiles.enumerated() {
-            let button = EqualizerPresetButton(id: i, title: String(profile))
-            buttons.append(button)
-        }
-
-        return buttons
-    }
-}
-
-class EqualizerPresetSelector: SpoilerButton {
+class EqualizerPresetSelector: SpoilerButton, UITableViewDataSource, UITableViewDelegate {
     // MARK: - Properties
     private let hiddenView = UIView()
     private let preampLabel = UILabel()
     private let preampSlider = UISlider()
     private let preampValueLabel = UILabel()
     private let preampStackView = UIStackView()
-    private let presetsScrollView = UIScrollView()
-    private let presetsStackView = UIStackView()
+    private let presetsTableView = UITableView()
     private let profiles: [NSString]
     var delegate: EqualizerPresetSelectorDelegate?
+    private let presetTableViewReuseIdentifier = "presetTableViewReuseIdentifier"
 
     // MARK: - Init
     required init(coder: NSCoder) {
@@ -101,7 +61,6 @@ class EqualizerPresetSelector: SpoilerButton {
         preampSlider.translatesAutoresizingMaskIntoConstraints = false
         preampSlider.addTarget(self, action: #selector(preampSliderDidChangeValue), for: .valueChanged)
 
-
         preampValueLabel.text = "\(Float(Int(preampSlider.value * 100)) / 100.0)dB"
         preampValueLabel.setContentCompressionResistancePriority(.required, for: .horizontal)
         preampValueLabel.setContentHuggingPriority(.required, for: .horizontal)
@@ -109,67 +68,66 @@ class EqualizerPresetSelector: SpoilerButton {
         preampValueLabel.textColor = PresentationTheme.darkTheme.colors.cellTextColor
     }
 
-    private func setupStackView() {
-        presetsStackView.axis = .vertical
-        presetsStackView.alignment = .top
-        presetsStackView.distribution = .fill
-        presetsStackView.spacing = 0
-        presetsStackView.translatesAutoresizingMaskIntoConstraints = false
-
-        let buttons = EqualizerPresetButtonsFactory.generate(with: profiles)
-        for button in buttons {
-            presetsStackView.addArrangedSubview(button)
-            button.addTarget(self, action: #selector(didSelectPreset(sender:)), for: .touchUpInside)
-            button.widthAnchor.constraint(equalTo: presetsStackView.widthAnchor).isActive = true
-        }
-    }
-
-    private func setupScrollView() {
-        presetsScrollView.translatesAutoresizingMaskIntoConstraints = false
-        presetsScrollView.indicatorStyle = .white
-        presetsScrollView.addSubview(presetsStackView)
-
-        let newConstraints = [
-            presetsStackView.topAnchor.constraint(equalTo: presetsScrollView.topAnchor),
-            presetsStackView.bottomAnchor.constraint(equalTo: presetsScrollView.bottomAnchor),
-            presetsStackView.widthAnchor.constraint(equalTo: presetsScrollView.widthAnchor),
-            presetsStackView.centerXAnchor.constraint(equalTo: presetsScrollView.centerXAnchor)
-        ]
-        NSLayoutConstraint.activate(newConstraints)
+    private func setupTableView() {
+        presetsTableView.translatesAutoresizingMaskIntoConstraints = false
+        presetsTableView.backgroundColor = PresentationTheme.darkTheme.colors.background
+        presetsTableView.dataSource = self
+        presetsTableView.delegate = self
     }
 
     private func setupViews() {
         setupPreampViews()
-        setupStackView()
-        setupScrollView()
+        setupTableView()
 
         hiddenView.translatesAutoresizingMaskIntoConstraints = false
         hiddenView.addSubview(preampStackView)
-        hiddenView.addSubview(presetsScrollView)
+        hiddenView.addSubview(presetsTableView)
 
         let newConstraints = [
             preampStackView.topAnchor.constraint(equalTo: hiddenView.topAnchor),
             preampStackView.leadingAnchor.constraint(equalTo: hiddenView.leadingAnchor),
             preampStackView.trailingAnchor.constraint(equalTo: hiddenView.trailingAnchor),
-            presetsScrollView.topAnchor.constraint(equalTo: preampStackView.bottomAnchor, constant: 10),
-            presetsScrollView.leadingAnchor.constraint(equalTo: hiddenView.leadingAnchor),
-            presetsScrollView.trailingAnchor.constraint(equalTo: hiddenView.trailingAnchor),
-            presetsScrollView.bottomAnchor.constraint(equalTo: hiddenView.bottomAnchor)
+            presetsTableView.topAnchor.constraint(equalTo: preampStackView.bottomAnchor, constant: 10),
+            presetsTableView.leadingAnchor.constraint(equalTo: hiddenView.leadingAnchor),
+            presetsTableView.trailingAnchor.constraint(equalTo: hiddenView.trailingAnchor),
+            presetsTableView.bottomAnchor.constraint(equalTo: hiddenView.bottomAnchor)
         ]
         NSLayoutConstraint.activate(newConstraints)
 
         setTitle(NSLocalizedString("PRESET_PREAMP_SETTING", comment: ""))
         setHiddenView(with: hiddenView)
+
+        presetsTableView.reloadData()
+    }
+
+    // MARK: - table view data source
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return profiles.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell.init(style: UITableViewCell.CellStyle.value1, reuseIdentifier: presetTableViewReuseIdentifier)
+
+        let colors = PresentationTheme.darkTheme.colors
+        cell.textLabel?.text = String(profiles[indexPath.row])
+        cell.textLabel?.textColor = colors.orangeUI
+        cell.textLabel?.font = .boldSystemFont(ofSize: 16)
+        cell.backgroundColor = colors.cellBackgroundA
+        cell.selectionStyle = UITableViewCell.SelectionStyle.none
+
+        return cell
+    }
+
+    // MARK: - table view delegate
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: false)
+        delegate?.equalizerPresetSelector(self, didSelectPreset: indexPath.row)
+        toggleHiddenView()
     }
 
     @objc func preampSliderDidChangeValue(sender: UISlider) {
         delegate?.equalizerPresetSelector(self, didSetPreamp: sender.value)
         preampValueLabel.text = "\(Float(Int(preampSlider.value * 100)) / 100.0)dB"
-    }
-
-    @objc private func didSelectPreset(sender: EqualizerPresetButton) {
-        delegate?.equalizerPresetSelector(self, didSelectPreset: sender.id)
-        toggleHiddenView()
     }
 
     // MARK: - Public
