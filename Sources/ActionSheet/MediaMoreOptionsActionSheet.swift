93ead0670e916a3b73bdaa1ddc4019867a6389ca
Felix Paul Kühne <felix@feepk.net>
2022-04-28 09:55:16 +0200

    Action Sheet: rename variable to be non-ambigous
    (cherry picked from commit 47a8e3b114ab910cc2b7f8a6f37e210104301c53)

--- Sources/ActionSheet/MediaMoreOptionsActionSheet.swift
+++ Sources/ActionSheet/MediaMoreOptionsActionSheet.swift
@@ -87,7 +87,7 @@ protocol MediaMoreOptionsActionSheetDelegate {
     }()
 
 // MARK: - Playback Speed View
-    private lazy var playbackView: PlaybackSpeedView = {
+    private lazy var playbackSpeedView: PlaybackSpeedView = {
         let playbackSpeedView = Bundle.main.loadNibNamed("PlaybackSpeedView",
                                                          owner: nil,
                                                          options: nil)?.first as! PlaybackSpeedView
@@ -128,7 +128,7 @@ protocol MediaMoreOptionsActionSheetDelegate {
     }
 
     func resetPlaybackSpeed() {
-        playbackView.reset()
+        playbackSpeedView.reset()
     }
 
     func resetEqualizer() {
@@ -145,7 +145,7 @@ protocol MediaMoreOptionsActionSheetDelegate {
 
     func updateThemes() {
         videoFiltersView.setupTheme()
-        playbackView.setupTheme()
+        playbackSpeedView.setupTheme()
         sleepTimerView.setupTheme()
         equalizerView.setupTheme()
         chapterView.setupTheme()
@@ -201,7 +201,7 @@ protocol MediaMoreOptionsActionSheetDelegate {
 
     func resetOptionsIfNecessary() {
         // FIXME: Reset Equalizer if needed
-        playbackView.resetSlidersIfNeeded()
+        playbackSpeedView.resetSlidersIfNeeded()
         updateThemes()
     }
 
@@ -210,7 +210,7 @@ protocol MediaMoreOptionsActionSheetDelegate {
         case .filter:
             openOptionView(videoFiltersView)
         case .playback:
-            openOptionView(playbackView)
+            openOptionView(playbackSpeedView)
         case .sleepTimer:
             openOptionView(sleepTimerView)
         case .equalizer:
@@ -319,7 +319,7 @@ extension MediaMoreOptionsActionSheet: MediaPlayerActionSheetDataSource {
         case .filter:
             return videoFiltersView
         case .playback:
-            return playbackView
+            return playbackSpeedView
         case .sleepTimer:
             return sleepTimerView
         case .equalizer:
