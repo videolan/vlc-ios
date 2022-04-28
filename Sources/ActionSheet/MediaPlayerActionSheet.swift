46b09d32dcb1e53c990e3bcd18e10cdc5728df6f
Felix Paul Kühne <felix@feepk.net>
2022-04-28 11:11:04 +0200

    MediaPlayerActionSheet: don't set a dark background for playback speed
    When manipulating the audio and subtitles delays, it is important to
    see the video to get it right.

--- Sources/ActionSheet/MediaPlayerActionSheet.swift
+++ Sources/ActionSheet/MediaPlayerActionSheet.swift
@@ -114,7 +114,7 @@ class MediaPlayerActionSheet: ActionSheet {
             (completed) in
             child.addGestureRecognizer(self.leftToRightGesture)
             self.currentChildView = child
-            if child is VideoFiltersView {
+            if child is VideoFiltersView || child is PlaybackSpeedView {
                 self.changeBackground(alpha: 0)
             }
 
