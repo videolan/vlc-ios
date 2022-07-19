6e16f05e828543fa3fb653c5a7aafdc20cb892de
Felix Paul Kühne <felix@feepk.net>
2022-07-19 18:29:29 +0200

    plex: fix browsing servers discovered using bonjour
    We resolve the hostname for the parser now.
    
    (cherry picked from commit ee6ea05f9ce404e12eca522266b033526f16c5d4)

--- SharedSources/ServerBrowsing/Plex/VLCLocalNetworkServiceBrowserPlex.m
+++ SharedSources/ServerBrowsing/Plex/VLCLocalNetworkServiceBrowserPlex.m
@@ -2,16 +2,18 @@
  * VLCLocalNetworkServiceBrowserPlex.m
  * VLC for iOS
  *****************************************************************************
- * Copyright (c) 2015-2019 VideoLAN. All rights reserved.
+ * Copyright (c) 2015-2019, 2022 VideoLAN. All rights reserved.
  * $Id$
  *
  * Authors: Tobias Conradi <videolan # tobias-conradi.de>
+ *          Felix Paul Kühne <fkuehne # videolan.org>
  *
  * Refer to the COPYING file of the official project for license.
  *****************************************************************************/
 
 #import "VLCLocalNetworkServiceBrowserPlex.h"
 #import "VLCNetworkServerBrowserPlex.h"
+#import <arpa/inet.h>
 
 @implementation VLCLocalNetworkServiceBrowserPlex
 
@@ -51,6 +53,44 @@ NSString *const VLCNetworkServerProtocolIdentifierPlex = @"plex";
     return login;
 }
 #else
+
+static NSString * ipAddressAsStringForData(NSData * data)
+{
+    char addressBuffer[INET6_ADDRSTRLEN] = { 0 };
+    NSString *returnValue = nil;
+
+    if (data == nil) {
+        return returnValue;
+    }
+
+    typedef union {
+        struct sockaddr sa;
+        struct sockaddr_in ipv4;
+        struct sockaddr_in6 ipv6;
+    } ip_socket_address;
+
+    ip_socket_address *socketAddress = (ip_socket_address *)[data bytes];
+
+    if (socketAddress) {
+        const char *addressStr = NULL;
+        if (socketAddress->sa.sa_family == AF_INET) {
+            addressStr = inet_ntop(socketAddress->sa.sa_family,
+                                           (void *)&(socketAddress->ipv4.sin_addr),
+                                           addressBuffer,
+                                           sizeof(addressBuffer));
+        } else if (socketAddress->sa.sa_family == AF_INET6) {
+            addressStr = inet_ntop(socketAddress->sa.sa_family,
+                                           (void *)&(socketAddress->ipv6.sin6_addr),
+                                           addressBuffer,
+                                           sizeof(addressBuffer));
+        }
+        if (addressStr != NULL) {
+            returnValue = [NSString stringWithUTF8String:addressStr];
+        }
+    }
+    return returnValue;
+}
+
 - (id<VLCNetworkServerBrowser>)serverBrowser
 {
     NSNetService *service = self.netService;
@@ -62,7 +102,10 @@ NSString *const VLCNetworkServerProtocolIdentifierPlex = @"plex";
     NSString *hostName = service.hostName;
     NSUInteger portNum = service.port;
 
-    VLCNetworkServerBrowserPlex *serverBrowser = [[VLCNetworkServerBrowserPlex alloc] initWithName:name host:hostName portNumber:@(portNum) path:@"" authentificication:@""];
+    VLCNetworkServerBrowserPlex *serverBrowser = [[VLCNetworkServerBrowserPlex alloc] initWithName:name host:ipAddressAsStringForData(service.addresses.firstObject)
+                                                                                        portNumber:@(portNum)
+                                                                                              path:@""
+                                                                                authentificication:@""];
 
     return serverBrowser;
 }
