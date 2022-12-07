524eb3bc9d92620a8bdfae8ff0d7ac650e2b723b
Diogo Simao Marques <dogo@videolabs.io>
2022-12-07 13:09:42 +0100

    GoogleSignInAuthenticator: Request additional scope when signing in
    Fix the missing drive scope when authenticating to Google Drive by
    requesting it along with the signIn function call.
    
    Closes #1440

--- Sources/Cloud/Services/Google Drive/GoogleSignInAuthenticator.swift
+++ Sources/Cloud/Services/Google Drive/GoogleSignInAuthenticator.swift
@@ -4,7 +4,7 @@
  * Copyright © 2022 VLC authors and VideoLAN
  * Copyright © 2022 Videolabs
  *
- * Authors: Diogo Simao Marques <diogo.simaomarquespro@gmail.com>
+ * Authors: Diogo Simao Marques <dogo@videolabs.io>
  *
  * Refer to the COPYING file of the official project for license.
  *****************************************************************************/
@@ -21,9 +21,10 @@ class GoogleSignInAuthenticator: NSObject {
     @objc class func signIn(_ signIn: GIDSignIn, presentingView: VLCGoogleDriveTableViewController) {
         let configuration = GIDConfiguration(clientID: kVLCGoogleDriveClientID)
 
-        signIn.addScopes([kGTLRAuthScopeDrive], presenting: presentingView, callback: nil)
-
-        signIn.signIn(with: configuration, presenting: presentingView, callback: { user, error in
+        signIn.signIn(with: configuration,
+                      presenting: presentingView,
+                      hint: nil,
+                      additionalScopes: [kGTLRAuthScopeDrive]) { user, error in
             if error != nil {
                 return
             }
@@ -33,6 +34,6 @@ class GoogleSignInAuthenticator: NSObject {
                grantedScopes.contains(kGTLRAuthScopeDrive) {
                 presentingView.setAuthorizerAndUpdate()
             }
-        })
+        }
     }
 }
