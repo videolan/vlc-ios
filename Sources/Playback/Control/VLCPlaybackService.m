videolan/vlc-ios

commit 98a6b8a200f95c0c9c9020932bd061344063a41c

playback service: update metadata on the main thread
Moving to the background was a nice idea but does not work out as too
many aspects are not thread safe.

Sources/Playback/Control/VLCPlaybackService.m
@@ -59,7 +59,6 @@ NSString *const VLCPlaybackServicePlaybackPositionUpdated = @"VLCPlaybackService
     UIView *_actualVideoOutputView;
     UIView *_preBackgroundWrapperView;
 
-    volatile atomic_bool _metadataUpdatePending;
     BOOL _mediaWasJustStarted;
     int _majorPositionChangeInProgress;
     BOOL _recheckForExistingThumbnail;
@@ -1343,21 +1342,12 @@ NSString *const VLCPlaybackServicePlaybackPositionUpdated = @"VLCPlaybackService
 
 - (void)setNeedsMetadataUpdate
 {
-    if (!atomic_load(&_metadataUpdatePending)) {
-        atomic_store(&_metadataUpdatePending, true);
-        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
-            atomic_store(&self->_metadataUpdatePending, false);
 #if TARGET_OS_IOS
-            VLCMLMedia *media = self->_mediaPlayer.media ? [self->_delegate mediaForPlayingMedia:self->_mediaPlayer.media] : nil;
-            [self->_metadata updateMetadataFromMedia:media mediaPlayer:self->_mediaPlayer];
+    VLCMLMedia *media = self->_mediaPlayer.media ? [self->_delegate mediaForPlayingMedia:self->_mediaPlayer.media] : nil;
+    [_metadata updateMetadataFromMedia:media mediaPlayer:_mediaPlayer];
 #else
-            [self->_metadata updateMetadataFromMediaPlayer:self->_mediaPlayer];
+    [_metadata updateMetadataFromMediaPlayer:_mediaPlayer];
 #endif
-            dispatch_async(dispatch_get_main_queue(), ^{
-                [self recoverDisplayedMetadata];
-            });
-        });
-    }
 }
 
 #if TARGET_OS_IOS

Sources/Playback/OS Integration/VLCMetadata.m
@@ -113,9 +113,8 @@
         self.elapsedPlaybackTime = @(elapsedPlaybackTime.value.floatValue / 1000.);
     }
     self.position = @(mediaPlayer.position);
-    dispatch_async(dispatch_get_main_queue(), ^{
-        [[NSNotificationCenter defaultCenter] postNotificationName:VLCPlaybackServicePlaybackMetadataDidChange object:self];
-    });
+
+    [[NSNotificationCenter defaultCenter] postNotificationName:VLCPlaybackServicePlaybackMetadataDidChange object:self];
 }
 
 - (void)checkIsAudioOnly:(VLCMediaPlayer *)mediaPlayer

commit d956fc67eeaff16a3fb8c0b4aaa48ec755a18be1

VPS: propagate updated metadata when available
This fixes a regression from 98a6b8a2 and thereby #1504

Sources/Playback/Control/VLCPlaybackService.m
@@ -1347,6 +1347,8 @@ NSString *const VLCPlaybackServicePlaybackPositionUpdated = @"VLCPlaybackService
 #else
     [_metadata updateMetadataFromMediaPlayer:_mediaPlayer];
 #endif
+
+    [self recoverDisplayedMetadata];
 }
 
 #if TARGET_OS_IOS