videolan/vlc-ios

commit c360a0c2dc7f8e637f1ffe47416e8e4a3c48b5cc

MediaCollectionViewCell: listen to playback state
This partially fixes #1505

Sources/Media Library/MediaCategoryCells/MediaCollectionViewCell.swift
@@ -271,10 +271,6 @@ class MediaCollectionViewCell: BaseCollectionViewCell, UIScrollViewDelegate {
             descriptionText += " · " + albumTitle
         }
 
-        let orangeColor: UIColor = PresentationTheme.current.colors.orangeUI
-        let playingAnimation = generateAnimation(with: "playing-animation-", color: orangeColor)
-        let pauseAnimation = generateAnimation(with: "pause-animation-", color: orangeColor)
-
         if let currentMedia = playbackService.currentlyPlayingMedia,
            let audiotrackURL = audiotrack.mainFile()?.mrl,
            let currentMediaURL = currentMedia.url,
@@ -283,28 +279,18 @@ class MediaCollectionViewCell: BaseCollectionViewCell, UIScrollViewDelegate {
 
             // Animate the current played cell
             if #available(iOS 13.0, *) {
-                if playbackService.isPlaying {
-                    thumbnailView.animationImages = playingAnimation
-                    thumbnailView.animationDuration = 1.2
-                    thumbnailView.animationRepeatCount = 0
-                    thumbnailView.image = thumbnailView.animationImages?.first
-                    thumbnailView.startAnimating()
-                } else {
-                    thumbnailView.stopAnimating()
-                    thumbnailView.animationImages = pauseAnimation
-                    thumbnailView.animationDuration = 0.8
-                    thumbnailView.animationRepeatCount = 1
-                    thumbnailView.image = thumbnailView.animationImages?.first
-                    thumbnailView.startAnimating()
-                    thumbnailView.image = pauseAnimation.last
-                }
-
+                NotificationCenter.default.addObserver(forName: NSNotification.Name(rawValue: VLCPlaybackServicePlaybackDidResume),
+                                                       object: nil, queue: OperationQueue.main, using: {_ in
+                    self.animateCurrentlyPlayingState()
+                })
+                animateCurrentlyPlayingState()
                 backupThumbnail = audiotrack.thumbnailImage()
             } else {
                 thumbnailView.image = audiotrack.thumbnailImage()
                 backupThumbnail = nil
             }
         } else {
+            NotificationCenter.default.removeObserver(self)
             isMediaBeingPlayed = false
             thumbnailView.image = audiotrack.thumbnailImage()
             backupThumbnail = nil
@@ -333,6 +319,32 @@ class MediaCollectionViewCell: BaseCollectionViewCell, UIScrollViewDelegate {
 
         updateSizeDescriptionLabelConstraint()
         updateLabelsViewContraint()
+    }
+
+    func animateCurrentlyPlayingState() {
+        if !isMediaBeingPlayed {
+            return
+        }
+
+        let orangeColor: UIColor = PresentationTheme.current.colors.orangeUI
+        let playingAnimation = generateAnimation(with: "playing-animation-", color: orangeColor)
+        let pauseAnimation = generateAnimation(with: "pause-animation-", color: orangeColor)
+
+        if playbackService.isPlaying {
+            thumbnailView.animationImages = playingAnimation
+            thumbnailView.animationDuration = 1.2
+            thumbnailView.animationRepeatCount = 0
+            thumbnailView.image = thumbnailView.animationImages?.first
+            thumbnailView.startAnimating()
+        } else {
+            thumbnailView.stopAnimating()
+            thumbnailView.animationImages = pauseAnimation
+            thumbnailView.animationDuration = 0.8
+            thumbnailView.animationRepeatCount = 1
+            thumbnailView.image = thumbnailView.animationImages?.first
+            thumbnailView.startAnimating()
+            thumbnailView.image = pauseAnimation.last
+        }
     }
 
     func update(album: VLCMLAlbum) {
@@ -440,6 +452,7 @@ class MediaCollectionViewCell: BaseCollectionViewCell, UIScrollViewDelegate {
 
     override func prepareForReuse() {
         super.prepareForReuse()
+        NotificationCenter.default.removeObserver(self)
         isEditing = false
         ignoreThemeDidChange = false
         titleLabel.text = ""

commit 46033c68d9526d1630d96a1eea15d5147724d6a5

MediaCollectionViewCell: fix color scheme and font size updates
This fixes a regression from c360a0c2 that removed all observers
instead of the one specific that was intended.

(cherry picked from commit d889f248829d673e7171018904cb425b87818bec)

Sources/Media Library/MediaCategoryCells/MediaCollectionViewCell.swift
@@ -290,7 +290,7 @@ class MediaCollectionViewCell: BaseCollectionViewCell, UIScrollViewDelegate {
                 backupThumbnail = nil
             }
         } else {
-            NotificationCenter.default.removeObserver(self)
+            NotificationCenter.default.removeObserver(self, name: NSNotification.Name(rawValue: VLCPlaybackServicePlaybackDidResume), object: nil)
             isMediaBeingPlayed = false
             thumbnailView.image = audiotrack.thumbnailImage()
             backupThumbnail = nil
@@ -452,7 +452,7 @@ class MediaCollectionViewCell: BaseCollectionViewCell, UIScrollViewDelegate {
 
     override func prepareForReuse() {
         super.prepareForReuse()
-        NotificationCenter.default.removeObserver(self)
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name(rawValue: VLCPlaybackServicePlaybackDidResume), object: nil)
         isEditing = false
         ignoreThemeDidChange = false
         titleLabel.text = ""