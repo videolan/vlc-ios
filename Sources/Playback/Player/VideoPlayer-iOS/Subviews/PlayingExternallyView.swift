fe42db66b5a44cbd379514b7731c7f82f27d1a81
Felix Paul Kühne <felix@feepk.net>
2023-05-04 14:47:14 +0200

    Playing Externally View: remove dead code

--- Sources/Playback/Player/VideoPlayer-iOS/Subviews/PlayingExternallyView.swift
+++ Sources/Playback/Player/VideoPlayer-iOS/Subviews/PlayingExternallyView.swift
@@ -17,32 +17,6 @@ import Foundation
     @IBInspectable var nibName: String?
     @IBOutlet weak var playingExternallyTitle: UILabel!
     @IBOutlet weak var playingExternallyDescription: UILabel!
-    @objc var displayView: UIView? {
-        return externalWindow?.rootViewController?.view
-    }
-    var externalWindow: UIWindow?
-
-    @objc func shouldDisplay(_ show: Bool, movieView: UIView) {
-        self.isHidden = !show
-        if show {
-            guard let screen = UIScreen.screens.count > 1 ? UIScreen.screens[1] : nil else {
-                return
-            }
-            screen.overscanCompensation = .none
-            externalWindow = UIWindow(frame: screen.bounds)
-            guard let externalWindow = externalWindow else {
-                return
-            }
-            externalWindow.rootViewController = VLCExternalDisplayController()
-            externalWindow.rootViewController?.view.addSubview(movieView)
-            externalWindow.screen = screen
-            externalWindow.rootViewController?.view.frame = externalWindow.bounds
-            movieView.frame = externalWindow.bounds
-        } else {
-            externalWindow = nil
-        }
-        externalWindow?.isHidden = !show
-    }
 
     override func awakeFromNib() {
         super.awakeFromNib()
