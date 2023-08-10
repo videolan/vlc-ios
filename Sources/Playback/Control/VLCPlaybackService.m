f28343e19acb618cab956cc6f3be7cee83ebf955
Felix Paul Kühne <felix@feepk.net>
2023-08-10 11:32:03 +0200

    VPS: remove duplicate stop call
    If playback is stopped, it is stopped. No need to do it twice. Fixes #1680

--- Sources/Playback/Control/VLCPlaybackService.m
+++ Sources/Playback/Control/VLCPlaybackService.m
@@ -772,7 +772,6 @@ NSString *const VLCPlaybackServicePlaybackDidMoveOnToNextItem = @"VLCPlaybackSer
             if ([_listPlayer.mediaList indexOfMedia:_mediaPlayer.media] == listCount - 1
                 && self.repeatMode == VLCDoNotRepeat) {
                 _sessionWillRestart = NO;
-                [self stopPlayback];
             }
         } break;
         default:
