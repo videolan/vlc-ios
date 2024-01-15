ffd1aa00dbdda1bf6414b486fcd955641a4fc078
Diogo Simao Marques <dogo@videolabs.io>
2024-01-15 09:50:33 +0100

    VLCPlaybackInfoPlaybackTVViewController: Rework playback options
    This adds the subtitles and audio delay options to the tvOS interface.
    
    This brings also changes to the value selection interface for the playback
    speed and both the delay options.
    
    Finally, the subtitles delay option is only available for medias having the
    video type.
    
    Closes #140
    Closes #1085

--- Sources/Playback/Player/VideoPlayer-tvOS/Playback Info/VLCPlaybackInfoPlaybackTVViewController.h
+++ Sources/Playback/Player/VideoPlayer-tvOS/Playback Info/VLCPlaybackInfoPlaybackTVViewController.h
@@ -6,21 +6,54 @@
  *
  * Authors: Tobias Conradi <videolan # tobias-conradi.de>
  *        Felix Paul Kühne <fkuehne # videolan.org>
+ *        Diogo Simao Marques <dogo@videolabs.io>
  *
  * Refer to the COPYING file of the official project for license.
  *****************************************************************************/
 
 #import "VLCPlaybackInfoPanelTVViewController.h"
+#import "VLCMetadata.h"
+
+#define DEFAULT_DELAY 0.0
+#define MIN_DELAY -30000.0
+#define MAX_DELAY 30000.0
+#define MIN_SPEED 0.25
+#define MAX_SPEED 8.0
+
+typedef enum {
+    VLCPlaybackOptionsTypePlaybackSpeed,
+    VLCPlaybackOptionsTypeSubtitlesDelay,
+    VLCPlaybackOptionsTypeAudioDelay,
+    VLCPlaybackOptionsTypeNone
+} VLCPlaybackOptionsType;
 
 @interface VLCPlaybackInfoPlaybackTVViewController : VLCPlaybackInfoPanelTVViewController
-@property (nonatomic, weak) IBOutlet UISegmentedControl *rateControl;
+@property (weak, nonatomic) IBOutlet UIStackView *optionsStackView;
 @property (nonatomic, weak) IBOutlet UILabel *rateLabel;
+@property (weak, nonatomic) IBOutlet UIButton *playbackSpeedButton;
+@property (weak, nonatomic) IBOutlet UIView *subtitlesDelayView;
+@property (weak, nonatomic) IBOutlet UILabel *subtitlesLabel;
+@property (weak, nonatomic) IBOutlet UIButton *subtitlesDelayButton;
+@property (weak, nonatomic) IBOutlet UILabel *audioLabel;
+@property (weak, nonatomic) IBOutlet UIButton *audioDelayButton;
 @property (nonatomic, weak) IBOutlet UISegmentedControl *repeatControl;
 @property (nonatomic, weak) IBOutlet UILabel *repeatLabel;
 @property (nonatomic, weak) IBOutlet UISegmentedControl *shuffleControl;
 @property (nonatomic, weak) IBOutlet UILabel *shuffleLabel;
 
-- (IBAction)rateControlChanged:(UISegmentedControl *)sender;
+@property (weak, nonatomic) IBOutlet UIView *valueSelectorView;
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *valueLabel;
+@property (weak, nonatomic) IBOutlet UIButton *increaseButton;
+@property (weak, nonatomic) IBOutlet UIButton *decreaseButton;
+@property (weak, nonatomic) IBOutlet UIButton *resetButton;
+
 - (IBAction)repeatControlChanged:(UISegmentedControl *)sender;
 - (IBAction)shuffleControlChanged:(UISegmentedControl *)sender;
+- (IBAction)handleResetButton:(UIButton *)sender;
+- (IBAction)handlePlaybackSpeed:(UIButton *)sender;
+- (IBAction)handleSubtitlesDelay:(UIButton *)sender;
+- (IBAction)handleAudioDelay:(UIButton *)sender;
+- (IBAction)handleIncreaseDecrease:(UIButton *)sender;
+
 @end
--- Sources/Playback/Player/VideoPlayer-tvOS/Playback Info/VLCPlaybackInfoPlaybackTVViewController.m
+++ Sources/Playback/Player/VideoPlayer-tvOS/Playback Info/VLCPlaybackInfoPlaybackTVViewController.m
@@ -6,17 +6,33 @@
  *
  * Authors: Tobias Conradi <videolan # tobias-conradi.de>
  *        Felix Paul Kühne <fkuehne # videolan.org>
+ *        Diogo Simao Marques <dogo@videolabs.io>
  *
  * Refer to the COPYING file of the official project for license.
  *****************************************************************************/
 
 #import "VLCPlaybackInfoPlaybackTVViewController.h"
+
 @interface VLCPlaybackInfoPlaybackTVViewController ()
-@property (nonatomic) NSArray<NSNumber*> *possibleRates;
+@property (nonatomic) VLCPlaybackService *playbackService;
+@property (nonatomic, readwrite) VLCPlaybackOptionsType currentOption;
+
+@property (nonatomic, readonly) CGFloat increaseDelay;
+@property (nonatomic, readonly) CGFloat decreaseDelay;
+@property (nonatomic, readonly) CGFloat increaseSpeed;
+@property (nonatomic, readonly) CGFloat decreaseSpeed;
+
+@property (nonatomic, readonly) CGFloat defaultDelay;
+@property (nonatomic, readonly) CGFloat defaultSpeed;
+
+@property (nonatomic, readwrite) CGFloat currentSubtitlesDelay;
+@property (nonatomic, readwrite) CGFloat currentAudioDelay;
+@property (nonatomic, readwrite) CGFloat currentSpeed;
 @end
 
 @implementation VLCPlaybackInfoPlaybackTVViewController
 
+// MARK: - Init
 
 - (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -27,48 +43,9 @@
     return self;
 }
 
-- (CGSize)preferredContentSize
-{
-    CGFloat height = _rateControl.frame.size.height + _repeatControl.frame.size.height + _shuffleControl.frame.size.height + (3 * CONTENT_INSET);
-
-    if (height < MINIMAL_CONTENT_SIZE) {
-        height = MINIMAL_CONTENT_SIZE;
-    }
-
-    return CGSizeMake(CGRectGetWidth(self.view.bounds), height);
-}
-
-+ (BOOL)shouldBeVisibleForPlaybackController:(VLCPlaybackService *)vpc
-{
-    return [vpc isSeekable];
-}
-
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.possibleRates = @[@(0.25),
-                           @(0.50),
-                           @(0.75),
-                           @(1.00),
-                           @(1.25),
-                           @(1.50),
-                           @(2.00),
-                           @(4.00),
-                           ];
-
-    NSNumberFormatter *formatter = [[NSNumberFormatter alloc] init];
-    formatter.numberStyle = NSNumberFormatterDecimalStyle;
-    formatter.maximumFractionDigits = 2;
-    formatter.minimumFractionDigits = 2;
-
-    UISegmentedControl *rateControl = self.rateControl;
-    [rateControl removeAllSegments];
-
-    [self.possibleRates enumerateObjectsUsingBlock:^(NSNumber * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-        NSString *title = [formatter stringFromNumber:obj];
-        [rateControl insertSegmentWithTitle:title atIndex:idx animated:NO];
-    }];
 
     self.rateLabel.text = NSLocalizedString(@"PLAYBACK_SPEED", nil);
 
@@ -89,39 +66,90 @@
     [shuffleControl insertSegmentWithTitle:NSLocalizedString(@"ON", nil)
                                   atIndex:1 animated:NO];
     self.shuffleLabel.text = NSLocalizedString(@"SHUFFLE", nil);
+
+    _playbackService = [VLCPlaybackService sharedInstance];
+    _currentOption = VLCPlaybackOptionsTypeNone;
+
+    _increaseDelay = 50.0;
+    _decreaseDelay = -50.0;
+    _increaseSpeed = 0.05;
+    _decreaseSpeed = -0.05;
+
+    _defaultDelay = 0.0;
+    _defaultSpeed = [[[NSUserDefaults standardUserDefaults] valueForKey:kVLCSettingPlaybackSpeedDefaultValue] doubleValue];
+
+    [_increaseButton setTitle:@"" forState:UIControlStateNormal];
+    [_decreaseButton setTitle:@"" forState:UIControlStateNormal];
+    [_resetButton setTitle:NSLocalizedString(@"BUTTON_RESET", "") forState:UIControlStateNormal];
+
+    _playbackSpeedButton.accessibilityHint = NSLocalizedString(@"PLAYBACK_SPEED_HINT", "");
+    _subtitlesDelayButton.accessibilityHint = NSLocalizedString(@"SUBTITLES_DELAY_HINT", "");
+    _audioDelayButton.accessibilityHint = NSLocalizedString(@"AUDIO_DELAY_HINT", "");
 }
 
 - (void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
-    [self updateRateControl];
     [self updateRepeatControl];
     [self updateShuffleControl];
+    [self updateButtonsText];
+    [_subtitlesDelayView setHidden:_playbackService.metadata.isAudioOnly];
 }
 
-- (void)updateRateControl
+- (void)viewDidDisappear:(BOOL)animated
 {
-    VLCPlaybackService *vpc = [VLCPlaybackService sharedInstance];
-    float currentRate = vpc.playbackRate;
-
-    NSInteger currentIndex = [self.possibleRates indexOfObjectPassingTest:^BOOL(NSNumber * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-        return ABS(obj.floatValue-currentRate)<0.2;
-    }];
-    UISegmentedControl *rateControl = self.rateControl;
-    rateControl.selectedSegmentIndex = currentIndex;
-    rateControl.enabled = [vpc isSeekable];
+    [super viewDidDisappear:animated];
+    _currentOption = VLCPlaybackOptionsTypeNone;
+    [_valueSelectorView setHidden:YES];
+    [_optionsStackView setHidden:NO];
 }
 
-- (IBAction)rateControlChanged:(UISegmentedControl *)sender
+// MARK: - Override methods
+
+- (CGSize)preferredContentSize
 {
-    float newRate = self.possibleRates[sender.selectedSegmentIndex].floatValue;
-    [VLCPlaybackService sharedInstance].playbackRate = newRate;
+    CGFloat height = _playbackSpeedButton.frame.size.height + _subtitlesDelayButton.frame.size.height + _audioDelayButton.frame.size.height +
+                        _repeatControl.frame.size.height + _shuffleControl.frame.size.height + (5 * CONTENT_INSET);
+
+    if (height < MINIMAL_CONTENT_SIZE) {
+        height = MINIMAL_CONTENT_SIZE;
+    }
+
+    return CGSizeMake(CGRectGetWidth(self.view.bounds), height);
 }
+
+- (NSArray<id<UIFocusEnvironment>> *)preferredFocusEnvironments
+{
+    if (!_optionsStackView.isHidden) {
+        return @[_playbackSpeedButton];
+    }
+
+    return @[_increaseButton];
+}
+
++ (BOOL)shouldBeVisibleForPlaybackController:(VLCPlaybackService *)vpc
+{
+    return [vpc isSeekable];
+}
+
+- (void)pressesBegan:(NSSet<UIPress *> *)presses withEvent:(UIPressesEvent *)event
+{
+    // Hide the value selector view if needed when the return button is pressed
+    if (((UIPress *)[presses anyObject]).type == UIPressTypeMenu && !_valueSelectorView.isHidden) {
+        [self updateButtonsText];
+        [_valueSelectorView setHidden:YES];
+        [_optionsStackView setHidden:NO];
+    } else {
+        [super pressesBegan:presses withEvent:event];
+    }
+}
+
+// MARK: - Helper methods
 
 - (void)updateRepeatControl
 {
     NSUInteger selectedIndex;
-    VLCRepeatMode repeatMode = [VLCPlaybackService sharedInstance].repeatMode;
+    VLCRepeatMode repeatMode = _playbackService.repeatMode;
     switch (repeatMode) {
         case VLCRepeatCurrentItem:
             selectedIndex = 1;
@@ -137,6 +165,54 @@
 
     self.repeatControl.selectedSegmentIndex = selectedIndex;
 }
+
+- (void)updateShuffleControl
+{
+    _shuffleControl.selectedSegmentIndex = _playbackService.shuffleMode;
+}
+
+- (void)setupAccessibilityWith:(NSString *)increaseHint and:(NSString *)decreaseHint
+{
+    _increaseButton.accessibilityLabel = NSLocalizedString(@"INCREASE_BUTTON", "");
+    _increaseButton.accessibilityHint = increaseHint;
+
+    _decreaseButton.accessibilityLabel = NSLocalizedString(@"DECREASE_BUTTON", "");
+    _decreaseButton.accessibilityHint = decreaseHint;
+}
+
+- (void)updateButtonsText
+{
+    [_playbackSpeedButton setTitle:[NSString stringWithFormat:@"%.2fx", _playbackService.playbackRate] forState:UIControlStateNormal];
+    [_subtitlesDelayButton setTitle:[NSString stringWithFormat:@"%.0f ms", _playbackService.subtitleDelay] forState:UIControlStateNormal];
+    [_audioDelayButton setTitle:[NSString stringWithFormat:@"%.0f ms", _playbackService.audioDelay] forState:UIControlStateNormal];
+}
+
+- (CGFloat)computeValueWith:(CGFloat)currentValue offset:(CGFloat)offset lowerBound:(CGFloat)lowerBound upperBound:(CGFloat)upperBound
+{
+    CGFloat returnValue = currentValue + offset;
+    if (returnValue >= lowerBound && returnValue <= upperBound) {
+        return returnValue;
+    }
+
+    return offset < 0 ? lowerBound : upperBound;
+}
+
+- (void)updateValueLabelWith:(CGFloat)value
+{
+    switch (_currentOption) {
+        case VLCPlaybackOptionsTypePlaybackSpeed:
+            [_valueLabel setText:[NSString stringWithFormat:@"%.2fx", value]];
+            break;
+        case VLCPlaybackOptionsTypeSubtitlesDelay:
+        case VLCPlaybackOptionsTypeAudioDelay:
+            [_valueLabel setText:[NSString stringWithFormat:@"%.0f ms", value]];
+            break;
+        default:
+            break;
+    }
+}
+
+// MARK: - Sender methods
 
 - (IBAction)repeatControlChanged:(UISegmentedControl *)sender
 {
@@ -154,17 +230,105 @@
             break;
     }
 
-    [VLCPlaybackService sharedInstance].repeatMode = repeatMode;
-}
-
-- (void)updateShuffleControl
-{
-    self.shuffleControl.selectedSegmentIndex = [VLCPlaybackService sharedInstance].shuffleMode;
+    _playbackService.repeatMode = repeatMode;
 }
 
 - (IBAction)shuffleControlChanged:(UISegmentedControl *)sender
 {
-    [VLCPlaybackService sharedInstance].shuffleMode = sender.selectedSegmentIndex == 1;
+    _playbackService.shuffleMode = sender.selectedSegmentIndex == 1;
+}
+
+- (IBAction)handleIncreaseDecrease:(UIButton *)sender {
+    CGFloat speedOffset = (sender.tag == 1) ? _increaseSpeed : _decreaseSpeed;
+    CGFloat delayOffset = (sender.tag == 1) ? _increaseDelay : _decreaseDelay;
+    CGFloat value = 0.0;
+
+    switch (_currentOption) {
+        case VLCPlaybackOptionsTypePlaybackSpeed:
+            _currentSpeed = [self computeValueWith:_currentSpeed offset:speedOffset lowerBound:MIN_SPEED upperBound:MAX_SPEED];
+            _playbackService.playbackRate = _currentSpeed;
+            value = _currentSpeed;
+            break;
+        case VLCPlaybackOptionsTypeSubtitlesDelay:
+            _currentSubtitlesDelay = [self computeValueWith:_currentSubtitlesDelay offset:delayOffset lowerBound:MIN_DELAY upperBound:MAX_DELAY];
+            _playbackService.subtitleDelay = _currentSubtitlesDelay;
+            value = _currentSubtitlesDelay;
+            break;
+        case VLCPlaybackOptionsTypeAudioDelay:
+            _currentAudioDelay = [self computeValueWith:_currentAudioDelay offset:delayOffset lowerBound:MIN_DELAY upperBound:MAX_DELAY];
+            _playbackService.audioDelay = _currentAudioDelay;
+            value = _currentAudioDelay;
+            break;
+        default:
+            break;
+    }
+
+    [self updateValueLabelWith:value];
+}
+
+- (IBAction)handlePlaybackSpeed:(UIButton *)sender {
+    _currentSpeed = _playbackService.playbackRate;
+    _currentOption = VLCPlaybackOptionsTypePlaybackSpeed;
+    [_titleLabel setText:NSLocalizedString(@"PLAYBACK_SPEED", "")];
+    [self updateValueLabelWith:_currentSpeed];
+
+    [self setupAccessibilityWith:NSLocalizedString(@"INCREASE_PLAYBACK_SPEED", "")
+                             and:NSLocalizedString(@"DECREASE_PLAYBACK_SPEED", "")];
+
+    [_optionsStackView setHidden:YES];
+    [_valueSelectorView setHidden:NO];
+}
+
+- (IBAction)handleSubtitlesDelay:(UIButton *)sender {
+    _currentSubtitlesDelay = _playbackService.subtitleDelay;
+    _currentOption = VLCPlaybackOptionsTypeSubtitlesDelay;
+    [_titleLabel setText:NSLocalizedString(@"SPU_DELAY", "")];
+    [self updateValueLabelWith:_currentSubtitlesDelay];
+
+    [self setupAccessibilityWith:NSLocalizedString(@"INCREASE_SUBTITLES_DELAY", "")
+                             and:NSLocalizedString(@"DECREASE_SUBTITLES_DELAY", "")];
+
+    [_optionsStackView setHidden:YES];
+    [_valueSelectorView setHidden:NO];
+}
+
+- (IBAction)handleAudioDelay:(UIButton *)sender {
+    _currentAudioDelay = _playbackService.audioDelay;
+    _currentOption = VLCPlaybackOptionsTypeAudioDelay;
+    [_titleLabel setText:NSLocalizedString(@"AUDIO_DELAY", "")];
+    [self updateValueLabelWith:_currentAudioDelay];
+
+    [self setupAccessibilityWith:NSLocalizedString(@"INCREASE_AUDIO_DELAY", "")
+                             and:NSLocalizedString(@"DECREASE_AUDIO_DELAY", "")];
+
+    [_optionsStackView setHidden:YES];
+    [_valueSelectorView setHidden:NO];
+}
+
+- (IBAction)handleResetButton:(UIButton *)sender {
+    CGFloat value = 0.0;
+
+    switch (_currentOption) {
+        case VLCPlaybackOptionsTypePlaybackSpeed:
+            _currentSpeed = _defaultSpeed;
+            _playbackService.playbackRate = _currentSpeed;
+            value = _currentSpeed;
+            break;
+        case VLCPlaybackOptionsTypeSubtitlesDelay:
+            _currentSubtitlesDelay = _defaultDelay;
+            _playbackService.subtitleDelay = _currentSubtitlesDelay;
+            value = _currentSubtitlesDelay;
+            break;
+        case VLCPlaybackOptionsTypeAudioDelay:
+            _currentAudioDelay = _defaultDelay;
+            _playbackService.audioDelay = _currentAudioDelay;
+            value = _currentAudioDelay;
+            break;
+        default:
+            break;
+    }
+
+    [self updateValueLabelWith:value];
 }
 
 @end
