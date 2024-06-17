videolan/vlc-ios

commit cffc06c56e9bb0cf7d6f42f4f92918404ac4113c

VLCPlaybackService: Refactor the audio and subtitles tracks attribution
This allows to remove the two cells allowing the user to select an external
audio and subtitles tracks on the tvOS interface as this feature is not available.

The audio and subtitles tracks titles attribution has also been simplified.

Sources/Playback/Control/VLCPlaybackService.m
@@ -636,12 +636,20 @@ NSString *const VLCPlaybackServicePlaybackDidMoveOnToNextItem = @"VLCPlaybackSer
 
 - (NSInteger)numberOfAudioTracks
 {
+#if TARGET_OS_IOS
     return [_mediaPlayer numberOfAudioTracks] + 1;
+#else
+    return [_mediaPlayer numberOfAudioTracks];
+#endif
 }
 
 - (NSInteger)numberOfVideoSubtitlesIndexes
 {
-    return _mediaPlayer.videoSubTitlesIndexes.count + 2;
+#if TARGET_OS_IOS
+    return [_mediaPlayer numberOfSubtitlesTracks] + 2;
+#else
+    return [_mediaPlayer numberOfSubtitlesTracks] + 1;
+#endif
 }
 
 - (NSInteger)numberOfTitles
@@ -657,24 +665,23 @@ NSString *const VLCPlaybackServicePlaybackDidMoveOnToNextItem = @"VLCPlaybackSer
 - (NSString *)videoSubtitleNameAtIndex:(NSInteger)index
 {
     NSInteger count = _mediaPlayer.videoSubTitlesNames.count;
-    if (index >= 0 && index < count) {
-        return _mediaPlayer.videoSubTitlesNames[index];
-    } else if (index == count) {
+
+    if (index == count) {
         return NSLocalizedString(@"SELECT_SUBTITLE_FROM_FILES", nil);
+    } else {
+        return _mediaPlayer.videoSubTitlesNames[index];
     }
-    else if (index == count + 1) {
-        return NSLocalizedString(@"DOWNLOAD_SUBS_FROM_OSO", nil);
-    }
-    return @"";
 }
 
 - (NSString *)audioTrackNameAtIndex:(NSInteger)index
 {
-    if (index >= 0 && index < _mediaPlayer.audioTrackNames.count)
-        return _mediaPlayer.audioTrackNames[index];
-    else if (index == _mediaPlayer.audioTrackNames.count)
+    NSInteger count = _mediaPlayer.audioTrackNames.count;
+
+    if (index == count) {
         return NSLocalizedString(@"SELECT_AUDIO_FROM_FILES", nil);
-    return @"";
+    } else {
+        return _mediaPlayer.audioTrackNames[index];
+    }
 }
 
 - (NSDictionary *)titleDescriptionsDictAtIndex:(NSInteger)index

Sources/Playback/Player/VideoPlayer-iOS/TitleSelectionView.swift
@@ -303,7 +303,10 @@ extension TitleSelectionView: UITableViewDelegate, UITableViewDataSource {
                 textColor = PresentationTheme.currentExcludingWhite.colors.orangeUI
                 cell.checkImageView.alpha = 1
             }
-            cellTitle = playbackService.videoSubtitleName(at: indexPath.row)
+
+            let count = playbackService.numberOfVideoSubtitlesIndexes
+            cellTitle = indexPath.row == count - 1 ? NSLocalizedString("DOWNLOAD_SUBS_FROM_OSO", comment: "") :
+                        playbackService.videoSubtitleName(at: indexPath.row)
         }
 
         if cellTitle == "Disable" {

Sources/Playback/Player/VideoPlayer-tvOS/Playback Info/VLCPlaybackInfoTracksTVViewController.m
@@ -154,7 +154,8 @@
     VLCPlaybackService *vpc = [VLCPlaybackService sharedInstance];
     NSInteger row = indexPath.row;
     NSString *trackName;
-    if (row >= [vpc numberOfVideoSubtitlesIndexes]) {
+
+    if (row == [vpc numberOfVideoSubtitlesIndexes] - 1) {
         trackName = NSLocalizedString(@"DOWNLOAD_SUBS_FROM_OSO", nil);
     } else {
         BOOL isSelected = [vpc indexOfCurrentSubtitleTrack] == row;

commit 906b331262691a794f946ce58bb5c75dc26411fc

VPS: fix track selection crashes
This is done by increased caching and adding missing sanity checks

This fixes #1837, that is a regression from cffc06c5

Sources/Playback/Control/VLCPlaybackService.m
@@ -662,24 +662,28 @@ NSString *const VLCPlaybackServicePlaybackDidMoveOnToNextItem = @"VLCPlaybackSer
 
 - (NSString *)videoSubtitleNameAtIndex:(NSInteger)index
 {
-    NSInteger count = _mediaPlayer.videoSubTitlesNames.count;
+    NSArray *videoSubTitlesNames = _mediaPlayer.videoSubTitlesNames;
+    NSInteger count = videoSubTitlesNames.count;
 
     if (index == count) {
         return NSLocalizedString(@"SELECT_SUBTITLE_FROM_FILES", nil);
-    } else {
-        return _mediaPlayer.videoSubTitlesNames[index];
+    } else if (index < count) {
+        return videoSubTitlesNames[index];
     }
+    return @"";
 }
 
 - (NSString *)audioTrackNameAtIndex:(NSInteger)index
 {
-    NSInteger count = _mediaPlayer.audioTrackNames.count;
+    NSArray *audioTrackNames = _mediaPlayer.audioTrackNames;
+    NSInteger count = audioTrackNames.count;
 
     if (index == count) {
         return NSLocalizedString(@"SELECT_AUDIO_FROM_FILES", nil);
-    } else {
-        return _mediaPlayer.audioTrackNames[index];
+    } else if (index < count) {
+        return audioTrackNames[index];
     }
+    return @"";
 }
 
 - (NSDictionary *)titleDescriptionsDictAtIndex:(NSInteger)index
@@ -699,16 +703,18 @@ NSString *const VLCPlaybackServicePlaybackDidMoveOnToNextItem = @"VLCPlaybackSer
 
 - (void)selectAudioTrackAtIndex:(NSInteger)index
 {
-    if (index >= 0 && index < _mediaPlayer.audioTrackIndexes.count) {
+    NSArray *audioTrackIndexes = _mediaPlayer.audioTrackIndexes;
+    if (index >= 0 && index < audioTrackIndexes.count) {
         //we can cast this cause we won't have more than 2 million audiotracks
-        _mediaPlayer.currentAudioTrackIndex = [_mediaPlayer.audioTrackIndexes[index] intValue];
+        _mediaPlayer.currentAudioTrackIndex = [audioTrackIndexes[index] intValue];
     }
 }
 
 - (void)selectVideoSubtitleAtIndex:(NSInteger)index
 {
-    if (index >= 0 && index < _mediaPlayer.videoSubTitlesIndexes.count) {
-        _mediaPlayer.currentVideoSubTitleIndex = [_mediaPlayer.videoSubTitlesIndexes[index] intValue];
+    NSArray *videoSubTitlesIndexes = _mediaPlayer.videoSubTitlesIndexes;
+    if (index >= 0 && index < videoSubTitlesIndexes.count) {
+        _mediaPlayer.currentVideoSubTitleIndex = [videoSubTitlesIndexes[index] intValue];
     }
 }
 
@@ -1141,7 +1147,8 @@ NSString *const VLCPlaybackServicePlaybackDidMoveOnToNextItem = @"VLCPlaybackSer
     if (!enabled)
         _mediaPlayer.currentVideoTrackIndex = -1;
     else if (_mediaPlayer.currentVideoTrackIndex == -1) {
-        for (NSNumber *trackId in _mediaPlayer.videoTrackIndexes) {
+        NSArray *videoTrackIndexes = _mediaPlayer.videoTrackIndexes;
+        for (NSNumber *trackId in videoTrackIndexes) {
             if ([trackId intValue] != -1) {
                 _mediaPlayer.currentVideoTrackIndex = [trackId intValue];
                 break;