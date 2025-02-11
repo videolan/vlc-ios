efb99f8199e8e92ff17150de25241299c12b81ba
Diogo Simao Marques <dogo@videolabs.io>
2025-02-11 16:40:18 +0100

    AudioViewController: Add a view controller for each audio tab
    A proper view controller is now handling each tab of the Audio section.
    
    This can be used to create a proper navigation tab without having to use
    the paging view controller.

--- Sources/Media Library/MediaViewControllers/AudioViewController.swift
+++ Sources/Media Library/MediaViewControllers/AudioViewController.swift
@@ -44,3 +44,107 @@ class AudioViewController: MediaViewController {
         navigationItem.titleView = nil
     }
 }
+
+class ArtistsViewController: MediaViewController {
+    override init(mediaLibraryService: MediaLibraryService) {
+        super.init(mediaLibraryService: mediaLibraryService)
+        setupUI()
+    }
+
+    private func setupUI() {
+        let localizedTitle: String = NSLocalizedString("ARTISTS", comment: "")
+        title = localizedTitle
+
+        let image: UIImage?
+        if #available(iOS 13.0, *) {
+            let color: UIColor = PresentationTheme.current.colors.tabBarIconColor
+            image = UIImage(named: "artists")?.withTintColor(color)
+        } else {
+            image = UIImage(named: "artists")
+        }
+
+        tabBarItem = UITabBarItem(title: localizedTitle, image: image, selectedImage: image)
+    }
+
+    override func viewControllers(for pagerTabStripController: PagerTabStripViewController) -> [UIViewController] {
+        return [ArtistCategoryViewController(mediaLibraryService)]
+    }
+}
+
+class AlbumsViewController: MediaViewController {
+    override init(mediaLibraryService: MediaLibraryService) {
+        super.init(mediaLibraryService: mediaLibraryService)
+        setupUI()
+    }
+
+    private func setupUI() {
+        let localizedTitle: String = NSLocalizedString("ALBUMS", comment: "")
+        title = localizedTitle
+
+        let image: UIImage?
+        if #available(iOS 13.0, *) {
+            let color: UIColor = PresentationTheme.current.colors.tabBarIconColor
+            image = UIImage(named: "albums")?.withTintColor(color)
+        } else {
+            image = UIImage(named: "albums")
+        }
+
+        tabBarItem = UITabBarItem(title: localizedTitle, image: image, selectedImage: image)
+    }
+
+    override func viewControllers(for pagerTabStripController: PagerTabStripViewController) -> [UIViewController] {
+        return [AlbumCategoryViewController(mediaLibraryService)]
+    }
+}
+
+class TracksViewController: MediaViewController {
+    override init(mediaLibraryService: MediaLibraryService) {
+        super.init(mediaLibraryService: mediaLibraryService)
+        setupUI()
+    }
+
+    private func setupUI() {
+        let localizedTitle: String = NSLocalizedString("SONGS", comment: "")
+        title = localizedTitle
+
+        let image: UIImage?
+        if #available(iOS 13.0, *) {
+            let color: UIColor = PresentationTheme.current.colors.tabBarIconColor
+            image = UIImage(named: "songs")?.withTintColor(color)
+        } else {
+            image = UIImage(named: "songs")
+        }
+
+        tabBarItem = UITabBarItem(title: localizedTitle, image: image, selectedImage: image)
+    }
+
+    override func viewControllers(for pagerTabStripController: PagerTabStripViewController) -> [UIViewController] {
+        return [TrackCategoryViewController(mediaLibraryService)]
+    }
+}
+
+class GenresViewController: MediaViewController {
+    override init(mediaLibraryService: MediaLibraryService) {
+        super.init(mediaLibraryService: mediaLibraryService)
+        setupUI()
+    }
+
+    private func setupUI() {
+        let localizedTitle: String = NSLocalizedString("GENRES", comment: "")
+        title = localizedTitle
+
+        let image: UIImage?
+        if #available(iOS 13.0, *) {
+            let color: UIColor = PresentationTheme.current.colors.tabBarIconColor
+            image = UIImage(named: "genres")?.withTintColor(color)
+        } else {
+            image = UIImage(named: "genres")
+        }
+
+        tabBarItem = UITabBarItem(title: localizedTitle, image: image, selectedImage: image)
+    }
+
+    override func viewControllers(for pagerTabStripController: PagerTabStripViewController) -> [UIViewController] {
+        return [GenreCategoryViewController(mediaLibraryService)]
+    }
+}
