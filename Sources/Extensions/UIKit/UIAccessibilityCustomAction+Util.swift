32e737889c09931e16f0eb7b1e72b0c9cfdca61f
Craig Reyenga <craig.reyenga@gmail.com>
2025-02-15 14:24:33 -0500

    Fix unwanted image scaling in accessibility custom actions by adding our own padding.

--- Sources/Extensions/UIKit/UIAccessibilityCustomAction+Util.swift
+++ Sources/Extensions/UIKit/UIAccessibilityCustomAction+Util.swift
@@ -17,7 +17,27 @@ extension UIAccessibilityCustomAction {
         let a = UIAccessibilityCustomAction(name: name, target: target, selector: selector)
 
         if #available(iOS 13.0, *) {
-            a.image = image
+            // For some reason, an image specified on these accessibility
+            // actions will get scaled up in size significantly. Below, we work
+            // against that by adding our own padding to the image. If Apple
+            // ever fixes the problem on their end, or offers an alternative
+            // approach to prevent this from happening, everything below should
+            // be removed or at least made conditional based on OS version.
+            a.image = image.flatMap { img in
+                // This was determined empirically:
+                let newSize = CGSize(width: 40, height: 40)
+
+                let renderer = UIGraphicsImageRenderer(size: newSize)
+                return renderer.image { context in
+                    UIColor.clear.setFill()
+                    context.fill(CGRect(origin: .zero, size: newSize))
+
+                    let x = (newSize.width - img.size.width) / 2
+                    let y = (newSize.height - img.size.height) / 2
+                    let imageOrigin = CGPoint(x: x, y: y)
+                    img.draw(at: imageOrigin)
+                }
+            }
         }
 
         return a
