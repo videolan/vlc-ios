videolan/vlc-ios

commit 4f8560b0f324647d9c4ecbb30937886afeefa6cb

Minor visionOS compilation fixes and optimizations

Resources/en.lproj/Localizable.strings
@@ -676,16 +676,7 @@
 "KEY_INCREASE_PLAYBACK_SPEED" = "Increase playback speed";
 "KEY_DECREASE_PLAYBACK_SPEED" = "Decrease playback speed";
 "KEY_RESET_PLAYBACK_SPEED" = "Reset playback speed";
-"KEY_SEEK_TO_10_PERCENT" = "Seek to 10% of duration";
-"KEY_SEEK_TO_20_PERCENT" = "Seek to 20% of duration";
-"KEY_SEEK_TO_30_PERCENT" = "Seek to 30% of duration";
-"KEY_SEEK_TO_40_PERCENT" = "Seek to 40% of duration";
-"KEY_SEEK_TO_50_PERCENT" = "Seek to 50% of duration";
-"KEY_SEEK_TO_60_PERCENT" = "Seek to 60% of duration";
-"KEY_SEEK_TO_70_PERCENT" = "Seek to 70% of duration";
-"KEY_SEEK_TO_80_PERCENT" = "Seek to 80% of duration";
-"KEY_SEEK_TO_90_PERCENT" = "Seek to 90% of duration";
-"KEY_SEEK_TO_START" = "Seek to beginning of video";
+"KEY_SEEK_TO_\(number * 10)_PERCENT" = "KEY_SEEK_TO_\(number * 10)_PERCENT";
 
 // MARK: - TitleSelection
 

Sources/Playback/Player/PlayerViewController.swift
@@ -967,9 +967,11 @@ class PlayerViewController: UIViewController {
 // MARK: - VLCPlaybackServiceDelegate
 
 extension PlayerViewController: VLCPlaybackServiceDelegate {
+#if os(iOS)
     func pictureInPictureStateDidChange(enabled: Bool) {
         mediaNavigationBar.updatePictureInPictureButton(enabled: enabled)
     }
+#endif
 
     func playbackPositionUpdated(_ playbackService: PlaybackService) {
         mediaScrubProgressBar.updateInterfacePosition()
@@ -1415,72 +1417,35 @@ extension PlayerViewController {
                                          action: #selector(keyLeftBracket))
         decreaseSpeed.discoverabilityTitle = NSLocalizedString("KEY_DECREASE_PLAYBACK_SPEED", comment: "")
 
-        var commands: [UIKeyCommand] = [
-            UIKeyCommand(input: " ",
-                         modifierFlags: [],
-                         action: #selector(handlePlayPauseGesture),
-                         discoverabilityTitle: NSLocalizedString("PLAY_PAUSE_BUTTON", comment: "")),
-            UIKeyCommand(input: "\r",
-                         modifierFlags: [],
-                         action: #selector(handlePlayPauseGesture),
-                         discoverabilityTitle: NSLocalizedString("PLAY_PAUSE_BUTTON", comment: "")),
-            UIKeyCommand(input: UIKeyCommand.inputLeftArrow,
-                         modifierFlags: [],
-                         action: #selector(keyLeftArrow),
-                         discoverabilityTitle: NSLocalizedString("KEY_JUMP_BACKWARDS", comment: "")),
-            UIKeyCommand(input: UIKeyCommand.inputRightArrow,
-                         modifierFlags: [],
-                         action: #selector(keyRightArrow),
-                         discoverabilityTitle: NSLocalizedString("KEY_JUMP_FORWARDS", comment: "")),
-            UIKeyCommand(input: "[",
-                         modifierFlags: [],
-                         action: #selector(keyRightBracket),
-                         discoverabilityTitle: NSLocalizedString("KEY_INCREASE_PLAYBACK_SPEED", comment: "")),
-            UIKeyCommand(input: "]",
-                         modifierFlags: [],
-                         action: #selector(keyLeftBracket),
-                         discoverabilityTitle: NSLocalizedString("KEY_DECREASE_PLAYBACK_SPEED", comment: "")),
-            UIKeyCommand(input: "1",
-                         modifierFlags: [],
-                         action: #selector(keyNumber1),
-                         discoverabilityTitle: NSLocalizedString("KEY_SEEK_TO_10_PERCENT", comment: "")),
-            UIKeyCommand(input: "2",
-                         modifierFlags: [],
-                         action: #selector(keyNumber2),
-                         discoverabilityTitle: NSLocalizedString("KEY_SEEK_TO_20_PERCENT", comment: "")),
-            UIKeyCommand(input: "3",
-                         modifierFlags: [],
-                         action: #selector(keyNumber3),
-                         discoverabilityTitle: NSLocalizedString("KEY_SEEK_TO_30_PERCENT", comment: "")),
-            UIKeyCommand(input: "4",
-                         modifierFlags: [],
-                         action: #selector(keyNumber4),
-                         discoverabilityTitle: NSLocalizedString("KEY_SEEK_TO_40_PERCENT", comment: "")),
-            UIKeyCommand(input: "5",
-                         modifierFlags: [],
-                         action: #selector(keyNumber5),
-                         discoverabilityTitle: NSLocalizedString("KEY_SEEK_TO_50_PERCENT", comment: "")),
-            UIKeyCommand(input: "6",
-                         modifierFlags: [],
-                         action: #selector(keyNumber6),
-                         discoverabilityTitle: NSLocalizedString("KEY_SEEK_TO_60_PERCENT", comment: "")),
-            UIKeyCommand(input: "7",
-                         modifierFlags: [],
-                         action: #selector(keyNumber7),
-                         discoverabilityTitle: NSLocalizedString("KEY_SEEK_TO_70_PERCENT", comment: "")),
-            UIKeyCommand(input: "8",
-                         modifierFlags: [],
-                         action: #selector(keyNumber8),
-                         discoverabilityTitle: NSLocalizedString("KEY_SEEK_TO_80_PERCENT", comment: "")),
-            UIKeyCommand(input: "9",
-                         modifierFlags: [],
-                         action: #selector(keyNumber9),
-                         discoverabilityTitle: NSLocalizedString("KEY_SEEK_TO_90_PERCENT", comment: "")),
-            UIKeyCommand(input: "0",
-                         modifierFlags: [],
-                         action: #selector(keyNumber0),
-                         discoverabilityTitle: NSLocalizedString("KEY_SEEK_TO_START", comment: ""))
-        ]
+        var commands: [UIKeyCommand] = {
+            let playPauseCommand1 = UIKeyCommand(input: " ", modifierFlags: [], action: #selector(handlePlayPauseGesture))
+            playPauseCommand1.discoverabilityTitle = NSLocalizedString("PLAY_PAUSE_BUTTON", comment: "")
+
+            let playPauseCommand2 = UIKeyCommand(input: "\r", modifierFlags: [], action: #selector(handlePlayPauseGesture))
+            playPauseCommand2.discoverabilityTitle = NSLocalizedString("PLAY_PAUSE_BUTTON", comment: "")
+
+            let leftArrowCommand = UIKeyCommand(input: UIKeyCommand.inputLeftArrow, modifierFlags: [], action: #selector(keyLeftArrow))
+            leftArrowCommand.discoverabilityTitle = NSLocalizedString("KEY_JUMP_BACKWARDS", comment: "")
+
+            let rightArrowCommand = UIKeyCommand(input: UIKeyCommand.inputRightArrow, modifierFlags: [], action: #selector(keyRightArrow))
+            rightArrowCommand.discoverabilityTitle = NSLocalizedString("KEY_JUMP_FORWARDS", comment: "")
+
+            let increaseSpeedCommand = UIKeyCommand(input: "[", modifierFlags: [], action: #selector(keyRightBracket))
+            increaseSpeedCommand.discoverabilityTitle = NSLocalizedString("KEY_INCREASE_PLAYBACK_SPEED", comment: "")
+
+            let decreaseSpeedCommand = UIKeyCommand(input: "]", modifierFlags: [], action: #selector(keyLeftBracket))
+            decreaseSpeedCommand.discoverabilityTitle = NSLocalizedString("KEY_DECREASE_PLAYBACK_SPEED", comment: "")
+
+            let numberCommands = (0...9).map { number -> UIKeyCommand in
+                let input = "\(number)"
+                let selector = Selector("keyNumber\(number)")
+                let command = UIKeyCommand(input: input, modifierFlags: [], action: selector)
+                command.discoverabilityTitle = NSLocalizedString("KEY_SEEK_TO_\(number * 10)_PERCENT", comment: "")
+                return command
+            }
+
+            return [playPauseCommand1, playPauseCommand2, leftArrowCommand, rightArrowCommand, increaseSpeedCommand, decreaseSpeedCommand] + numberCommands
+        }()
 
         if abs(playbackService.playbackRate - 1.0) > .ulpOfOne {
             let resetSpeed = UIKeyCommand(input: "=",

Sources/App/iOS/TabBarCoordinator.swift
@@ -86,15 +86,18 @@ class TabBarCoordinator: NSObject {
         let bottomBarLayer = bottomBar.layer
 
         //Setting this in appearanceManager doesn't update tabbar and UINavigationbar of the settingsViewController on change hence we do it here
-        tabBar.isTranslucent = true
         tabBar.backgroundColor = colors.tabBarColor
         tabBar.barTintColor = colors.tabBarColor
         tabBar.itemPositioning = .fill
 
-        bottomBar.isTranslucent = false
         bottomBar.backgroundColor = colors.tabBarColor
         bottomBar.barTintColor = colors.tabBarColor
         bottomBar.itemPositioning = .fill
+
+#if os(iOS)
+        tabBar.isTranslucent = true
+        bottomBar.isTranslucent = false
+#endif
 
         tabBarLayer.shadowOffset = CGSize(width: 0, height: 0)
         tabBarLayer.shadowRadius = 1.0

Sources/Network/Favorite/iOS/FavoriteListViewController.swift
@@ -206,6 +206,7 @@ class FavoriteListViewController: UIViewController {
         }
     }
 
+#if os(iOS)
     private func showCloudFavVC(fav: VLCFavorite) {
         let favURL = fav.url
         var cloudVC: VLCCloudStorageTableViewController?
@@ -236,6 +237,7 @@ class FavoriteListViewController: UIViewController {
             self.navigationController?.pushViewController(viewController, animated: true)
         }
     }
+#endif
 }
 
 // MARK: - UITableViewDelegate

Sources/Playback/Player/VideoPlayer-iOS/Subviews/EqualizerPresetSelector.swift
@@ -1,12 +1,12 @@
 /*****************************************************************************
-* EqualizerPresetSelector.swift
-*
-* Copyright © 2021 VLC authors and VideoLAN
-*
-* Authors: Edgar Fouillet <vlc # edgar.fouillet.eu>
-*
-* Refer to the COPYING file of the official project for license.
-*****************************************************************************/
+ * EqualizerPresetSelector.swift
+ *
+ * Copyright © 2021 VLC authors and VideoLAN
+ *
+ * Authors: Edgar Fouillet <vlc # edgar.fouillet.eu>
+ *
+ * Refer to the COPYING file of the official project for license.
+ *****************************************************************************/
 
 import UIKit
 
@@ -218,7 +218,6 @@ class EqualizerPresetSelector: SpoilerButton, UITableViewDataSource, UITableView
         let actions: [UIContextualAction] = [moveUpAction, moveDownAction]
         return UISwipeActionsConfiguration(actions: actions)
     }
-#endif
 
     // MARK: - Slider event
     @objc func preampSliderDidChangeValue(sender: UISlider) {

Sources/Playback/Player/VideoPlayer-iOS/Subviews/MediaNavigationBar.swift
@@ -85,8 +85,6 @@ private enum RendererActionSheetContent: Int, CaseIterable {
         return rotateButton
     }()
 
-    private var closureQueue: (() -> Void)? = nil
-
 #if os(iOS)
     lazy var chromeCastButton: UIButton = {
         var chromeButton = UIButton(type: .system)
@@ -224,13 +222,10 @@ private enum RendererActionSheetContent: Int, CaseIterable {
         addArrangedSubview(queueButton)
 #if os(iOS)
         addArrangedSubview(deviceButton)
-<<<<<<< HEAD
         addArrangedSubview(pictureInPictureButton)
-=======
 #else
         addArrangedSubview(airplayVolumeView)
 #endif
->>>>>>> 0b803b8dd (Update video player for visionOS)
     }
 
     // MARK: Gesture recognizer
@@ -289,10 +284,12 @@ private enum RendererActionSheetContent: Int, CaseIterable {
         closePlaybackButton.accessibilityHint = accessibility.1
     }
 
+#if os(iOS)
     func updatePictureInPictureButton(enabled:Bool) {
         let image = UIImage(named: enabled ? "pip.exit" : "pip.enter")
         pictureInPictureButton.setImage(image, for: .normal)
     }
+#endif
 }
 
 #if os(iOS)

Sources/Settings/Controller/PasscodeLockController.swift
@@ -347,11 +347,12 @@ class PasscodeLockController: UIViewController {
     @objc private func handleCancel() {
         completionHandler?(false, nil)
 
+#if os(iOS)
         if #available(iOS 10, *) {
             ImpactFeedbackGenerator().selectionChanged()
         }
+#endif
 
-<<<<<<< HEAD
         dismiss(animated: true)
     }
 

commit 3ac0381b631f47679b036d934718f5e95388d530

Player: fix minor localization regression
Regression from optimization in 4f8560b0

Resources/en.lproj/Localizable.strings
@@ -682,7 +682,7 @@
 "KEY_INCREASE_PLAYBACK_SPEED" = "Increase playback speed";
 "KEY_DECREASE_PLAYBACK_SPEED" = "Decrease playback speed";
 "KEY_RESET_PLAYBACK_SPEED" = "Reset playback speed";
-"KEY_SEEK_TO_\(number * 10)_PERCENT" = "KEY_SEEK_TO_\(number * 10)_PERCENT";
+"KEY_SEEK_TO_PERCENT" = "Seek to %i%% of duration";
 
 // MARK: - TitleSelection
 

Sources/Playback/Player/PlayerViewController.swift
@@ -1578,7 +1578,7 @@ extension PlayerViewController {
                 let input = "\(number)"
                 let selector = Selector("keyNumber\(number)")
                 let command = UIKeyCommand(input: input, modifierFlags: [], action: selector)
-                command.discoverabilityTitle = NSLocalizedString("KEY_SEEK_TO_\(number * 10)_PERCENT", comment: "")
+                command.discoverabilityTitle = String(format:NSLocalizedString("KEY_SEEK_TO_PERCENT", comment: ""), number * 10)
                 return command
             }
 