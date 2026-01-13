78fb285c9f2d5f7ec4a455ae2a51f3484fe6cb03
Felix Paul Kühne <felix@feepk.net>
2026-01-13 16:58:35 +0100

    Shared Library: compare IP address instead of hostname
    The previously used hostname function in this context is responsible
    for 22% of all hangs (so N°2). Comparing IPs instead is much faster.

--- Sources/Network/Server Browsing/Data/Protocols/Shared Library/VLCLocalNetworkServiceBrowserHTTP.m
+++ Sources/Network/Server Browsing/Data/Protocols/Shared Library/VLCLocalNetworkServiceBrowserHTTP.m
@@ -16,6 +16,69 @@
 #import "VLCHTTPUploaderController.h"
 #import "VLCNetworkServerBrowserSharedLibrary.h"
 
+#import <ifaddrs.h>
+#import <arpa/inet.h>
+#import <net/if.h>
+#import <netinet/in.h>
+#import <sys/socket.h>
+
+static BOOL SocketAddressHasIP(const struct sockaddr *socketaddress) {
+    return socketaddress && (socketaddress->sa_family == AF_INET || socketaddress->sa_family == AF_INET6);
+}
+
+static BOOL SockaddrIPEqualIgnoringPort(const struct sockaddr *a, const struct sockaddr *b) {
+    if (!SocketAddressHasIP(a) || !SocketAddressHasIP(b))
+        return NO;
+
+    if (a->sa_family != b->sa_family)
+        return NO;
+
+    if (a->sa_family == AF_INET) {
+        const struct sockaddr_in *ia = (const struct sockaddr_in *)a;
+        const struct sockaddr_in *ib = (const struct sockaddr_in *)b;
+        // Compare only IPv4 address (ignore port)
+        return (ia->sin_addr.s_addr == ib->sin_addr.s_addr);
+    } else { // AF_INET6
+        const struct sockaddr_in6 *ia6 = (const struct sockaddr_in6 *)a;
+        const struct sockaddr_in6 *ib6 = (const struct sockaddr_in6 *)b;
+        // Compare only IPv6 address (ignore port)
+        return (memcmp(&ia6->sin6_addr, &ib6->sin6_addr, sizeof(struct in6_addr)) == 0);
+    }
+}
+
+static NSArray<NSData *> *LocalInterfaceSocketAddresses(void) {
+    NSMutableArray<NSData *> *ret = [NSMutableArray array];
+
+    struct ifaddrs *ifaddr = NULL;
+    if (getifaddrs(&ifaddr) != 0 || !ifaddr) {
+        return ret;
+    }
+
+    for (struct ifaddrs *ifa = ifaddr; ifa; ifa = ifa->ifa_next) {
+        if (!ifa->ifa_addr)
+            continue;
+
+        if ((ifa->ifa_flags & IFF_UP) == 0)
+            continue;
+
+        if (ifa->ifa_flags & IFF_LOOPBACK)
+            continue;
+
+        const struct sockaddr *sa = ifa->ifa_addr;
+        if (!SocketAddressHasIP(sa))
+            continue;
+
+        if (sa->sa_family == AF_INET) {
+            [ret addObject:[NSData dataWithBytes:sa length:sizeof(struct sockaddr_in)]];
+        } else if (sa->sa_family == AF_INET6) {
+            [ret addObject:[NSData dataWithBytes:sa length:sizeof(struct sockaddr_in6)]];
+        }
+    }
+
+    freeifaddrs(ifaddr);
+    return ret;
+}
+
 @interface VLCLocalNetworkServiceBrowserHTTP()
 @property (nonatomic, strong) VLCSharedLibraryParser *httpParser;
 @end
@@ -36,13 +99,31 @@
     }
     return _httpParser;
 }
+
 - (void)netServiceDidResolveAddress:(NSNetService *)sender {
 #if !TARGET_OS_TV
-    NSString *ownHostname = [[[VLCAppCoordinator sharedInstance] httpUploaderController] hostname];
-    if (!ownHostname)
+    if (!sender)
         return;
-    if ([[sender hostName] rangeOfString:ownHostname options:NSCaseInsensitiveSearch].location != NSNotFound) {
+
+    NSArray<NSData *> *serviceAddrs = sender.addresses;
+    if (serviceAddrs.count == 0)
         return;
+
+    NSArray<NSData *> *localAddrs = LocalInterfaceSocketAddresses();
+    if (localAddrs.count == 0)
+        return;
+
+    for (NSData *sData in serviceAddrs) {
+        const struct sockaddr *sSa = (const struct sockaddr *)sData.bytes;
+        if (!SocketAddressHasIP(sSa))
+            continue;
+
+        for (NSData *lData in localAddrs) {
+            const struct sockaddr *lSa = (const struct sockaddr *)lData.bytes;
+            if (SockaddrIPEqualIgnoringPort(sSa, lSa)) {
+                return;
+            }
+        }
     }
 #endif
     [self.httpParser checkNetserviceForVLCService:sender];
