c83f7d610803af21f6202f542bb3ff6e85eb4c08
Felix Paul Kühne <felix@feepk.net>
2026-02-09 06:18:08 +0100

    Cloud: move controller setup to controller
    A view controller is inappropriate to handle controller setup
    
    This reverts !1622 and fixes #1936

--- Sources/Cloud/Services/VLCCloudStorageController.m
+++ Sources/Cloud/Services/VLCCloudStorageController.m
@@ -1,22 +1,75 @@
-//
-//  VLCCloudStorageController.m
-//  VLC for iOS
-//
-//  Created by Carola Nitz on 31/12/14.
-//  Copyright (c) 2014 VideoLAN. All rights reserved.
-//
+/*****************************************************************************
+ * VLCCloudStorageController.m
+ * VLC for iOS
+ *****************************************************************************
+ * Copyright (c) 2014-2015, 2026 VideoLAN. All rights reserved.
+ * $Id$
+ *
+ * Authors: Carola Nitz <nitz.carola # googlemail.com>
+ *          Felix Paul Kühne <fkuehne # videolan.org>
+ *
+ * Refer to the COPYING file of the official project for license.
+ *****************************************************************************/
 
 #import "VLCCloudStorageController.h"
 
+#import "VLCBoxController.h"
+#import <OneDriveSDK.h>
+#import "VLCOneDriveConstants.h"
+#import "VLCDropboxController.h"
+#import "VLCDropboxConstants.h"
+#import "VLC-Swift.h"
+
+@interface VLCCloudStorageController()
+{
+    BOOL _controllersLoaded;
+}
+@end
+
 @implementation VLCCloudStorageController
 
-+ (VLCCloudStorageController *)sharedInstance {
-    return nil;
++ (VLCCloudStorageController *)sharedInstance
+{
+    static VLCCloudStorageController *sharedInstance = nil;
+    static dispatch_once_t pred;
+
+    dispatch_once(&pred, ^{
+        sharedInstance = [[VLCCloudStorageController alloc] init];
+    });
+
+    return sharedInstance;
 }
+
+- (void)configureCloudControllers
+{
+    if (_controllersLoaded) {
+        return;
+    }
+
+    VLCBoxController *boxController = [VLCBoxController sharedInstance];
+    // Start Box session on init to check whether it is logged in or not as soon as possible
+    [boxController startSession];
+
+    // Configure Dropbox
+    [DBClientsManager setupWithAppKey:kVLCDropboxAppKey];
+
+    // Configure OneDrive
+    [ODClient setMicrosoftAccountAppId:kVLCOneDriveClientID scopes:@[@"onedrive.readwrite", @"offline_access"]];
+
+    VLCPCloudController  *controller = [VLCPCloudController pCloudInstance];
+    // Start P Cloud session on init to check whether it is logged in or not as soon as possible
+    [controller startSession];
+
+    [DBClientsManager authorizedClient];
+
+    _controllersLoaded = YES;
+}
+
 - (void)startSession
 {
     // nop
 }
+
 - (void)logout
 {
     // nop
