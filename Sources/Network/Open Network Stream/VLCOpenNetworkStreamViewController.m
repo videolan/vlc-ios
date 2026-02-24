b68a962ce34d0630034a58cbff6b78803991c583
Ashis Kumar Naik <ashishami2002@gmail.com>
2026-02-24 21:54:28 +0530

    VLCOpenNetworkStreamViewController: Edit feature is deactivated if the list is empty
    State of edit button changes when the user removes all the Network streams from the list
    
    Signed-off-by: Ashis Kumar Naik <ashishami2002@gmail.com>

--- Sources/Network/Open Network Stream/VLCOpenNetworkStreamViewController.m
+++ Sources/Network/Open Network Stream/VLCOpenNetworkStreamViewController.m
@@ -56,6 +56,7 @@
     _recentURLs = [NSMutableArray arrayWithArray:[[NSUbiquitousKeyValueStore defaultStore] arrayForKey:kVLCRecentURLs]];
     _recentURLTitles = [NSMutableDictionary dictionaryWithDictionary:[[NSUbiquitousKeyValueStore defaultStore] dictionaryForKey:kVLCRecentURLTitles]];
     [self.historyTableView reloadData];
+    [self updateEditButtonState];
 }
 
 - (void)viewDidLoad
@@ -202,6 +203,7 @@
     self.navigationItem.rightBarButtonItem.title = NSLocalizedString(@"BUTTON_EDIT", nil);
     self.navigationItem.rightBarButtonItem.style = UIBarButtonItemStylePlain;
 
+    [self updateEditButtonState];
     [super viewWillAppear:animated];
 }
 
@@ -262,6 +264,7 @@
             [self _saveData];
 
             [self.historyTableView reloadData];
+            [self updateEditButtonState];
         }
     }
     [self.urlField resignFirstResponder];
@@ -421,6 +424,7 @@
 
         [tableView endEditing:NO];
         [tableView reloadData];
+        [self updateEditButtonState];
     }
 }
 
@@ -479,6 +483,17 @@ forRowAtIndexPath:(NSIndexPath *)indexPath
 }
 
 #pragma mark - internals
+- (void)updateEditButtonState
+{
+    BOOL hasItems = _recentURLs.count > 0;
+    self.navigationItem.rightBarButtonItem.enabled = hasItems;
+    if (!hasItems && self.historyTableView.editing) {
+        [self.historyTableView setEditing:NO animated:YES];
+        self.navigationItem.rightBarButtonItem.title = NSLocalizedString(@"BUTTON_EDIT", nil);
+        self.navigationItem.rightBarButtonItem.style = UIBarButtonItemStylePlain;
+    }
+}
+	
 - (void)_openURLStringAndDismiss:(NSString *)url
 {
     [[ParentalControlCoordinator sharedInstance] authorizeIfParentalControlIsEnabledWithAction:^{
