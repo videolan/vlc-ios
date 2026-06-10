videolan/vlc-ios

commit b33a3439031d7f62c23bc1a328af6f2341e02927

Media Library: Pagination Support

Sources/Media Library/MediaLibraryModel/AlbumModel.swift
@@ -31,6 +31,10 @@ class AlbumModel: AudioCollectionModel {
 
     var indicatorName: String = NSLocalizedString("ALBUMS", comment: "")
 
+    var intialPageSize = 12
+    var currentPage = 0
+    var firstTime = true
+
     required init(medialibrary: MediaLibraryService) {
         defer {
             fileArrayLock.unlock()
@@ -38,7 +42,6 @@ class AlbumModel: AudioCollectionModel {
         self.medialibrary = medialibrary
         medialibrary.observable.addObserver(self)
         fileArrayLock.lock()
-        files = medialibrary.albums()
     }
 
     init(medialibrary: MediaLibraryService, artist: VLCMLArtist) {
@@ -63,6 +66,21 @@ class AlbumModel: AudioCollectionModel {
         fileArrayLock.lock()
         files.append(item)
     }
+
+    func getMedia() {
+        print("page", intialPageSize)
+        currentPage += 1
+        let offset = (currentPage - 1) * intialPageSize
+        let mediaAtOffset = medialibrary.albums(sortingCriteria: sortModel.currentSort, desc: sortModel.desc, items: UInt32(intialPageSize), offset: UInt32(offset))
+
+           for  album in mediaAtOffset {
+                files.append(album)
+            }
+
+        observable.notifyObservers {
+            $0.mediaLibraryBaseModelReloadView()
+        }
+    }
 }
 
 // MARK: - Sort
@@ -72,23 +90,17 @@ extension AlbumModel {
             fileArrayLock.unlock()
         }
         fileArrayLock.lock()
-        if artist != nil {
-            var albums: [VLCMLAlbum] = []
-            medialibrary.albums(sortingCriteria: criteria, desc: desc).forEach() {
-                if let albumArtist = $0.albumArtist?.artistName(),
-                   let artist = self.artist?.artistName(),
-                   albumArtist == artist {
-                    albums.append($0)
-                }
-            }
-            files = albums
-        } else {
-            files = medialibrary.albums(sortingCriteria: criteria, desc: desc)
-        }
         sortModel.currentSort = criteria
         sortModel.desc = desc
-        observable.notifyObservers {
-            $0.mediaLibraryBaseModelReloadView()
+        if firstTime {
+            print("Intial Page" , intialPageSize)
+            getMedia()
+            firstTime = false
+        } else {
+            files.removeAll()
+            intialPageSize = 12
+            currentPage = 0
+            getMedia()
         }
     }
 }

Sources/Media Library/MediaCategories/MediaCategoryViewController.swift
@@ -60,6 +60,7 @@ class MediaCategoryViewController: UICollectionViewController, UISearchBarDelega
     private var toSize = CGSize.zero
     private var longPressGesture: UILongPressGestureRecognizer!
     weak var delegate: MediaCategoryViewControllerDelegate?
+    private var isScrolling = false
 
     private lazy var statusBarView: UIView = {
         let statusBarFrame: CGRect
@@ -528,6 +529,7 @@ class MediaCategoryViewController: UICollectionViewController, UISearchBarDelega
         if userDefaults.bool(forKey: kVLCSettingEnableScrollToCurrentlyPlayingMedia) && PlaybackService.sharedInstance().isPlaying {
              scrollToCurrentlyPlaying()
         }
+        loadSort()
     }
 
     override func viewWillDisappear(_ animated: Bool) {
@@ -732,6 +734,7 @@ class MediaCategoryViewController: UICollectionViewController, UISearchBarDelega
 
     override func scrollViewDidScroll(_ scrollView: UIScrollView) {
         // This ensures that the search bar is always visible like a sticky while searching
+        isScrolling = true
         if searchDataSource.isSearching {
             searchBar.endEditing(true)
             delegate?.enableCategorySwitching(for: self, enable: true)
@@ -1901,6 +1904,29 @@ extension MediaCategoryViewController: UICollectionViewDelegateFlowLayout {
         return cachedCellSize
     }
 
+    override func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+
+        if model.anyfiles.count == indexPath.row + 1 {
+            if model is CollectionModel {
+                return
+            }
+            else if model is ShowEpisodeModel {
+                return
+            }
+            else if model is HistoryModel {
+                return
+            }
+            else {
+                if isScrolling {
+                    model.getMedia()
+                }
+            }
+        }
+    }
+
+    override func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        isScrolling = false
+    }
     override func viewSafeAreaInsetsDidChange() {
         cachedCellSize = .zero
         collectionView?.collectionViewLayout.invalidateLayout()

Sources/Media Library/MediaCategoryCells/MediaCollectionViewCell.swift
@@ -637,7 +637,9 @@ class MediaCollectionViewCell: BaseCollectionViewCell, UIScrollViewDelegate {
         let titleHeight = UIFont.preferredFont(forTextStyle: .title3).lineHeight
         let subtitleHeight = UIFont.preferredFont(forTextStyle: .subheadline).lineHeight
 
-        return CGSize(width: cellWidth, height: titleHeight + subtitleHeight + edgePadding + interItemPadding * 2)
+        let finalHeight = titleHeight + subtitleHeight + edgePadding + interItemPadding * 2
+
+        return CGSize(width: cellWidth, height: finalHeight)
     }
 
     // MARK: - Handle  New label Text

Sources/Media Library/MediaCategoryCells/MediaGridCollectionCell.swift
@@ -389,8 +389,9 @@ class MediaGridCollectionCell: BaseCollectionViewCell {
         let cellWidth = floor(overallCellWidthWithoutPadding / numberOfCells)
         let titleHeight = UIFont.preferredCustomFont(forTextStyle: .headline).semibolded.lineHeight
         let newHeight = UIFont.preferredCustomFont(forTextStyle: .headline).bolded.lineHeight
+        let finalHeight = cellWidth * aspectRatio + (titleHeight * 2) + newHeight + (3 * 3)
 
-        return CGSize(width: cellWidth, height: cellWidth * aspectRatio + (titleHeight * 2) + newHeight + (3 * 3))
+        return CGSize(width: cellWidth, height: finalHeight)
     }
 
     override func prepareForReuse() {

Sources/Media Library/MediaLibraryModel/ArtistModel.swift
@@ -33,6 +33,10 @@ class ArtistModel: AudioCollectionModel {
         return UserDefaults.standard.bool(forKey: "\(kVLCAudioLibraryHideFeatArtists)")
     }
 
+    var intialPageSize = 12
+    var currentPage = 0
+    var firstTime = true
+
     required init(medialibrary: MediaLibraryService) {
         defer {
             fileArrayLock.unlock()
@@ -40,7 +44,6 @@ class ArtistModel: AudioCollectionModel {
         self.medialibrary = medialibrary
         medialibrary.observable.addObserver(self)
         fileArrayLock.lock()
-        files = hideFeatArtists ? medialibrary.artists(listAll: false) : medialibrary.artists()
     }
 
     func append(_ item: VLCMLArtist) {
@@ -81,6 +84,18 @@ class ArtistModel: AudioCollectionModel {
             }
         }
     }
+
+    func getMedia() {
+        currentPage += 1
+        let offset = (currentPage - 1) * intialPageSize
+        let mediaAtOffset = hideFeatArtists ? medialibrary.artists(sortingCriteria: sortModel.currentSort, desc: sortModel.desc, listAll: true, items: UInt32(intialPageSize), offset: UInt32(offset)) : medialibrary.artists(sortingCriteria: sortModel.currentSort, desc: sortModel.desc, listAll: false, items: UInt32(intialPageSize), offset: UInt32(offset))
+            for artist in mediaAtOffset {
+                files.append(artist)
+            }
+        observable.notifyObservers {
+            $0.mediaLibraryBaseModelReloadView()
+        }
+    }
 }
 
 // MARK: - Sort
@@ -89,13 +104,16 @@ extension ArtistModel {
         defer {
             fileArrayLock.unlock()
         }
-        fileArrayLock.lock()
-        files = hideFeatArtists ? medialibrary.artists(sortingCriteria: criteria, desc: desc, listAll: false) :
-                                medialibrary.artists(sortingCriteria: criteria, desc: desc, listAll: true)
         sortModel.currentSort = criteria
         sortModel.desc = desc
-        observable.notifyObservers {
-            $0.mediaLibraryBaseModelReloadView()
+        if firstTime {
+            getMedia()
+            firstTime = false
+        } else {
+            files.removeAll()
+            intialPageSize = 12
+            currentPage = 0
+            getMedia()
         }
     }
 }

Sources/Media Library/MediaLibraryModel/CollectionModel.swift
@@ -11,6 +11,14 @@
 import Foundation
 
 class CollectionModel: MLBaseModel {
+    var intialPageSize: Int = 0
+    var currentPage: Int = 0
+    var firstTime: Bool = false
+
+    func getMedia() {
+        // dummy
+    }
+
     typealias MLType = VLCMLMedia
 
     var sortModel = SortModel([.alpha, .duration, .insertionDate, .releaseDate, .fileSize, .lastPlaybackDate, .playCount])

Sources/Media Library/MediaLibraryModel/FolderModel.swift
@@ -20,6 +20,10 @@ class FolderModel: MLBaseModel {
     var folderMediaFiles = [VLCMLMedia]()
     var currentFolder: VLCMLFolder
 
+    var intialPageSize: Int = 0
+    var currentPage: Int = 0
+    var firstTime: Bool = false
+
     var name: String = "Folder"
     var indicatorName: String = NSLocalizedString("BUTTON_FOLDER", comment: "")
     var cellType: BaseCollectionViewCell.Type {
@@ -53,6 +57,10 @@ class FolderModel: MLBaseModel {
 
     func delete(_ items: [VLCMLFolder]) {
         // dummy function
+    }
+
+    func getMedia() {
+        // stub - FolderModel uses setupData() instead
     }
 
     func append(_ item: VLCMLFolder) {

Sources/Media Library/MediaLibraryModel/GenreModel.swift
@@ -29,6 +29,10 @@ class GenreModel: AudioCollectionModel {
 
     var indicatorName: String = NSLocalizedString("GENRES", comment: "")
 
+    var intialPageSize = 12
+    var currentPage = 0
+    var firstTime = true
+
     required init(medialibrary: MediaLibraryService) {
         defer {
             fileArrayLock.unlock()
@@ -36,7 +40,7 @@ class GenreModel: AudioCollectionModel {
         self.medialibrary = medialibrary
         medialibrary.observable.addObserver(self)
         fileArrayLock.lock()
-        files = medialibrary.genres()
+        getMedia()
     }
 
     func append(_ item: VLCMLGenre) {
@@ -45,6 +49,20 @@ class GenreModel: AudioCollectionModel {
         }
         fileArrayLock.lock()
         files.append(item)
+    }
+
+    func getMedia() {
+        currentPage += 1
+        let offset = (currentPage - 1) * intialPageSize
+        let mediaAtOffset = medialibrary.medialib.genres(with: sortModel.currentSort, desc: sortModel.desc, UInt32(intialPageSize), UInt32(offset))
+        if let generes = mediaAtOffset {
+            for  genre in  generes {
+                files.append(genre)
+            }
+        }
+        observable.notifyObservers {
+            $0.mediaLibraryBaseModelReloadView()
+        }
     }
 }
 
@@ -108,11 +126,16 @@ extension GenreModel {
             fileArrayLock.unlock()
         }
         fileArrayLock.lock()
-        files = medialibrary.genres(sortingCriteria: criteria, desc: desc)
         sortModel.currentSort = criteria
         sortModel.desc = desc
-        observable.notifyObservers {
-            $0.mediaLibraryBaseModelReloadView()
+        if firstTime {
+            getMedia()
+            firstTime = false
+        } else {
+            files.removeAll()
+            intialPageSize = 12
+            currentPage = 0
+            getMedia()
         }
     }
 }

commit db95f5babc20f9c31d8070cf24e3a7147f52070d

AlbumModel: Restore the albums filtering by artist
This brings back the albums filtering when an artist is selected in
the Audio tab.

Fixes a regression introduced with: b33a3439031d7f62c23bc1a328af6f2341e02927

Sources/Media Library/MediaLibraryModel/AlbumModel.swift
@@ -57,10 +57,25 @@ class AlbumModel: AudioCollectionModel {
     }
 
     func fetchPage(offset: Int, limit: Int) -> [VLCMLAlbum] {
-        return medialibrary.albums(sortingCriteria: sortModel.currentSort,
-                                   desc: sortModel.desc,
-                                   items: UInt32(limit),
-                                   offset: UInt32(offset))
+        var albums: [VLCMLAlbum] = []
+        if let artist = artist {
+            medialibrary.albums(sortingCriteria: sortModel.currentSort,
+                                desc: sortModel.desc,
+                                items: UInt32(limit),
+                                offset: UInt32(offset)).forEach { album in
+                if let albumArtist = album.albumArtist?.artistName(),
+                   albumArtist == artist.artistName() {
+                    albums.append(album)
+                }
+            }
+        } else {
+            albums = medialibrary.albums(sortingCriteria: sortModel.currentSort,
+                                                 desc: sortModel.desc,
+                                                 items: UInt32(limit),
+                                                 offset: UInt32(offset))
+        }
+
+        return albums
     }
 }
 