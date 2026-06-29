videolan/vlc-ios

commit be95186bce979ba7cfb46757e7040466e18d3f81

media category VC: update archiving API use

Sources/Media Library/MediaCategories/MediaCategoryViewController.swift
@@ -265,7 +265,7 @@ class MediaCategoryViewController: UICollectionViewController, UISearchBarDelega
     private var lastPlaylist: LastPlayedPlaylistModel? {
         let encodedLastPlaylist = userDefaults.data(forKey: kVLCLastPlayedPlaylist)
         guard let encodedData = encodedLastPlaylist,
-              let lastPlayed = NSKeyedUnarchiver(forReadingWith: encodedData).decodeObject(forKey: "root") as? LastPlayedPlaylistModel else {
+              let lastPlayed = (try? NSKeyedUnarchiver(forReadingFrom: encodedData))?.decodeObject(forKey: "root") as? LastPlayedPlaylistModel else {
             return nil
         }
         return lastPlayed
@@ -2583,7 +2583,7 @@ extension MediaCategoryViewController {
 
         let lastMedia = LastPlayed(identifier: media.identifier(), title: media.title)
         let playlistInfo = LastPlayedPlaylistModel(identifier: playlistId, title: playlistTitle, lastPlayedMedia: lastMedia)
-        userDefaults.setValue(NSKeyedArchiver.archivedData(withRootObject: playlistInfo), forKey: kVLCLastPlayedPlaylist)
+        userDefaults.setValue(try? NSKeyedArchiver.archivedData(withRootObject: playlistInfo, requiringSecureCoding: false), forKey: kVLCLastPlayedPlaylist)
     }
 
     private func addPlaybackWillStopObserver() {

commit 93252f40821efb6478f7fdd0daebad6472c34a00

Media Category VC: fix unarchiving regression
Regression from be95186

Sources/Media Library/MediaCategories/MediaCategoryViewController.swift
@@ -263,12 +263,12 @@ class MediaCategoryViewController: UICollectionViewController, UISearchBarDelega
     }()
 
     private var lastPlaylist: LastPlayedPlaylistModel? {
-        let encodedLastPlaylist = userDefaults.data(forKey: kVLCLastPlayedPlaylist)
-        guard let encodedData = encodedLastPlaylist,
-              let lastPlayed = (try? NSKeyedUnarchiver(forReadingFrom: encodedData))?.decodeObject(forKey: "root") as? LastPlayedPlaylistModel else {
+        guard let encodedData = userDefaults.data(forKey: kVLCLastPlayedPlaylist),
+              let unarchiver = try? NSKeyedUnarchiver(forReadingFrom: encodedData) else {
             return nil
         }
-        return lastPlayed
+        unarchiver.requiresSecureCoding = false
+        return unarchiver.decodeObject(forKey: "root") as? LastPlayedPlaylistModel
     }
 
     // Indicating that the current chosen collection to play is playlist, useful for handling Observer