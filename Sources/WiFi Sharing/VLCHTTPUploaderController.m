videolan/vlc-ios

commit d2dd54d4eedb0baa3dbff4a2e27ca8526a2f65d9

Wifi Sharing: adapt for visionOS

Sources/WiFi Sharing/VLCHTTPUploaderController.m
@@ -27,7 +27,7 @@
 
 #import "NSString+SupportedMedia.h"
 
-#if TARGET_OS_IOS
+#if TARGET_OS_IOS || TARGET_OS_VISION
 #import "VLC-Swift.h"
 #import "VLCMediaFileDiscoverer.h"
 #endif
@@ -181,12 +181,14 @@ NSString *VLCHTTPUploaderBackgroundTaskName = @"VLCHTTPUploaderBackgroundTaskNam
         _nameOfUsedNetworkInterface = preferredipv6Interface;
     }
     if (_nameOfUsedNetworkInterface == nil) {
+        APLog(@"No suitable interface was found");
         _isReachable = NO;
         [self changeHTTPServerState:NO];
         return;
     }
     _isReachable = YES;
     if (serverWasRunning) {
+        APLog(@"Server was running, restart");
         [self changeHTTPServerState:YES];
     }
 }
@@ -229,11 +231,12 @@ NSString *VLCHTTPUploaderBackgroundTaskName = @"VLCHTTPUploaderBackgroundTaskNam
 
 - (BOOL)changeHTTPServerState:(BOOL)state
 {
-    if (!state) {
+    /* if (!state) {
+        APLog(@"Stopping server");
         [_httpServer stop];
         [self endBackgroundTask];
         return true;
-    }
+    }*/
 
     if (_nameOfUsedNetworkInterface == nil) {
         APLog(@"No interface to listen on, server not started");
@@ -242,7 +245,7 @@ NSString *VLCHTTPUploaderBackgroundTaskName = @"VLCHTTPUploaderBackgroundTaskNam
         return NO;
     }
 
-#if TARGET_OS_IOS
+#if TARGET_OS_IOS || TARGET_OS_VISION
     // clean cache before accepting new stuff
     [self cleanCache];
 #endif
@@ -273,7 +276,7 @@ NSString *VLCHTTPUploaderBackgroundTaskName = @"VLCHTTPUploaderBackgroundTaskNam
     if (![_httpServer start:&error]) {
         if (error.code == EACCES) {
             APLog(@"Port forbidden by OS, trying another one");
-            [_httpServer setPort:8888];
+            [_httpServer setPort:0];
             if (![_httpServer start:&error]) {
                 [self beginBackgroundTask];
                 return true;
@@ -354,7 +357,7 @@ NSString *VLCHTTPUploaderBackgroundTaskName = @"VLCHTTPUploaderBackgroundTaskNam
     return [NSString stringWithFormat:@"%i", _httpServer.listeningPort];
 }
 
-#if TARGET_OS_IOS
+#if TARGET_OS_IOS || TARGET_OS_VISION
 - (void)moveFileOutOfCache:(NSString *)filepath
 {
     NSString *fileName = [filepath lastPathComponent];
@@ -426,12 +429,12 @@ NSString *VLCHTTPUploaderBackgroundTaskName = @"VLCHTTPUploaderBackgroundTaskNam
 
     /* on tvOS, the media remains in the cache folder and will disappear from there
      * while on iOS we have persistent storage, so move it there */
-#if TARGET_OS_IOS
+#if TARGET_OS_IOS || TARGET_OS_VISION
     [self moveFileOutOfCache:filepath];
 #endif
 }
 
-#if TARGET_OS_IOS
+#if TARGET_OS_IOS || TARGET_OS_VISION
 // never clean the cache on tvOS as we use it as a Documents folder replacement
 - (void)cleanCache
 {

Sources/WiFi Sharing/VLCHTTPConnection.m
@@ -53,12 +53,12 @@
 
 @implementation VLCHTTPConnection
 
-#if TARGET_OS_IOS
+#if TARGET_OS_IOS || TARGET_OS_VISION
 static NSMutableDictionary *authentificationAttemptsHosts;
 static NSMutableDictionary *authentifiedHosts;
 #endif
 
-#if TARGET_OS_IOS
+#if TARGET_OS_IOS || TARGET_OS_VISION
 + (void)initialize
 {
     authentificationAttemptsHosts = [[NSMutableDictionary alloc] init];
@@ -170,7 +170,7 @@ static NSMutableDictionary *authentifiedHosts;
     return [super expectsRequestBodyFromMethod:method atPath:path];
 }
 
-#if TARGET_OS_IOS
+#if TARGET_OS_IOS || TARGET_OS_VISION
 - (int)authenticate:(NSString *)host
 {
     NSDictionary *params = [self parseGetParams];
@@ -272,7 +272,7 @@ static NSMutableDictionary *authentifiedHosts;
     NSError *error;
     NSURLRelationship relationship;
 
-#if TARGET_OS_IOS
+#if TARGET_OS_IOS || TARGET_OS_VISION
     NSArray *searchPaths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
 #else
     NSArray *searchPaths = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES);
@@ -313,7 +313,7 @@ static NSMutableDictionary *authentifiedHosts;
     return dataResponse;
 }
 
-#if TARGET_OS_IOS
+#if TARGET_OS_IOS || TARGET_OS_VISION
 - (NSObject<HTTPResponse> *)_httpGETLibraryForPath:(NSString *)path
 {
     NSString *filePath = [self filePathForURI:path];
@@ -372,7 +372,7 @@ static NSMutableDictionary *authentifiedHosts;
                         stringByReplacingOccurrencesOfString:@"'" withString:@"&#039;"];
 }
 
-#if TARGET_OS_IOS
+#if TARGET_OS_IOS || TARGET_OS_VISION
 - (NSString *)createHTMLMediaObjectFromMedia:(VLCMLMedia *)media
 {
     return [NSString stringWithFormat:
@@ -439,7 +439,7 @@ static NSMutableDictionary *authentifiedHosts;
     NSString *deviceModel = [[UIDevice currentDevice] model];
     NSMutableArray *mediaInHtml = [[NSMutableArray alloc] initWithCapacity:media.count];
 
-#if TARGET_OS_IOS
+#if TARGET_OS_IOS || TARGET_OS_VISION
     for (NSObject <VLCMLObject> *mediaObject in media) {
         if ([mediaObject isKindOfClass:[VLCMLMedia class]]) {
             [mediaInHtml addObject:[self createHTMLMediaObjectFromMedia:(VLCMLMedia *)mediaObject]];
@@ -509,7 +509,7 @@ static NSMutableDictionary *authentifiedHosts;
     return fileResponse;
 }
 
-#if TARGET_OS_IOS
+#if TARGET_OS_IOS || TARGET_OS_VISION
 - (HTTPDynamicFileResponse *)generateXMLResponseFrom:(NSArray *)media path:(NSString *)path
 {
     NSMutableArray *mediaInXml = [[NSMutableArray alloc] initWithCapacity:media.count];
@@ -579,7 +579,7 @@ static NSMutableDictionary *authentifiedHosts;
 
 - (NSObject<HTTPResponse> *)_httpGETCSSForPath:(NSString *)path
 {
-#if TARGET_OS_IOS
+#if TARGET_OS_IOS || TARGET_OS_VISION
     NSDictionary *replacementDict = @{@"WEBINTF_TITLE" : _webInterfaceTitle};
 #else
     NSDictionary *replacementDict = @{@"WEBINTF_TITLE" : NSLocalizedString(@"WEBINTF_TITLE_ATV", nil)};
@@ -723,7 +723,7 @@ static NSMutableDictionary *authentifiedHosts;
     if ([path hasPrefix:@"/Thumbnail/"]) {
         return [self _httpGETThumbnailForPath:path];
     }
-#if TARGET_OS_IOS
+#if TARGET_OS_IOS || TARGET_OS_VISION
     if ([path hasPrefix:@"/public/auth.html"]) {
         return [self _httpGETAuthentification];
     }
@@ -931,7 +931,7 @@ static NSMutableDictionary *authentifiedHosts;
     if (_storeFile) {
         @try {
             [_storeFile writeData:data];
-#if TARGET_OS_IOS
+#if TARGET_OS_IOS || TARGET_OS_VISION
             [_httpUploaderController resetIdleTimer];
 #endif
         }

Sources/WiFi Sharing/VLCHTTPUploaderController.h
@@ -33,7 +33,7 @@
 
 - (void)moveFileFrom:(nonnull NSString *)filepath;
 
-#if TARGET_OS_IOS
+#if TARGET_OS_IOS || TARGET_OS_VISION
 - (void)cleanCache;
 - (void)resetIdleTimer;
 #endif

commit 967bb623344457a8ff2e5ceaddf8359bffaa57f9

http uploader: allow disabling the server
Accidental regression from d2dd54d4e. Fixes #2269

Sources/WiFi Sharing/VLCHTTPUploaderController.m
@@ -231,12 +231,12 @@ NSString *VLCHTTPUploaderBackgroundTaskName = @"VLCHTTPUploaderBackgroundTaskNam
 
 - (BOOL)changeHTTPServerState:(BOOL)state
 {
-    /* if (!state) {
+    if (!state) {
         APLog(@"Stopping server");
         [_httpServer stop];
         [self endBackgroundTask];
         return true;
-    }*/
+    }
 
     if (_nameOfUsedNetworkInterface == nil) {
         APLog(@"No interface to listen on, server not started");