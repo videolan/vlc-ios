videolan/vlc-ios

commit 38e55f8c4300f5e1d5bfaa8ff006fa3c2ad598cf

VLCWatchOSApp: Add starter code for watchOS
This commit adds the required WatchConnectivity APIs to enable iPhone
and watch communication. The iPhone and watch can now send data back
and forth.

On the watchOS side, this adds 6 additional tabs to the watch app for
testing iPhone to watch communication and uses TimedColor as the data
model being transfered.

Note: TimedColor and many objects are left purely for testing purposes
and serves as an example of how the watch APIs work.

This commit heavily references Apple's sample code.
https://developer.apple.com/documentation/WatchConnectivity/transferring-data-with-watch-connectivity

Sources/Media Library/Editing/EditButtons.swift
@@ -20,6 +20,14 @@
     case playNextInQueue
     case appendToQueue
     case playAsAudio
+
+    // For testing, remove later
+    case updateAppContext
+    case sendMessage
+    case sendMessageData
+    case transferFile
+    case transferUserInfo
+    case transferComplicationUserInfo
 }
 
 class EditButton {
@@ -82,6 +90,15 @@ class EditButtonsFactory {
         }
         actionList.append(.delete)
         actionList.append(.share)
+
+        // For testing
+//        actionList.append(.updateAppContext)
+//        actionList.append(.sendMessage)
+//        actionList.append(.sendMessageData)
+        actionList.append(.transferFile)
+//        actionList.append(.transferUserInfo)
+//        actionList.append(.transferComplicationUserInfo)
+
         return actionList
     }
 
@@ -152,6 +169,42 @@ class EditButtonsFactory {
                                                   image: "Audio",
                                                   accessibilityLabel: NSLocalizedString("PLAY_AS_AUDIO", comment: ""),
                                                   accessibilityHint: NSLocalizedString("PLAY_AS_AUDIO_HINT", comment: "")))
+                case .updateAppContext:
+                editButtons.append(EditButton(identifier: button,
+                                              title: "Update App Context",
+                                              image: "addToPlaylist",
+                                              accessibilityLabel: NSLocalizedString("PLAY_AS_AUDIO", comment: ""),
+                                              accessibilityHint: NSLocalizedString("PLAY_AS_AUDIO_HINT", comment: "")))
+                case .sendMessage:
+                editButtons.append(EditButton(identifier: button,
+                                              title: "Send message",
+                                              image: "MiniPlay",
+                                              accessibilityLabel: NSLocalizedString("PLAY_AS_AUDIO", comment: ""),
+                                              accessibilityHint: NSLocalizedString("PLAY_AS_AUDIO_HINT", comment: "")))
+                case .sendMessageData:
+                editButtons.append(EditButton(identifier: button,
+                                              title: "Send message data",
+                                              image: "addToMediaGroup",
+                                              accessibilityLabel: NSLocalizedString("PLAY_AS_AUDIO", comment: ""),
+                                              accessibilityHint: NSLocalizedString("PLAY_AS_AUDIO_HINT", comment: "")))
+                case .transferFile:
+                editButtons.append(EditButton(identifier: button,
+                                              title: "Transfer File",
+                                              image: "playNextInQueue",
+                                              accessibilityLabel: NSLocalizedString("PLAY_AS_AUDIO", comment: ""),
+                                              accessibilityHint: NSLocalizedString("PLAY_AS_AUDIO_HINT", comment: "")))
+                case .transferUserInfo:
+                editButtons.append(EditButton(identifier: button,
+                                              title: "Transfer User Info",
+                                              image: "share",
+                                              accessibilityLabel: NSLocalizedString("PLAY_AS_AUDIO", comment: ""),
+                                              accessibilityHint: NSLocalizedString("PLAY_AS_AUDIO_HINT", comment: "")))
+                case .transferComplicationUserInfo:
+                editButtons.append(EditButton(identifier: button,
+                                              title: "Transfer Complication User Info",
+                                              image: "delete",
+                                              accessibilityLabel: NSLocalizedString("PLAY_AS_AUDIO", comment: ""),
+                                              accessibilityHint: NSLocalizedString("PLAY_AS_AUDIO_HINT", comment: "")))
             }
         }
         return editButtons

Sources/App/iOS/VLCAppDelegate.m
@@ -23,12 +23,16 @@
 #import "VLC-Swift.h"
 #import "VLCAppSceneDelegate.h"
 #import "VLCMLMedia+isWatched.h"
+#import <WatchConnectivity/WatchConnectivity.h>
 
 @interface VLCAppDelegate ()
 {
     BOOL _isComingFromHandoff;
     id<VLCURLHandler> _urlHandlerToExecute;
     NSURL *_urlToHandle;
+#if TARGET_OS_IOS || TARGET_OS_WATCH
+    VLCSessionDelegate *sessionDelegate;
+# endif
 }
 
 @end
@@ -91,12 +95,12 @@
                                   kVLCSettingCastingAudioPassthrough : @(NO),
                                   kVLCSettingCastingConversionQuality : @(2),
                                   kVLCForceSMBV1 : @(YES),
-                                  @"kVLCAudioLibraryGridLayoutALBUMS" : @(YES),
-                                  @"kVLCAudioLibraryGridLayoutARTISTS" : @(YES),
-                                  @"kVLCAudioLibraryGridLayoutGENRES" : @(YES),
-                                  @"kVLCVideoLibraryGridLayoutALL_VIDEOS" : @(YES),
-                                  @"kVLCVideoLibraryGridLayoutVIDEO_GROUPS" : @(YES),
-                                  @"kVLCVideoLibraryGridLayoutVLCMLMediaGroupCollections" : @(YES),
+                                  kVLCAudioLibraryGridLayoutALBUMS : @(YES),
+                                  kVLCAudioLibraryGridLayoutARTISTS : @(YES),
+                                  kVLCAudioLibraryGridLayoutGENRES : @(YES),
+                                  kVLCVideoLibraryGridLayoutALL_VIDEOS : @(YES),
+                                  kVLCVideoLibraryGridLayoutVIDEO_GROUPS : @(YES),
+                                  kVLCVideoLibraryGridLayoutVLCMLMediaGroupCollections : @(YES),
                                   kVLCPlayerShouldRememberState: @(YES),
                                   kVLCPlayerIsShuffleEnabled: kVLCPlayerIsShuffleEnabledDefaultValue,
                                   kVLCPlayerIsRepeatEnabled: kVLCPlayerIsRepeatEnabledDefaultValue,
@@ -173,6 +177,14 @@
     if (shortcutItem) {
         [[VLCAppCoordinator sharedInstance] handleShortcutItem:shortcutItem];
     }
+
+#if TARGET_OS_IOS || TARGET_OS_WATCH
+    if ([WCSession isSupported]) {
+        sessionDelegate = [[VLCSessionDelegate alloc] init];
+        [WCSession defaultSession].delegate = sessionDelegate;
+        [[WCSession defaultSession] activateSession];
+    }
+#endif
 
     return YES;
 }

Sources/App/watchOS/ContentView.swift
@@ -1,83 +1,0 @@
-//
-//  ContentView.swift
-//  VLC-watchOS
-//
-//  Created by Fahri Novaldi on 10/06/25.
-//  Copyright © 2025 VideoLAN. All rights reserved.
-//
-
-import SwiftUI
-import VLCKit
-
-struct ContentView: View {
-    @EnvironmentObject var dialogProvider: CustomSwiftUIDialogProvider
-    
-    let dialogBridge: CustomSwiftUIDialogObjCBridge = CustomSwiftUIDialogObjCBridge()
-    
-    var body: some View {
-        ScrollView {
-            VStack(spacing: 20) {
-                Image(systemName: "cone")
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
-                
-                // Test shared-localizable strings
-                Text(NSLocalizedString("CHOOSE_CHAPTER", comment: ""))
-                    .onAppear {
-                        // This is a simple test to ensure that VLCKit is properly linked and can be used in the watchOS app.
-                        let sampleObjectFromVLCKit = VLCMedia()
-                        
-                        print("Sample object from VLCKit: \(sampleObjectFromVLCKit)")
-                        // This is just to ensure that VLCKit is properly linked and can be used.
-                    }
-                
-                VStack(spacing: 12) {
-                    Button("Show play-pause dialog") {
-                        dialogBridge.showContinuePlaybackDialog(mediaTitle: "Sample song") { shouldContinue in
-                            print("User chose to \(shouldContinue ? "continue" : "not continue") playback.")
-                        }
-                    }
-                    .foregroundColor(.white)
-                    .padding()
-                    .background(Color.blue)
-                    .cornerRadius(8)
-                    
-                    Button("Show Dialog") {
-                        dialogProvider.show {
-                            VStack(spacing: 12) {
-                                Text("This is a custom dialog")
-                                    .font(.headline)
-                                
-                                Button("Dismiss") {
-                                    dialogProvider.dismiss()
-                                }
-                            }
-                            .ignoresSafeArea()
-                            .frame(maxWidth: .infinity, maxHeight: .infinity)
-                            .padding()
-                            .background(Color.red)
-                            .cornerRadius(12)
-                        }
-                    }
-                    .foregroundColor(.white)
-                    .padding()
-                    .background(Color.blue)
-                    .cornerRadius(8)
-                    
-                    Button("Test Error Dialog (objc)") {
-                        dialogBridge.showErrorDialog(title: "Error", message: "This is a test error dialog from Objective-C bridge.")
-                    }
-                    .foregroundColor(.white)
-                    .padding()
-                    .background(Color.orange)
-                    .cornerRadius(8)
-                }
-            }
-            .padding()
-        }
-    }
-}
-
-#Preview {
-    ContentView()
-}

Sources/App/watchOS/Models/VLCWatchMLMedia.swift
@@ -1,0 +1,28 @@
+/*****************************************************************************
+ * VLCWatchMLMedia.swift
+ * VLC for iOS
+ *****************************************************************************
+ * Copyright (c) 2026 VideoLAN. All rights reserved.
+ * $Id$
+ *
+ * Authors: Timmy Nguyen <timmypass21 # gmail.com>
+ *
+ * Refer to the COPYING file of the official project for license.
+ *****************************************************************************/
+
+import Foundation
+
+// Wrapper around VLCMLMedia to be used in SwiftUI view
+struct VLCWatchMLMedia: Identifiable {
+    let id: VLCMLIdentifier
+    let title: String
+    let artist: VLCMLArtist?    // TODO: VLCMLArtist is a class, is this fine to have as field for SwiftUI struct?
+    let thumbnail: URL?
+
+    init(_ media: VLCMLMedia) {
+        self.id = media.identifier()
+        self.title = media.title
+        self.artist = media.artist
+        self.thumbnail = media.thumbnail()
+    }
+}

Sources/App/watchOS/VLCWatchAppDelegate.swift
@@ -1,0 +1,171 @@
+//
+//  VLCWatchAppDelegate.swift
+//  VLC-watchOS
+//
+//  Created by Timmy Nguyen on 5/13/26.
+//  Copyright © 2026 VideoLAN. All rights reserved.
+//
+
+import Foundation
+import WatchKit
+import WatchConnectivity
+
+class VLCWatchAppDelegate: NSObject, WKApplicationDelegate {
+
+    private lazy var sessionDelegate: VLCSessionDelegate = {
+        return VLCSessionDelegate()
+    }()
+
+    // Hold the KVO observers to keep observing the change in the extension's lifetime.
+    //
+    private var activationStateObservation: NSKeyValueObservation?
+    private var hasContentPendingObservation: NSKeyValueObservation?
+
+    // An array to keep the background tasks.
+    //
+    private var wcBackgroundTasks = [WKWatchConnectivityRefreshBackgroundTask]()
+
+    override init() {
+        super.init()
+        assert(WCSession.isSupported(), "This sample requires a platform supporting Watch Connectivity!")
+
+        let appDefaults: [String : Any] = [
+            kVLCSettingAppTheme : kVLCSettingAppThemeBright,
+            kVLCSettingPasscodeEnableBiometricAuth : 1,
+            kVLCSettingContinueAudioInBackgroundKey : true,
+            kVLCSettingStretchAudio : true,
+            kVLCSettingDefaultPreampLevel : 6,
+            kVLCSettingTextEncoding : kVLCSettingTextEncodingDefaultValue,
+            kVLCSettingSkipLoopFilter : 1,
+            kVLCSettingSubtitlesFont : kVLCSettingSubtitlesFontDefaultValue,
+            kVLCSettingSubtitlesFontColor : kVLCSettingSubtitlesFontColorDefaultValue,
+            kVLCSettingSubtitlesFontSize : kVLCSettingSubtitlesFontSizeDefaultValue,
+            kVLCSettingSubtitlesBoldFont: false,
+            kVLCSettingDeinterlace : -1,
+            kVLCSettingHardwareDecoding : kVLCSettingHardwareDecodingDefault,
+            kVLCSettingNetworkCaching : 999,
+            kVLCSettingVolumeGesture : true,
+            kVLCSettingPlayPauseGesture : true,
+            kVLCSettingBrightnessGesture : true,
+            kVLCSettingSeekGesture : true,
+            kVLCSettingCloseGesture : true,
+            kVLCSettingSnapshotGesture : false,
+            kVLCSettingPlaybackLongTouchSpeedUp : true,
+            kVLCSettingVideoFullscreenPlayback : true,
+            kVLCSettingContinuePlayback : 1,
+            kVLCSettingContinueAudioPlayback : 1,
+            kVLCSettingWiFiSharingIPv6 : false,
+            kVLCSettingNetworkRTSPTCP : false,
+            kVLCSettingNetworkRTSPHTTP : false,
+            kVLCSettingNetworkSatIPChannelListUrl : "",
+            kVLCSettingEqualizerProfile : 0,
+            kVLCSettingEqualizerProfileDisabled : true,
+            kVLCSettingPlaybackForwardBackwardEqual: true,
+            kVLCSettingPlaybackTapSwipeEqual:  true,
+            kVLCSettingPlaybackForwardSkipLength : 10,
+            kVLCSettingPlaybackBackwardSkipLength : 10,
+            kVLCSettingPlaybackForwardSkipLengthSwipe : 10,
+            kVLCSettingPlaybackBackwardSkipLengthSwipe : 10,
+            kVLCSettingPlaybackLockscreenSkip : false,
+            kVLCSettingPlaybackRemoteControlSkip : false,
+            kVLCSettingOpenAppForPlayback : true,
+            kVLCAutomaticallyPlayNextItem : true,
+            kVLCPlaylistPlayNextItem: true,
+            kVLCSettingEnableMediaCellTextScrolling : false,
+            kVLCSettingShowThumbnails : true,
+            kVLCSettingShowArtworks : true,
+            kVLCSettingBackupMediaLibrary : false,
+            kVLCSettingCastingAudioPassthrough : false,
+            kVLCSettingCastingConversionQuality : 2,
+            kVLCForceSMBV1 : true,
+            kVLCAudioLibraryGridLayoutALBUMS : true,
+            kVLCAudioLibraryGridLayoutARTISTS : true,
+            kVLCAudioLibraryGridLayoutGENRES : true,
+            kVLCVideoLibraryGridLayoutALL_VIDEOS : true,
+            kVLCVideoLibraryGridLayoutVIDEO_GROUPS : true,
+            kVLCVideoLibraryGridLayoutVLCMLMediaGroupCollections : true,
+            kVLCPlayerShouldRememberState: true,
+            kVLCPlayerIsShuffleEnabled: false,
+            kVLCPlayerIsRepeatEnabled: 0,
+            kVLCSettingPlaybackSpeedDefaultValue: 1.0,
+            kVLCPlayerShowPlaybackSpeedShortcut: false,
+            kVLCSettingAlwaysPlayURLs: false,
+            kVLCRestoreLastPlayedMedia: false,
+            kVLCSettingPlayerControlDuration: 4,
+            kVLCSettingPauseWhenShowingControls: false
+        ]
+
+        UserDefaults.standard.register(defaults: appDefaults)
+
+        // Apps must complete WKWatchConnectivityRefreshBackgroundTask. Otherwise, tasks keep consuming
+        // the background executing time and eventually cause a crash.
+        // The timing to complete the tasks is when the current WCSession turns to a state other than .activated
+        // or hasContentPending flips false (see completeBackgroundTasks), so use KVO to observe
+        // the changes of the two properties.
+        //
+        activationStateObservation = WCSession.default.observe(\.activationState) { _, _ in
+            DispatchQueue.main.async {
+                self.completeBackgroundTasks()
+            }
+        }
+        hasContentPendingObservation = WCSession.default.observe(\.hasContentPending) { _, _ in
+            DispatchQueue.main.async {
+                self.completeBackgroundTasks()
+            }
+        }
+
+        // Activate the session asynchronously as early as possible.
+        // When the system needs to launch the app to run a background task, this saves some background runtime budget.
+        //
+        WCSession.default.delegate = sessionDelegate
+        WCSession.default.activate()
+    }
+
+    // Complete the background tasks, and schedule a snapshot refresh.
+    //
+    func completeBackgroundTasks() {
+        guard !wcBackgroundTasks.isEmpty else { return }
+
+        guard WCSession.default.activationState == .activated,
+            WCSession.default.hasContentPending == false else { return }
+
+        wcBackgroundTasks.forEach { $0.setTaskCompletedWithSnapshot(false) }
+
+        // Use Logger to log tasks for debugging purposes.
+        //
+        print("\(#function):\(wcBackgroundTasks) was completed!")
+
+        // Schedule a snapshot refresh if the UI is updated by background tasks.
+        //
+        let date = Date(timeIntervalSinceNow: 1)
+        WKApplication.shared().scheduleSnapshotRefresh(withPreferredDate: date, userInfo: nil) { error in
+
+            if let error = error {
+                print("scheduleSnapshotRefresh error: \(error)!")
+            }
+        }
+        wcBackgroundTasks.removeAll()
+    }
+
+    // Apps must complete WKWatchConnectivityRefreshBackgroundTask after the pending data is received.
+    // This sample retains the tasks first, and complete them in the following cases:
+    // 1. hasContentPending flips false, meaning no pending data waiting for processing. Pending data means
+    //    the data the device receives prior to when the WCSession gets activated.
+    //    More data might arrive, but it isn't pending when the session gets activated.
+    // 2. The end of the handle method.
+    //    This happens when hasContentPending flips to false before the app retains the tasks.
+    //
+
+    func handle(_ backgroundTasks: Set<WKRefreshBackgroundTask>) {
+        for task in backgroundTasks {
+            if let wcTask = task as? WKWatchConnectivityRefreshBackgroundTask {
+                wcBackgroundTasks.append(wcTask)
+                print("\(#function):\(wcTask.description) was appended!")
+            } else {
+                task.setTaskCompletedWithSnapshot(false)
+                print("\(#function):\(task.description) was completed!")
+            }
+        }
+        completeBackgroundTasks()
+    }
+}

Sources/App/watchOS/VLCWatchOSApp.swift
@@ -1,23 +1,92 @@
-//
-//  VLCWatchOSApp.swift
-//  VLC-watchOS
-//
-//  Created by Fahri Novaldi on 10/06/25.
-//  Copyright © 2025 VideoLAN. All rights reserved.
-//
+/*****************************************************************************
+ * VLCWatchOSApp.swift
+ * VLC for iOS
+ *****************************************************************************
+ * Copyright (c) 2026 VideoLAN. All rights reserved.
+ * $Id$
+ *
+ * Authors: Timmy Nguyen <timmypass21 # gmail.com>
+ *
+ * Refer to the COPYING file of the official project for license.
+ *****************************************************************************/
 
 import SwiftUI
+import Combine
+import WatchConnectivity
 
 @main
 struct VLCWatchOSApp: App {
+    @WKApplicationDelegateAdaptor var appDelegate: VLCWatchAppDelegate
+//    @Environment(\.isLuminanceReduced) var isLuminanceReduced
+
+    @StateObject var tracksViewModel: TracksViewModel
+    @State private var selection: Command = .updateAppContext
+
     private let appCoordinator = VLCAppCoordinator.sharedInstance()
-    @StateObject private var dialogProvider = CustomSwiftUIDialogProvider.shared
-    
+
+    // For testing WatchConnectivity APIs
+    let commands: [Command] = [.updateAppContext, .sendMessage, .sendMessageData,
+                               .transferFile, .transferUserInfo,
+                               .transferCurrentComplicationUserInfo]
+
+    let mediaLibraryService: MediaLibraryService
+    let playbackService: PlaybackService
+
+    init() {
+        mediaLibraryService = VLCAppCoordinator.sharedInstance().mediaLibraryService
+        playbackService = PlaybackService.sharedInstance()
+        let tracksViewModel = TracksViewModel(mediaLibraryService: mediaLibraryService, playbackService: playbackService)
+        _tracksViewModel = StateObject(wrappedValue: tracksViewModel)
+    }
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environmentObject(dialogProvider)
-                .customDialogOverlay()
+            TabView(selection: $selection) {
+                // TODO: Artists Tab
+
+                // TODO: Albums tab
+
+                MediaListView(medias: tracksViewModel.tracks) { media in
+                    tracksViewModel.play(media: media)
+                }
+
+                // TODO: Add radio discovery view here
+
+                // For testing WatchConnectivity APIs
+                ForEach(commands, id: \.self) { command in
+                    CommandView(command: command, selectedTab: $selection).tag(command)
+                }
+            }
+            .onReceive(NotificationCenter.default.activationDidCompletePublisher) { notification in
+                activationDidComplete(notification)
+            }
+            .onReceive(NotificationCenter.default.reachabilityDidChangePublisher) { notification in
+                reachabilityDidChange(notification)
+            }
+            .environmentObject(tracksViewModel)
         }
+    }
+
+    /**
+     Observe the activation state change and log the current state.
+     */
+    private func activationDidComplete(_ notification: Notification) {
+        print("\(#function): activationState:\(WCSession.default.activationState.rawValue)")
+    }
+    /**
+     Observe the reachability state change and log the current state.
+     */
+    private func reachabilityDidChange(_ notification: Notification) {
+        print("\(#function): isReachable:\(WCSession.default.isReachable)")
+    }
+}
+
+
+extension NotificationCenter {
+    var activationDidCompletePublisher: Publishers.ReceiveOn<NotificationCenter.Publisher, DispatchQueue> {
+        return publisher(for: .activationDidComplete).receive(on: .main)
+    }
+    var reachabilityDidChangePublisher: Publishers.ReceiveOn<NotificationCenter.Publisher, DispatchQueue> {
+        return publisher(for: .reachabilityDidChange).receive(on: .main)
     }
 }

Sources/App/watchOS/ViewModels/TracksViewModel.swift
@@ -1,0 +1,46 @@
+/*****************************************************************************
+ * TracksViewModel.swift
+ * VLC for iOS
+ *****************************************************************************
+ * Copyright (c) 2026 VideoLAN. All rights reserved.
+ * $Id$
+ *
+ * Authors: Timmy Nguyen <timmypass21 # gmail.com>
+ *
+ * Refer to the COPYING file of the official project for license.
+ *****************************************************************************/
+
+
+import Foundation
+
+class TracksViewModel: ObservableObject {
+    let model: MediaLibraryBaseModel
+    let mediaLibraryService: MediaLibraryService
+    let playbackService: PlaybackService
+
+    @Published var tracks: [VLCWatchMLMedia] = []
+    var _tracksMap: [VLCMLIdentifier: VLCMLMedia] = [:]
+
+    init(mediaLibraryService: MediaLibraryService, playbackService: PlaybackService) {
+        self.mediaLibraryService = mediaLibraryService
+        self.playbackService = playbackService
+        model = TrackModel(medialibrary: mediaLibraryService)
+
+        model.sort(by: .default, desc: true)
+        tracks = model.anyfiles.compactMap { (obj: VLCMLObject) -> VLCWatchMLMedia? in
+            guard let media = obj as? VLCMLMedia else { return nil }
+            _tracksMap[media.identifier()] = media
+            return VLCWatchMLMedia(media)
+        }
+
+        if let tracks = model.anyfiles as? [VLCMLMedia] {
+            print("Tracks (\(tracks.count)): \(tracks)")
+        }
+    }
+
+    func play(media: VLCWatchMLMedia) {
+        guard let mlMedia = _tracksMap[media.id] else { return }
+         playbackService.play(mlMedia)
+        print("Playing media: \(media.title)")
+    }
+}

Sources/App/watchOS/Views/MediaListView.swift
@@ -1,0 +1,64 @@
+/*****************************************************************************
+ * MediaListView.swift
+ * VLC for iOS
+ *****************************************************************************
+ * Copyright (c) 2026 VideoLAN. All rights reserved.
+ * $Id$
+ *
+ * Authors: Timmy Nguyen <timmypass21 # gmail.com>
+ *
+ * Refer to the COPYING file of the official project for license.
+ *****************************************************************************/
+
+import SwiftUI
+
+struct MediaListView: View {
+    @EnvironmentObject var contentViewModel: TracksViewModel
+    var medias: [VLCWatchMLMedia]
+    var didTapCell: (VLCWatchMLMedia) -> Void
+
+    var body: some View {
+        List(medias) { media in
+            MediaCellView(thumbnail: media.thumbnail, title: media.title, subtitle: media.artist?.name ?? "")
+                .onTapGesture {
+                    didTapCell(media)
+                }
+        }
+    }
+}
+
+struct MediaCellView: View {
+    var thumbnail: URL?
+    var title: String
+    var subtitle: String
+
+    var body: some View {
+        HStack {
+            Group {
+                if let thumbnailPath = thumbnail?.path(),
+                   let uiImage = UIImage(contentsOfFile: thumbnailPath) {
+                    Image(uiImage: uiImage)
+                        .resizable()
+                        .scaledToFit()
+                } else {
+                    Rectangle()
+                }
+            }
+            .frame(width: 42, height: 42)
+            .cornerRadius(8)
+
+            VStack(alignment: .leading) {
+                Text(title)
+                    .lineLimit(1)
+                Text(subtitle)
+                    .lineLimit(1)
+                    .foregroundStyle(.secondary)
+            }
+        }
+    }
+
+}
+
+//#Preview {
+//    AlbumListView()
+//}

commit 2e3a56d8707ee20a3a8658551bada47e36176470

EditButton: disable transfer file
By using the same icon, this led to a collision with play-new-in-queue. Regression from 38e55f8c4

Sources/Media Library/Editing/EditButtons.swift
@@ -104,7 +104,7 @@ class EditButtonsFactory {
 //        actionList.append(.updateAppContext)
 //        actionList.append(.sendMessage)
 //        actionList.append(.sendMessageData)
-        actionList.append(.transferFile)
+//        actionList.append(.transferFile)
 //        actionList.append(.transferUserInfo)
 //        actionList.append(.transferComplicationUserInfo)
 