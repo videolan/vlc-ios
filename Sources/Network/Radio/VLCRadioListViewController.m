b9645cc21d932902d3c4eeed08c4be12a6455ebf
Felix Paul Kühne <fkuehne@videolan.org>
2026-07-21 14:20:01 +0200

    Radio: show favorites and recent countries in the radio tab

--- Sources/Network/Radio/VLCRadioListViewController.m
+++ Sources/Network/Radio/VLCRadioListViewController.m
@@ -11,31 +11,25 @@
  *****************************************************************************/
 
 #import "VLCRadioListViewController.h"
-#import "VLCServiceBrowserRadio.h"
+#import "VLCRadioCountryListViewController.h"
+#import "VLCRadioCountryService.h"
+#import "VLCRadioCountry.h"
+#import "VLCFavoriteService.h"
+#import "VLCAppCoordinator.h"
 #import "VLCNetworkListCell.h"
 #import "VLCNetworkServerBrowserViewController.h"
 #import "VLCPlaybackService.h"
 
 #import "VLC-Swift.h"
 
-@interface VLCRadioListViewController () <VLCLocalNetworkServiceBrowserDelegate>
+@interface VLCRadioListViewController ()
 {
-    VLCServiceBrowserRadio *_serviceBrowser;
-    NSArray<id<VLCLocalNetworkService>> *_searchResults;
+    VLCRadioCountryService *_countryService;
+    NSArray<VLCFavorite *> *_radioFavorites;
 }
 @end
 
 @implementation VLCRadioListViewController
-
-- (instancetype)init
-{
-    self = [super init];
-    if (self) {
-        _serviceBrowser = [[VLCServiceBrowserRadio alloc] init];
-        _serviceBrowser.delegate = self;
-    }
-    return self;
-}
 
 - (void)viewDidLoad
 {
@@ -43,58 +37,69 @@
 
     self.title = NSLocalizedString(@"RADIO", nil);
     [self removePlayAllAction];
+    self.navigationItem.searchController = nil;
+
+    _countryService = [[VLCAppCoordinator sharedInstance] radioCountryService];
+    _radioFavorites = @[];
+
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(radioCountriesDidUpdate:)
+                                                 name:VLCRadioCountriesDidUpdateNotification
+                                               object:nil];
 }
 
 - (void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
 
-    [self startActivityIndicator];
-    [_serviceBrowser startDiscovery];
+    self.navigationController.navigationBar.prefersLargeTitles = YES;
+    self.navigationItem.largeTitleDisplayMode = UINavigationItemLargeTitleDisplayModeAlways;
+
+    _radioFavorites = [[[VLCAppCoordinator sharedInstance] favoriteService] favoritesInGroupWithIdentifier:VLCFavoriteGroupRadio];
+    [self.tableView reloadData];
+
+    [_countryService startCountryDiscoveryIfNeeded];
 }
 
 - (void)viewWillDisappear:(BOOL)animated
 {
     [super viewWillDisappear:animated];
 
-    [_serviceBrowser stopDiscovery];
+    self.navigationController.navigationBar.prefersLargeTitles = NO;
+
+    [_countryService stopCountryDiscovery];
 }
 
-#pragma mark - service access
-
-- (BOOL)isSearching
+- (void)radioCountriesDidUpdate:(NSNotification *)notification
 {
-    return self.searchController.isActive && self.searchController.searchBar.text.length > 0;
+    [self.tableView reloadData];
 }
 
-- (id<VLCLocalNetworkService>)serviceForIndexPath:(NSIndexPath *)indexPath
+#pragma mark - section layout
+
+- (BOOL)favoritesSectionVisible
 {
-    NSUInteger row = indexPath.row;
-
-    if ([self isSearching]) {
-        return row < _searchResults.count ? _searchResults[row] : nil;
-    }
-
-    return [_serviceBrowser networkServiceForIndex:row];
+    return _radioFavorites.count > 0;
 }
 
-- (VLCMedia *)mediaForService:(id<VLCLocalNetworkService>)service
+- (NSInteger)countriesSection
 {
-    if (![service isKindOfClass:[VLCLocalNetworkServiceVLCMedia class]])
-        return nil;
-
-    return [(VLCLocalNetworkServiceVLCMedia *)service mediaItem];
+    return self.favoritesSectionVisible ? 1 : 0;
 }
 
 #pragma mark - table view data source
 
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return self.favoritesSectionVisible ? 2 : 1;
+}
+
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    if ([self isSearching]) {
-        return _searchResults.count;
+    if (section == self.countriesSection) {
+        return _countryService.visitedCountries.count + 1;
     }
-
-    return _serviceBrowser.numberOfItems;
+    return _radioFavorites.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -103,17 +108,47 @@
     if (cell == nil)
         cell = [VLCNetworkListCell cellWithReuseIdentifier:VLCNetworkListCellIdentifier];
 
-    id<VLCLocalNetworkService> service = [self serviceForIndexPath:indexPath];
-    VLCMedia *media = [self mediaForService:service];
-
-    [cell setIsDirectory:media.mediaType == VLCMediaTypeDirectory];
-    [cell setIconURL:service.iconURL];
-    if (cell.iconURL == nil)
-        [cell setIcon:service.icon];
-    [cell setTitle:service.title];
-    [cell setTitleLabelCentered:YES];
+    if (indexPath.section == self.countriesSection) {
+        [self configureCountryCell:cell atRow:indexPath.row];
+    } else {
+        [self configureFavoriteCell:cell atRow:indexPath.row];
+    }
 
     return cell;
+}
+
+- (void)configureFavoriteCell:(VLCNetworkListCell *)cell atRow:(NSInteger)row
+{
+    VLCFavorite *favorite = _radioFavorites[row];
+
+    [cell setIsDirectory:NO];
+    [cell setTitleLabelCentered:favorite.mediaDescription.length == 0];
+    [cell setTitle:favorite.userVisibleName];
+    [cell setSubtitle:favorite.mediaDescription];
+    if (favorite.artworkURL) {
+        [cell setIconURL:favorite.artworkURL];
+    } else if (@available(iOS 13.0, *)) {
+        [cell setIcon:[UIImage systemImageNamed:@"antenna.radiowaves.left.and.right"]];
+    }
+}
+
+- (void)configureCountryCell:(VLCNetworkListCell *)cell atRow:(NSInteger)row
+{
+    [cell setIsDirectory:YES];
+
+    NSArray<VLCRadioCountry *> *visited = _countryService.visitedCountries;
+    if (row < visited.count) {
+        VLCRadioCountry *country = visited[row];
+        [cell setTitle:country.localizedName];
+        [cell setIcon:country.flagImage];
+    } else {
+        [cell setTitle:NSLocalizedString(@"ALL_COUNTRIES", nil)];
+        if (@available(iOS 14.2, *)) {
+            [cell setIcon:[UIImage systemImageNamed:@"globe.europe.africa"]];
+        } else if (@available(iOS 13.0, *)) {
+            [cell setIcon:[UIImage systemImageNamed:@"globe"]];
+        }
+    }
 }
 
 #pragma mark - table view delegate
@@ -127,59 +162,83 @@
     cell.subtitleLabel.textColor = themeColors.cellDetailTextColor;
 }
 
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    return 40.0;
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
+{
+    NSString *title = (section == self.countriesSection) ? NSLocalizedString(@"COUNTRIES", nil)
+                                                         : NSLocalizedString(@"FAVORITES", nil);
+    return [self sectionHeaderViewWithTitle:title];
+}
+
+- (UIView *)sectionHeaderViewWithTitle:(NSString *)title
+{
+    ColorPalette *themeColors = PresentationTheme.current.colors;
+
+    UIView *header = [[UIView alloc] init];
+    header.backgroundColor = themeColors.background;
+
+    UILabel *label = [[UILabel alloc] init];
+    label.translatesAutoresizingMaskIntoConstraints = NO;
+    label.font = [UIFont systemFontOfSize:16.0 weight:UIFontWeightMedium];
+    label.textColor = themeColors.cellTextColor;
+    label.text = title;
+    [header addSubview:label];
+
+    [NSLayoutConstraint activateConstraints:@[
+        [label.leadingAnchor constraintEqualToAnchor:header.safeAreaLayoutGuide.leadingAnchor constant:20.0],
+        [label.centerYAnchor constraintEqualToAnchor:header.centerYAnchor]
+    ]];
+
+    return header;
+}
+
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
 
-    id<VLCLocalNetworkService> service = [self serviceForIndexPath:indexPath];
-    id<VLCNetworkServerBrowser> serverBrowser = [service serverBrowser];
-    if (!serverBrowser) {
-        VLCMedia *media = [self mediaForService:service];
-        if (media) {
-            VLCMediaList *mediaList = [[VLCMediaList alloc] init];
-            [mediaList addMedia:media];
-            [[VLCPlaybackService sharedInstance] playMediaList:mediaList firstIndex:0 subtitlesFilePath:nil];
-        }
+    if (indexPath.section == self.countriesSection) {
+        [self didSelectCountryAtRow:indexPath.row];
+    } else {
+        [self playFavoriteAtRow:indexPath.row];
+    }
+}
+
+- (void)playFavoriteAtRow:(NSInteger)row
+{
+    VLCFavorite *favorite = _radioFavorites[row];
+    VLCMedia *media = [VLCMedia mediaWithURL:favorite.url];
+    if (!media)
+        return;
+
+    VLCMediaList *mediaList = [[VLCMediaList alloc] init];
+    [mediaList addMedia:media];
+    [[VLCPlaybackService sharedInstance] playMediaList:mediaList firstIndex:0 subtitlesFilePath:nil];
+}
+
+- (void)didSelectCountryAtRow:(NSInteger)row
+{
+    NSArray<VLCRadioCountry *> *visited = _countryService.visitedCountries;
+    if (row >= visited.count) {
+        VLCRadioCountryListViewController *targetViewController = [[VLCRadioCountryListViewController alloc] init];
+        [self.navigationController pushViewController:targetViewController animated:YES];
         return;
     }
+
+    VLCRadioCountry *country = visited[row];
+    [_countryService markCountryVisited:country];
+
+    id<VLCNetworkServerBrowser> serverBrowser = [country makeServerBrowser];
+    if (!serverBrowser)
+        return;
 
     VLCNetworkServerBrowserViewController *targetViewController =
         [[VLCNetworkServerBrowserViewController alloc] initWithServerBrowser:serverBrowser
                                                          medialibraryService:[[VLCAppCoordinator sharedInstance] mediaLibraryService]];
     [self.navigationController pushViewController:targetViewController animated:YES];
-}
-
-#pragma mark - search
-
-- (void)updateSearchResultsForSearchController:(UISearchController *)searchController
-{
-    NSString *searchString = searchController.searchBar.text;
-    NSUInteger count = _serviceBrowser.numberOfItems;
-    NSMutableArray<id<VLCLocalNetworkService>> *results = [NSMutableArray arrayWithCapacity:count];
-
-    for (NSUInteger i = 0; i < count; i++) {
-        id<VLCLocalNetworkService> service = [_serviceBrowser networkServiceForIndex:i];
-        if ([service.title rangeOfString:searchString options:NSCaseInsensitiveSearch].location != NSNotFound) {
-            [results addObject:service];
-        }
-    }
-
-    _searchResults = results;
-    [self.tableView reloadData];
-}
-
-#pragma mark - VLCLocalNetworkServiceBrowserDelegate
-
-- (void)localNetworkServiceBrowserDidUpdateServices:(id<VLCLocalNetworkServiceBrowser>)serviceBrowser
-{
-    [self stopActivityIndicator];
-
-    if ([self isSearching]) {
-        [self updateSearchResultsForSearchController:self.searchController];
-        return;
-    }
-
-    [self.tableView reloadData];
 }
 
 @end
