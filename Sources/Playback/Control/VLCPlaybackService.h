videolan/vlc-ios

commit 7532ab3f0687327c06a8fd96a05e02b4fd18b557

VLCPlaybackService: Use parser to read m3u file

Sources/Playback/Control/VLCPlaybackService.h
@@ -205,5 +205,7 @@ NS_SWIFT_NAME(PlaybackService)
 - (NSIndexPath *)selectedEqualizerProfile;
 #endif
 
+- (void) saveCurrentlyPlayingMediaIdentifier;
+- (void) restoreLastPlayedMediaList;
 NS_ASSUME_NONNULL_END
 @end

Sources/App/iOS/VLCAppDelegate.m
@@ -165,7 +165,7 @@
     }
 
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
-        [self restoreLastPlayedMediaList];
+        [[VLCAppCoordinator sharedInstance].mediaLibraryService restoreLastPlayedMediaList];
     });
 
     self.orientationLock = UIInterfaceOrientationMaskPortrait | UIInterfaceOrientationMaskLandscape;
@@ -279,7 +279,7 @@
     VLCFavoriteService *fs = [[VLCAppCoordinator sharedInstance] favoriteService];
     [fs storeContentSynchronously];
 
-    [self savePlayingMediaIdentifier];
+    [vps saveCurrentlyPlayingMediaIdentifier];
 }
 
 - (void)application:(UIApplication *)application performActionForShortcutItem:(UIApplicationShortcutItem *)shortcutItem completionHandler:(void (^)(BOOL))completionHandler
@@ -339,44 +339,6 @@
 
 - (void)application:(UIApplication *)application didDiscardSceneSessions:(NSSet<UISceneSession *> *)sceneSessions  API_AVAILABLE(ios(13.0))
 {
-}
-
-#pragma mark - Recover last playing media
-
-- (void)savePlayingMediaIdentifier {
-    VLCMLMedia *libraryMedia = [[VLCPlaybackService sharedInstance] currentlyPlayingLibraryMedia];
-    VLCMLIdentifier identifier = libraryMedia ? libraryMedia.identifier : -1;
-
-    [[NSUserDefaults standardUserDefaults] setInteger:identifier forKey:kVLCLastPlayedMediaIdentifier];
-}
-
-- (void)restoreLastPlayedMediaList {
-    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-    if (![defaults boolForKey:kVLCRestoreLastPlayedMedia]) {
-        return;
-    }
-
-    MediaLibraryService *mediaLibraryService = [[VLCAppCoordinator sharedInstance] mediaLibraryService];
-    NSArray<VLCMLMedia *> *mediaList = [mediaLibraryService getLastPlayedMediaList];
-    NSUInteger mediaCount = mediaList.count;
-
-    if (mediaCount == 0) {
-        return;
-    }
-
-    VLCMLIdentifier lastPlayedMediaId = [defaults integerForKey:kVLCLastPlayedMediaIdentifier];
-    NSInteger lastPlayedMediaIndex = NSNotFound;
-
-    for (NSUInteger i = 0; i < mediaCount; i++) {
-        if ([mediaList[i] identifier] == lastPlayedMediaId) {
-            lastPlayedMediaIndex = i;
-            break;
-        }
-    }
-
-    lastPlayedMediaIndex = (lastPlayedMediaIndex != NSNotFound) ? lastPlayedMediaIndex : 0;
-    [[VLCPlaybackService sharedInstance] configurePlaybackWithMediaAtIndex:lastPlayedMediaIndex fromCollection:mediaList openInMiniPlayer:YES];
-    [defaults setInteger:-1 forKey:kVLCLastPlayedMediaIdentifier];
 }
 
 @end

Sources/Helpers/StreamReader.swift
@@ -1,81 +1,0 @@
-// Created by Martin R (https://stackoverflow.com/a/24648951/20237973)
-
-class StreamReader  {
-
-    let encoding : String.Encoding
-    let chunkSize : Int
-    var fileHandle : FileHandle!
-    let delimData : Data
-    var buffer : Data
-    var atEof : Bool
-
-    init?(path: String, delimiter: String = "\n", encoding: String.Encoding = .utf8,
-          chunkSize: Int = 4096) {
-
-        guard let fileHandle = FileHandle(forReadingAtPath: path),
-            let delimData = delimiter.data(using: encoding) else {
-                return nil
-        }
-        self.encoding = encoding
-        self.chunkSize = chunkSize
-        self.fileHandle = fileHandle
-        self.delimData = delimData
-        self.buffer = Data(capacity: chunkSize)
-        self.atEof = false
-    }
-
-    deinit {
-        self.close()
-    }
-
-    /// Return next line, or nil on EOF.
-    func nextLine() -> String? {
-        precondition(fileHandle != nil, "Attempt to read from closed file")
-
-        // Read data chunks from file until a line delimiter is found:
-        while !atEof {
-            if let range = buffer.range(of: delimData) {
-                // Convert complete line (excluding the delimiter) to a string:
-                let line = String(data: buffer.subdata(in: 0..<range.lowerBound), encoding: encoding)
-                // Remove line (and the delimiter) from the buffer:
-                buffer.removeSubrange(0..<range.upperBound)
-                return line
-            }
-            let tmpData = fileHandle.readData(ofLength: chunkSize)
-            if !tmpData.isEmpty {
-                buffer.append(tmpData)
-            } else {
-                // EOF or read error.
-                atEof = true
-                if !buffer.isEmpty {
-                    // Buffer contains last line in file (not terminated by delimiter).
-                    let line = String(data: buffer as Data, encoding: encoding)
-                    buffer.count = 0
-                    return line
-                }
-            }
-        }
-        return nil
-    }
-
-    /// Start reading from the beginning of file.
-    func rewind() -> Void {
-        fileHandle.seek(toFileOffset: 0)
-        buffer.count = 0
-        atEof = false
-    }
-
-    /// Close the underlying file. No reading must be done after calling this method.
-    func close() -> Void {
-        fileHandle?.closeFile()
-        fileHandle = nil
-    }
-}
-
-extension StreamReader : Sequence {
-    func makeIterator() -> AnyIterator<String> {
-        return AnyIterator {
-            return self.nextLine()
-        }
-    }
-}

Sources/Media Library/MediaLibraryService.swift
@@ -257,6 +257,8 @@ class MediaLibraryService: NSObject {
         desiredThumbnailWidth = UInt(scaledCellWidth)
         desiredThumbnailHeight = UInt(scaledCellWidth / 1.6)
         #endif
+
+        VLCMediaParser.shared().delegate = self
     }
 
 #if !os(tvOS) && !os(watchOS)
@@ -639,42 +641,19 @@ private extension MediaLibraryService {
         _ = try? FileManager.default.copyItem(atPath: databasePath, toPath: targetPath)
     }
 
-    @objc func getLastPlayedMediaList() -> [VLCMLMedia] {
-        guard let appSupportURL = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask).first,
-              let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
-        else {
-            return []
-        }
+    func restoreLastPlayedMediaList() {
+        guard UserDefaults.standard.bool(forKey: kVLCRestoreLastPlayedMedia) else { return }
+
+        guard let appSupportURL = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask).first
+        else { return }
 
         let m3uFileName = NSLocalizedString("LAST_PLAYED_MEDIALIST", comment: "").appending(".m3u")
         let m3uFileURL = appSupportURL.appendingPathComponent(m3uFileName)
-        guard FileManager.default.fileExists(atPath: m3uFileURL.path) else { return [] }
+        guard FileManager.default.fileExists(atPath: m3uFileURL.path) else { return }
 
-        var collection: [VLCMLMedia] = []
-
-        if let streamReader = StreamReader(path: m3uFileURL.path) {
-            defer {
-                streamReader.close()
-            }
-
-            let _ = streamReader.nextLine() // skip header line (i.e. #EXTM3U)
-            while streamReader.nextLine() != nil { // skip title
-                guard let mediaURLString = streamReader.nextLine(),
-                      let range = mediaURLString.range(of: "Documents/")
-                else { continue }
-
-                let relativePath = String(mediaURLString[range.upperBound...])
-                let newMediaURLString = documentsURL.absoluteString + relativePath
-                let mediaURL = URL(string: newMediaURLString)
-
-                if let media = fetchMedia(with: mediaURL),
-                   !media.isExternalMedia() {
-                    collection.append(media)
-                }
-            }
+        if let media = VLCMedia(url: m3uFileURL) {
+            VLCMediaParser.shared().queue(media)
         }
-
-        return collection
     }
 }
 
@@ -1118,3 +1097,38 @@ extension MediaLibraryService {
     }
 }
 #endif
+
+extension MediaLibraryService: VLCMediaParserDelegate {
+    func mediaFinishedParsing(_ media: VLCMedia, with status: VLCMediaParsedStatus) {
+        guard status == .done,
+              let mediaList = media.subitems
+        else { return }
+
+        guard let appSupportURL = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask).first else { return }
+
+        let m3uFileName = NSLocalizedString("LAST_PLAYED_MEDIALIST", comment: "").appending(".m3u")
+        let m3uFileURL = appSupportURL.appendingPathComponent(m3uFileName)
+        guard FileManager.default.fileExists(atPath: m3uFileURL.path) else { return }
+
+        let defaults = UserDefaults.standard
+        let mediaCount = mediaList.count
+
+        guard mediaCount > 0 else { return }
+
+        let lastPlayedMediaId = defaults.integer(forKey: kVLCLastPlayedMediaIdentifier)
+        var collection: [VLCMLMedia] = []
+
+        for i in 0..<mediaCount {
+            guard let media = mediaList.media(at: UInt(i)),
+                  let mlMedia = fetchMedia(with: media.url),
+                  !mlMedia.isExternalMedia()
+            else { continue }
+
+            collection.append(mlMedia)
+        }
+
+        let lastPlayedMediaIndex = collection.firstIndex { $0.identifier() == lastPlayedMediaId } ?? 0
+        PlaybackService.sharedInstance().configurePlaybackWithMedia(at: lastPlayedMediaIndex, fromCollection: collection, openInMiniPlayer: true)
+        defaults.set(-1, forKey: kVLCLastPlayedMediaIdentifier)
+    }
+}

Sources/Playback/Control/VLCPlaybackService.m
@@ -2053,6 +2053,12 @@ NSString *const VLCLastPlaylistPlayedMedia = @"LastPlaylistPlayedMedia";
 }
 
 #if !TARGET_OS_WATCH
+
+- (void)saveCurrentlyPlayingMediaIdentifier {
+    VLCMLIdentifier identifier = _currentlyPlayingLibraryMedia ? _currentlyPlayingLibraryMedia.identifier : -1;
+    [[NSUserDefaults standardUserDefaults] setInteger:identifier forKey:kVLCLastPlayedMediaIdentifier];
+}
+
 - (void) saveCurrentMediaList {
     NSString *appSupportDir = [NSSearchPathForDirectoriesInDomains(NSApplicationSupportDirectory, NSUserDomainMask, YES) lastObject];
     if (![[NSFileManager defaultManager] fileExistsAtPath:appSupportDir isDirectory:NULL]) {
@@ -2068,7 +2074,7 @@ NSString *const VLCLastPlaylistPlayedMedia = @"LastPlaylistPlayedMedia";
     [[NSFileManager defaultManager] removeItemAtURL:fileURL error:nil];
 
     NSError *error = nil;
-    if (![_mediaList writeM3UToURL:fileURL error:&error]) {
+    if (![_mediaList writeM3UToURL:fileURL relativeToDirectory:appSupportURL error:&error]) {
         APLog(@"Failed to write M3U for saving medialist: %@", error);
         return;
     }

VLC.xcodeproj/project.pbxproj
@@ -93,11 +93,6 @@
 		32269BA12FEE347B00C80FFF /* MoveToRootHeader.swift in Sources */ = {isa = PBXBuildFile; fileRef = 32269B9F2FEE347B00C80FFF /* MoveToRootHeader.swift */; };
 		32269BA22FEE347B00C80FFF /* MoveToRootHeader.swift in Sources */ = {isa = PBXBuildFile; fileRef = 32269B9F2FEE347B00C80FFF /* MoveToRootHeader.swift */; };
 		322A104A2F6C69EE005EC9F0 /* GameControllerManager.swift in Sources */ = {isa = PBXBuildFile; fileRef = 32B2A2352F65E17300D5EFBC /* GameControllerManager.swift */; };
-		323F38313018665300709EF1 /* StreamReader.swift in Sources */ = {isa = PBXBuildFile; fileRef = 323F38303018665300709EF1 /* StreamReader.swift */; };
-		323F38323018665300709EF1 /* StreamReader.swift in Sources */ = {isa = PBXBuildFile; fileRef = 323F38303018665300709EF1 /* StreamReader.swift */; };
-		323F38333018665300709EF1 /* StreamReader.swift in Sources */ = {isa = PBXBuildFile; fileRef = 323F38303018665300709EF1 /* StreamReader.swift */; };
-		323F38343018667100709EF1 /* StreamReader.swift in Sources */ = {isa = PBXBuildFile; fileRef = 323F38303018665300709EF1 /* StreamReader.swift */; };
-		323F38353018667100709EF1 /* StreamReader.swift in Sources */ = {isa = PBXBuildFile; fileRef = 323F38303018665300709EF1 /* StreamReader.swift */; };
 		324ACA732FFF25C70002F804 /* MLSyncManagerProtocol.swift in Sources */ = {isa = PBXBuildFile; fileRef = 324ACA722FFF25C70002F804 /* MLSyncManagerProtocol.swift */; };
 		324ACA742FFF25C80002F804 /* MLSyncManagerProtocol.swift in Sources */ = {isa = PBXBuildFile; fileRef = 324ACA722FFF25C70002F804 /* MLSyncManagerProtocol.swift */; };
 		324BD7332FDB8BEA0037F446 /* BuildFile in Sources */ = {isa = PBXBuildFile; };
@@ -1728,7 +1723,6 @@
 		321B3B592FB6C92C007E4E8A /* UserInfoTransfersView.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = UserInfoTransfersView.swift; sourceTree = "<group>"; };
 		32269B9F2FEE347B00C80FFF /* MoveToRootHeader.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = MoveToRootHeader.swift; sourceTree = "<group>"; };
 		323B00FA2FC940590049CE0E /* VLCWatchMLMedia.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = VLCWatchMLMedia.swift; sourceTree = "<group>"; };
-		323F38303018665300709EF1 /* StreamReader.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = StreamReader.swift; sourceTree = "<group>"; };
 		324ACA722FFF25C70002F804 /* MLSyncManagerProtocol.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = MLSyncManagerProtocol.swift; sourceTree = "<group>"; };
 		3278C8C42FB7A230008581AA /* WatchLogger.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = WatchLogger.swift; sourceTree = "<group>"; };
 		3278C8C82FB7D129008581AA /* TracksViewModel.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = TracksViewModel.swift; sourceTree = "<group>"; };
@@ -3092,7 +3086,6 @@
 				91C1BB7F25EFD7A40096F97E /* ColorThemeExtension.swift */,
 				7D39B6DE3005356700D95829 /* VLCMigrationCursor.h */,
 				7D39B6DF3005356700D95829 /* VLCMigrationCursor.m */,
-				323F38303018665300709EF1 /* StreamReader.swift */,
 			);
 			path = Helpers;
 			sourceTree = "<group>";
@@ -5735,7 +5728,6 @@
 				7D1E8F57300FA4160014A118 /* VLCRadioCountry.m in Sources */,
 				226686992A91DED1003114C7 /* AddToPlaylistViewController.swift in Sources */,
 				322A104A2F6C69EE005EC9F0 /* GameControllerManager.swift in Sources */,
-				323F38343018667100709EF1 /* StreamReader.swift in Sources */,
 				7DC869BA28CB7A7000EE99F8 /* VLCOpenNetworkStreamTVViewController.m in Sources */,
 				7DEF111F2B8BA9DC0068D030 /* VLCLocalNetworkServiceBrowserFavorites.m in Sources */,
 				7D2DE69424D5A9AA00E8D84D /* VLCOpenSubtitlesDownloader.m in Sources */,
@@ -6078,7 +6070,6 @@
 				7D50C6C02BBD20DF00B9F1A0 /* MiniPlayer.swift in Sources */,
 				7D1E8F41300F7A850014A118 /* VLCRadioFavoritesGridCell.m in Sources */,
 				7D50C6C12BBD20DF00B9F1A0 /* PlayerViewController.swift in Sources */,
-				323F38333018665300709EF1 /* StreamReader.swift in Sources */,
 				7D50C6C22BBD20DF00B9F1A0 /* AppearanceManager.swift in Sources */,
 				7D50C6C32BBD20DF00B9F1A0 /* PlayerInfoView.swift in Sources */,
 				7D50C6C42BBD20DF00B9F1A0 /* TrackModel.swift in Sources */,
@@ -6314,7 +6305,6 @@
 				8DB0D71924CED15C00915506 /* VideoPlayerControls.swift in Sources */,
 				448BA8482D83116D008CCDFF /* PreferenceSetting.swift in Sources */,
 				41B93C051A53835300102E8B /* VLCCloudServiceCell.m in Sources */,
-				323F38313018665300709EF1 /* StreamReader.swift in Sources */,
 				1B39FA082C73BBA200F6F960 /* NSURLSessionConfiguration+default.m in Sources */,
 				8DE1887421089B3A00A091D2 /* MediaLibraryBaseModel.swift in Sources */,
 				7D6D2CEF2A0A6056001604B3 /* VLCOneDriveController.m in Sources */,
@@ -6686,7 +6676,6 @@
 				7DA514D72EF82B63000C9995 /* PlaylistHeader.swift in Sources */,
 				7DA514D82EF82B63000C9995 /* VLCHTTPFileDownloader.m in Sources */,
 				7DA514D92EF82B63000C9995 /* VLCLinearProgressIndicator.m in Sources */,
-				323F38323018665300709EF1 /* StreamReader.swift in Sources */,
 				7DA514DA2EF82B63000C9995 /* VLCBoxTableViewController.m in Sources */,
 				7DA514DB2EF82B63000C9995 /* VLCPlexParser.m in Sources */,
 				7DA514DC2EF82B63000C9995 /* HistoryModel.swift in Sources */,
@@ -6894,7 +6883,6 @@
 				3278C8CB2FB7E02B008581AA /* TrackModel.swift in Sources */,
 				321B3B4D2FB6B8D0007E4E8A /* TimedColor.swift in Sources */,
 				3217BE772FCFDBEB006D75BC /* VLCWatchMLAlbum.swift in Sources */,
-				323F38353018667100709EF1 /* StreamReader.swift in Sources */,
 				328BB88D2FCE7CFF00E1FE19 /* VLCMLMedia+Podcast.m in Sources */,
 				BE4D24812E32C0E400434D7A /* Observable.swift in Sources */,
 				321B3B562FB6BCFB007E4E8A /* SessionTransfer.swift in Sources */,

commit 10b290fb5db4a7c88130952013358789c9aaa533

VPS: fix compilation warnings
Minor regressions from 7532ab3f0

Sources/Playback/Control/VLCPlaybackService.h
@@ -207,7 +207,9 @@ NS_SWIFT_NAME(PlaybackService)
 - (NSIndexPath *)selectedEqualizerProfile;
 #endif
 
-- (void) saveCurrentlyPlayingMediaIdentifier;
-- (void) restoreLastPlayedMediaList;
+#if !TARGET_OS_WATCH
+- (void)saveCurrentlyPlayingMediaIdentifier;
+#endif
+
 NS_ASSUME_NONNULL_END
 @end