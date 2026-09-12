4c6a11a19d40f878e031ec55177a41c3ea910ae0
Felix Paul Kühne <fkuehne@videolan.org>
2026-09-12 09:44:54 +0200

    Quick actions: drop the browse action

--- Sources/App/iOS/VLCAppDelegate.m
+++ Sources/App/iOS/VLCAppDelegate.m
@@ -147,13 +147,9 @@
                                                                                  localizedSubtitle:nil
                                                                                               icon:[UIApplicationShortcutIcon iconWithTemplateImageName:@"Playlist"]
                                                                                           userInfo:nil];
-    UIApplicationShortcutItem *browseItem = [[UIApplicationShortcutItem alloc] initWithType:kVLCApplicationShortcutNetwork
-                                                                             localizedTitle:NSLocalizedString(@"BROWSE",nil)
-                                                                          localizedSubtitle:nil
-                                                                                       icon:[UIApplicationShortcutIcon iconWithTemplateImageName:@"Network"]
-                                                                                   userInfo:nil];
-    /* the dynamic last played item is added by the tab coordinator once the media library is up */
-    application.shortcutItems = @[localVideoItem, localAudioItem, localplaylistItem, browseItem];
+    /* iOS shows at most four quick actions and the tab coordinator prepends the dynamic last
+       played item once the media library is up, so there is room for three static ones only */
+    application.shortcutItems = @[localVideoItem, localAudioItem, localplaylistItem];
 }
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
