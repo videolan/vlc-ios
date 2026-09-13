35fe6c05ad0cd1a6240111b0cb06fd6b886bd9ea
Felix Paul Kühne <fkuehne@videolan.org>
2026-09-13 08:56:49 +0200

    settings: add save audio delay toggle

--- Sources/Settings/Model/SettingsSection.swift
+++ Sources/Settings/Model/SettingsSection.swift
@@ -206,6 +206,11 @@ enum PlaybackOptions {
                      action: .showActionSheet(title: "SETTINGS_PLAYBACK_SPEED_DEFAULT", preferenceKey: k, hasInfo: false))
     }
 
+    static var saveAudioDelay: SettingsItem {
+        .toggle(title: "SETTINGS_SAVE_AUDIO_DELAY",
+                preferenceKey: kVLCSettingSaveAudioDelay)
+    }
+
     static var continueAudioPlayback: SettingsItem {
         let k = kVLCSettingContinueAudioPlayback
         return .init(title: "SETTINGS_CONTINUE_AUDIO_PLAYBACK",
@@ -257,6 +262,7 @@ enum PlaybackOptions {
     static func section() -> SettingsSection? {
         .init(title: "SETTINGS_PLAYBACK_TITLE", items: [
             defaultPlaybackSpeed,
+            saveAudioDelay,
             continueAudioPlayback,
             playVideoInFullScreen,
             continueVideoPlayback,
